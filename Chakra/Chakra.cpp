c73ae13ffc8259cda8569cf34de41dc53726a681
ShrBox <shrbox@outlook.com>
2021-02-06 22:46:04 +0800

    fix

--- Chakra/Chakra.cpp
+++ Chakra/Chakra.cpp
@@ -5,14 +5,5 @@
 #include "framework.h"
 #include "Chakra.h"
 
-/*
-
-extern "C" {
-// The core api of the hook function
-HOOKAPI int HookFunction(void *oldfunc, void **poutold, void *newfunc);
-// Used to get a server-defined specific function by name
-//HOOKAPI void *GetServerSymbol(char const *name);
-}
-*/
-#pragma comment(linker, "/export:HookFunction=LiteLoader.JsAddRef")
+#pragma comment(linker, "/export:HookFunction=LiteLoader.HookFunction")
 #pragma comment(linker, "/export:GetServerSymbol=LiteLoader.dlsym_real")
