f86ca559713ba7481914dcb4830b9df78a9e075f
WangYneos <WangYneos@outlook.com>
2021-02-14 13:47:08 +0800

    Update auto build

--- headers/mc/OffsetHelper.h
+++ headers/mc/OffsetHelper.h
@@ -1,14 +1,18 @@
 #include <loader/Loader.h>
 class Player;
 class Level;
+class Certificate;
 namespace offPlayer {
-	void* getCert(Player* pl) {
-		return dAccess<void*, 2840>(pl);
+	Certificate* getCert(Player* pl) {
+		return dAccess<Certificate*, 2840>(pl);
 	}
 	string getXUID(Player* pl) {
 		Level* level = (Level*)*((uintptr_t*)((uintptr_t)pl + 856));
 		void* mce_uuid = (void*)((uintptr_t)pl + 2824); //witherboss::_hurt
 		return SymCall("?getPlayerXUID@Level@@QEBAAEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@AEBVUUID@mce@@@Z",
 			string&, Level*, void*)(level, mce_uuid);
+	}
+	xuid_t getXUIDbyCert(Player* pl) {
+		return atoll(SymCall("?getXuid@ExtendedCertificate@@SA?AV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@AEBVCertificate@@@Z", string, void*)(offPlayer::getCert((Player*)pl)).c_str());
 	}
 };
