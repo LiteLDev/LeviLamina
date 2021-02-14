d4130647e1f0e389d19d31ce24cc45c396a52ce0
DreamGuXiang <166664660@qq.com>
2021-02-14 12:16:30 +0800

    add offPlayer::getXUID

--- headers/mc/OffsetHelper.h
+++ headers/mc/OffsetHelper.h
@@ -1,7 +1,14 @@
 #include <loader/Loader.h>
 class Player;
+class Level;
 namespace offPlayer {
 	void* getCert(Player* pl) {
 		return dAccess<void*, 2840>(pl);
+	}
+	string getXUID(Player* pl) {
+		Level* level = (Level*)*((uintptr_t*)((uintptr_t)pl + 856));
+		void* mce_uuid = (void*)((uintptr_t)pl + 2824); //witherboss::_hurt
+		return SymCall("?getPlayerXUID@Level@@QEBAAEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@AEBVUUID@mce@@@Z",
+			string&, Level*, void*)(level, mce_uuid);
 	}
 };
