29a307d4d00c68d1b5f6eb40fa72b852d43148c4
ShrBox <shrbox@outlook.com>
2021-03-16 19:20:49 +0800

    update CheckUpdate

--- LiteLoader/CheckUpdate.cpp
+++ LiteLoader/CheckUpdate.cpp
@@ -4,31 +4,31 @@
 
 void updateCheck() {
     std::thread t([] {
-        httplib::Client cli("https://gitee.com");
-        auto res = cli.Get("/dreamguxiang/LLessentials/raw/master/llupdate.json");
+        httplib::Client cli("https://liteldev.github.io");
+        auto res = cli.Get("/CheckUpdate/version.json");
         cli.set_connection_timeout(0, 300000);
         rapidjson::Document json;
         if (res) {
             json.Parse(res->body.c_str());
             if (json.HasParseError()) {
-                std::cout << "[BDSLiteloader] Failed to get updates(1)\n";
+                std::cout << "[Liteloader] Failed to get updates(1)\n";
                 return;
             }
             auto arr = json.GetArray();
             std::string LatestRelease = arr[arr.Size() - 1]["name"].GetString();
             int latestVersionNum = arr[arr.Size() - 1]["versionNum"].GetInt();
             if (latestVersionNum > LiteLoaderVersionNum) {
-                std::cout << "[BDSLiteloader] Found a new version: " << LatestRelease << "\n";
+                std::cout << "[Liteloader] Found a new version: " << LatestRelease << "\n";
             }
             if (latestVersionNum == LiteLoaderVersionNum) {
             }
             if (latestVersionNum < LiteLoaderVersionNum) {
-                std::cout << "[BDSLiteloader] Using preview version: " << LatestRelease << "\n";
+                std::cout << "[Liteloader] Using preview version: " << LatestRelease << "\n";
             }
         }
         else
         {
-            std::cout << "[BDSLiteloader] Failed to get updates(0)\n";
+            std::cout << "[Liteloader] Failed to get updates(0)\n";
         }
         });
     t.detach();
