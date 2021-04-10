caadda4f97d7ddd083ba4ed79ac84c3f6fdcb991
DreamGuXiang <166664660@qq.com>
2021-04-10 18:45:49 +0800

    add relativefloat

---
+++ LiteLoader/RelativeFloat.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class RelativeFloat {
+public:
+    float value;
+    bool is_relative;
+
+    RelativeFloat(float value) : value(value), is_relative(true) {}
+
+    inline float getFloat(float center) const {
+        if (is_relative) return center + value;
+        return value;
+    }
+};
