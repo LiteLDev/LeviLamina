09060cfa06e371a5ababcf105a8141c555235810
WangYneos <WangYneos@outlook.com>
2021-05-01 11:31:31 +0800

    Fix LocateService

--- headers/api/serviceLocate.h
+++ headers/api/serviceLocate.h
@@ -1,8 +1,8 @@
 ﻿#pragma once
 #include <liteloader.h>
-template<class T>
+template<typename T>
 struct LocateS {
-	LIAPI static T* _srv;
+	static T* _srv;
 	T& operator*() {
 		return *_srv;
 	}
@@ -19,3 +19,26 @@ struct LocateS {
 		_srv = (T*)&srv;
 	}
 };
+template <typename T> T* LocateService() {
+	return LocateS<T>::_srv;
+}
+
+struct RakPeer_t;
+struct WLevel;
+class CommandRegistry;
+class Level;
+class Minecraft;
+class MinecraftCommands;
+class RakNetServerLocator;
+class ServerLevel;
+class ServerNetworkHandler;
+
+template <> LIAPI RakPeer_t* LocateService<RakPeer_t>();
+template <> LIAPI WLevel* LocateService<WLevel>();
+template <> LIAPI CommandRegistry* LocateService<CommandRegistry>();
+template <> LIAPI Level* LocateService<Level>();
+template <> LIAPI Minecraft* LocateService<Minecraft>();
+template <> LIAPI MinecraftCommands* LocateService<MinecraftCommands>();
+template <> LIAPI RakNetServerLocator* LocateService<RakNetServerLocator>();
+template <> LIAPI ServerLevel* LocateService<ServerLevel>();
+template <> LIAPI ServerNetworkHandler* LocateService<ServerNetworkHandler>();
