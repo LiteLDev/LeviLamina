0414f0b6c26a3002887b45268b516594d281bac3
ShrBox <shrbox@outlook.com>
2021-06-25 15:28:13 +0800

    remove useless headers

--- headers/api/types/types.h
+++ headers/api/types/types.h
@@ -61,17 +61,17 @@ class ItemStack;
 
 struct WDim : Wrapped<Dimension> {
 	WDim(Dimension& x) : Wrapped<Dimension>(x) {}
-	LIAPI class BlockSource& getBlockSource_();
-	LIAPI void setBlock(int x, int y, int z, Block const& blk);
-	LIAPI struct WBlock getBlock(int x, int y, int z);
-	LIAPI int getID();
+	//LIAPI class BlockSource& getBlockSource_();
+	//LIAPI void setBlock(int x, int y, int z, Block const& blk);
+	//LIAPI struct WBlock getBlock(int x, int y, int z);
+	//LIAPI int getID();
 };
 struct WLevel : Wrapped<ServerLevel> {
 	WLevel() : Wrapped<ServerLevel>() {}
 	WLevel(ServerLevel& x) : Wrapped<ServerLevel>(x) {}
-	LIAPI array_view<WPlayer> getUsers();
-	LIAPI optional<WPlayer> getPlayer(string_view name);
-	LIAPI void broadcastText(string_view text, TextType type = RAW);
+	//LIAPI array_view<WPlayer> getUsers();
+	//LIAPI optional<WPlayer> getPlayer(string_view name);
+	//LIAPI void broadcastText(string_view text, TextType type = RAW);
 };
 struct WActor : Wrapped<Actor> {
 	WActor(Actor& x) : Wrapped<Actor>(x) {}
@@ -144,7 +144,7 @@ struct WBlockActor : Wrapped<BlockActor> {
 };
 struct WBlockSource :Wrapped<BlockSource> {
 	WBlockSource(BlockSource& x) : Wrapped<BlockSource>(x) {}
-	LIAPI WDim getDim();
+	//LIAPI WDim getDim();
 };
 struct WExplosion {
 	WBlockSource bs;
