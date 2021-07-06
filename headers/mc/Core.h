31e82bcc3c82f06c86aa2b3ba0d06bb7231d6ea0
dreamguxiang <166664660@qq.com>
2021-07-06 19:58:52 +0800

    add Vec3:toBlockPos,add Vec3:add,add BlockPos:add

--- headers/mc/Core.h
+++ headers/mc/Core.h
@@ -15,19 +15,6 @@ static inline int iround(float x) {
     return r;
 }
 
-class Vec3 {
-  public:
-    float x, y, z;
-    std::string toString() {
-        return "(" + std::to_string(x) + "," + std::to_string(y) + "," + std::to_string(z) + ")";
-    }
-    template <typename _TP>
-    void pack(WBStreamImpl<_TP> &ws) const {
-        ws.apply(x, y, z);
-    }
-    void unpack(RBStream &rs) { rs.apply(x, y, z); }
-};
-
 class BlockPos {
   public:
     int x, y, z;
@@ -41,7 +28,35 @@ class BlockPos {
         ws.apply(x, y, z);
     }
     void unpack(RBStream &rs) { rs.apply(x, y, z); }
+    inline BlockPos add(int dx, int dy, int dz) { return {x + dx, y + dy, z + dz}; }
 };
+
+class Vec3 {
+  public:
+    float x, y, z;
+    std::string toString() {
+        return "(" + std::to_string(x) + "," + std::to_string(y) + "," + std::to_string(z) + ")";
+    }
+    inline BlockPos toBlockPos() {
+        auto px = (int)x;
+        auto py = (int)y;
+        auto pz = (int)z;
+        if (px < 0)
+            px = px - 1;
+        if (px < 0)
+            py = py - 1;
+        if (pz < 0)
+            pz = pz - 1;
+        return {px, py, pz};
+    }
+    template <typename _TP>
+    void pack(WBStreamImpl<_TP> &ws) const {
+        ws.apply(x, y, z);
+    }
+    void unpack(RBStream &rs) { rs.apply(x, y, z); }
+    inline Vec3 add(float dx, float dy, float dz) { return {x + dx, y + dy, z + dz}; }
+};
+
 
 struct IVec2 {
     int x, z;
