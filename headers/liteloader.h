4521a3236f3046c0c6fd5bcbae0c9cee95f002e4
ShrBox <shrbox@outlook.com>
2021-07-14 22:26:43 +0800

    Release 1.1.0

--- headers/liteloader.h
+++ headers/liteloader.h
@@ -5,5 +5,5 @@
 #    define LIAPI __declspec(dllexport)
 #endif
 typedef unsigned char uchar;
-#define LiteLoaderVersion "1.0.3"
-#define LiteLoaderVersionNum (unsigned short)9
+#define LiteLoaderVersion "1.1.0"
+#define LiteLoaderVersionNum (unsigned short)10
