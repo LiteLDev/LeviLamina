83251d478b3a902f05790afe5c0a6c4427a0d369
ShrBox <shrbox@outlook.com>
2021-07-25 19:35:50 +0800

    Update LiteLoader.cpp & Version.cpp

--- LiteLoader/Version.cpp
+++ LiteLoader/Version.cpp
@@ -8,10 +8,14 @@ LIAPI std::string getLoaderVersion() {
 LIAPI unsigned short getLoaderVersionNum() {
     return LiteLoaderVersionNum;
 }
-}  // namespace version
+}  // namespace loaderapi
 
 THook(std::string,
       "?getServerVersionString@Common@@YA?AV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@"
       "@std@@XZ") {
+#ifdef LiteLoaderVersionGithub
+    return original() + " with LiteLoaderBDS " + LiteLoaderVersionGithub;
+#else
     return original() + " with LiteLoaderBDS " + LiteLoaderVersion;
+#endif
 }
