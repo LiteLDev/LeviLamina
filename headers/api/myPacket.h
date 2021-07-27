91be6d213ef3caf9a85afaec9f71a6b2a230adb9
qingyu <62042544+dreamguxiang@users.noreply.github.com>
2021-07-27 15:06:28 +0800

    fix mypakcet

--- headers/api/myPacket.h
+++ headers/api/myPacket.h
@@ -1,45 +1,72 @@
 ﻿#pragma once
+#include "loader/Loader.h"
 #include <stl/useful.h>
 #include <string>
 #include <string_view>
-#include "loader/Loader.h"
 using std::string_view;
+class StreamReadResult;
+class ReadOnlyBinaryStream;
 class BinaryStream {
-  public:
-    std::string &getRaw() {  // BinaryStream::getAndReleaseData
-        return *dAccess<std::string *, 96>(this);
+public:
+    std::string& getRaw() { // BinaryStream::getAndReleaseData
+        return *dAccess<std::string*, 96>(this);
     }
 };
+
+enum class PacketReliability {
+    Relible,
+    RelibleOrdered
+};
+
 class Packet {
-  public:
-    u32 unk2            = 2;  // 8
-    u32 reliableOrdered = 1;  // 12
-    u8 clientSubId      = 0;  // 16
-    u64 unk24_0         = 0;  // 24
-    u64 unk24_1         = 0;  // 32
-    u32 compressible    = 0;  // 40
-    inline Packet(unsigned compress) : compressible(compress) {}
-    inline Packet() {}
-    inline virtual ~Packet() {}
-    virtual int getId() const                = 0;
-    virtual std::string getName() const      = 0;
-    virtual void write(BinaryStream &) const = 0;
-    virtual void dummyread()                 = 0;
-    virtual bool disallowBatching() const    = 0;
+public:
+    unsigned          unk2            = 2;                                 // 8
+    PacketReliability reliableOrdered = PacketReliability::RelibleOrdered; // 12
+    unsigned char     clientSubId     = 0;                                 // 16
+    uint64_t          unk24           = 0;                                 // 24
+    uint64_t          unk40           = 0;                                 // 32
+    uint32_t          incompressible  = 0;                                 // 40
+    inline Packet(unsigned compress)
+        : incompressible(!compress) {
+    }
+    inline Packet() {
+    }
+    inline virtual ~Packet() {
+    }
+    virtual int         getId() const              = 0;
+    virtual std::string getName() const            = 0;
+    virtual void        write(BinaryStream&) const = 0;
+    virtual void        dummyread()                = 0;
+    virtual bool        disallowBatching() const   = 0;
 };
 static_assert(sizeof(Packet) == 48);
 
 template <int pid, bool batching = true, bool compress = true>
 class MyPkt : public Packet {
-  public:
+public:
     string_view view;
-    MyPkt() { compressible = compress; }
-    MyPkt(string_view sv) : view(sv) { compressible = compress; }
-    inline virtual ~MyPkt() {}
-    virtual int getId() const { return pid; }
-    virtual std::string getName() const { return "MyPkt"; }
-    virtual void write(BinaryStream &bs) const { bs.getRaw().append(view); }
-    virtual void dummyread() {}
-    virtual bool disallowBatching() const { return !batching; }
+    MyPkt() {
+        compressible = compress;
+    }
+    MyPkt(string_view sv)
+        : view(sv) {
+        incompressible = compress;
+    }
+    inline virtual ~MyPkt() {
+    }
+    virtual int getId() const {
+        return pid;
+    }
+    virtual std::string getName() const {
+        return "MyPkt";
+    }
+    virtual void write(BinaryStream& bs) const {
+        bs.getRaw().append(view);
+    }
+    virtual void dummyread() {
+    }
+    virtual bool disallowBatching() const {
+        return !batching;
+    }
 };
 static_assert(offsetof(MyPkt<0>, view) == 48);
