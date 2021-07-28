ccf76776c95a3b9b9cd74b17a78906836abe37ee
ShrBox <shrbox@outlook.com>
2021-07-28 19:47:35 +0800

    LiteLoader.rc

--- headers/liteloader.h
+++ headers/liteloader.h
@@ -6,8 +6,8 @@
 #    define LIAPI __declspec(dllexport)
 #endif
 typedef unsigned char uchar;
-#define LiteLoaderVersion "1.1.1"
-#define LiteLoaderVersionNum (unsigned short)11
+#define LiteLoaderVersion "1.1.2"
+#define LiteLoaderVersionNum (unsigned short)12
 
 namespace loaderapi {
 LIAPI std::string getLoaderVersion();
