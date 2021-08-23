85187b4aed8f03d20bf8cd3902bdbaaf136579bc
twoone3l <3197653242@qq.com>
2021-08-24 07:14:00 +0800

    fix build

--- LiteLoader/Version.cpp
+++ LiteLoader/Version.cpp
@@ -13,8 +13,8 @@ LIAPI unsigned short getLoaderVersionNum() {
 THook(std::string,
       "?getServerVersionString@Common@@YA?AV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@"
       "@std@@XZ") {
-#ifdef LiteLoaderVersionGithub
-    return original() + " with LiteLoaderBDS " + LiteLoaderVersionGithub;
+#ifdef LITELOADER_VERSION_GITHUB
+    return original() + " with LiteLoaderBDS " + LITELOADER_VERSION_GITHUB;
 #else
     return original() + " with LiteLoaderBDS " + LITELOADER_VERSION;
 #endif
