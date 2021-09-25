d13d60f6f3b7dfa8006d4f57b6b0e8a0d5d2b064
ShrBox <shrbox@outlook.com>
2021-09-25 15:21:50 +0800

    ServerCommands.cpp: Add server version check

--- LiteLoader/ServerCommands.cpp
+++ LiteLoader/ServerCommands.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 
+Logger<stdio_commit> LOG1(stdio_commit{"[LL] "});
+
 void checkUpdate();
 bool versionCommand(CommandOrigin const &, CommandOutput &outp) {
     outp.success("The server is running Bedrock Dedicated Server " + loaderapi::getServerVersion() + " with LiteLoaderBDS " +
@@ -10,6 +12,12 @@ bool versionCommand(CommandOrigin const &, CommandOutput &outp) {
 void registerCommands() {
     Event::addEventListener([](RegCmdEV ev) {  // Register commands
         CMDREG::SetCommandRegistry(ev.CMDRg);
+        std::string server_version = loaderapi::getServerVersion();
+        if (server_version.find("1.17.1") != std::string::npos) {
+            LOG1("Are you sure you want to run LiteLoader" + loaderapi::getLoaderVersion() + " with BDS " + server_version + "?");
+            system("pause");
+            return;
+        }
         MakeCommand("version", "Gets the version of this server", 0);
         CmdOverload(version, versionCommand);
     });
