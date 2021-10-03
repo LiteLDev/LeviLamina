9ff9b5ff2775d7cc79dceadc8a8df299e4a62f6e
ShrBox <shrbox@outlook.com>
2021-10-03 15:48:30 +0800

    Add FixListenPort

--- LiteLoader/BugFix.cpp
+++ LiteLoader/BugFix.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 
 bool isFixDisconnectBug();
+bool isFixListenPort();
 
 //Fix disconnect packet crash bug
 THook(void, "?handle@ServerNetworkHandler@@UEAAXAEBVNetworkIdentifier@@AEBVDisconnectPacket@@@Z", ServerNetworkHandler* thi, NetworkIdentifier* ni, void* packet) {
@@ -8,4 +9,19 @@ THook(void, "?handle@ServerNetworkHandler@@UEAAXAEBVNetworkIdentifier@@AEBVDisco
         return;
     }
     return original(thi, ni, packet);
+}
+//Fix the listening port twice
+//From https://github.com/Redbeanw44602/FixIPLogger
+bool a_call = true;
+THook(__int64, "?LogIPSupport@RakPeerHelper@@AEAAXXZ",
+      void* _this) {
+    if (isFixListenPort()) {
+        if (a_call) {
+            a_call = false;
+            return original(_this);
+        }
+        return 0;
+    } else {
+        return original(_this);
+    }
 }
