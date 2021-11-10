151dc79d0d23bcfbe15cce58fc80b8a3877f75e1
qingyu <62042544+dreamguxiang@users.noreply.github.com>
2021-11-10 11:26:26 +0800

    change log

--- LiteLoader/ModifyName.cpp
+++ LiteLoader/ModifyName.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <LLAPI.h>
 #include <ServerAPI.h>
+#include <regex>
 //#include <MCApi/BedrockLog.hpp>
 using namespace std;
 
@@ -10,14 +11,23 @@ THook(std::string, "?getServerVersionString@Common@@YA?AV?$basic_string@DU?$char
     return original() + "(ProtocolVersion " + to_string(LL::getServerProtocolVersion()) + ") with LiteLoaderBDS " + LL::getLoaderVersionString();
 }
 
-/*
-THook(void, "?BedrockLogOut@@YAXIPEBDZZ",
-      unsigned int a1, const char* a2, ...) {
-    va_list va;
-    string  mode = a2;
-    va_start(va, a2);
-    auto    ts    = va_arg(va, __int64);
-    string  input = (char*)ts;
-    std::cout << input << std::endl;
+string& replace_all_distinct(string& str, const string& old_value, const string& new_value) {
+    for (string::size_type pos(0); pos != string::npos; pos += new_value.length()) {
+        if ((pos = str.find(old_value, pos)) != string::npos)
+            str.replace(pos, old_value.length(), new_value);
+        else
+            break;
+    }
+    return str;
 }
-*/
+
+THook(void, "?PlatformBedrockLogOut@@YAXIPEBD@Z",
+      int, const char* ts) {
+    string      input  = ts;
+    std::string output = std::regex_replace(input, std::regex("\\[.*?\\]"), std::string("$1"));
+    output.erase(std::remove(output.begin(), output.end(), '\n'),
+                 output.end());
+    output.erase(output.find_first_of(' '), output.find_first_not_of(' '));
+    output = replace_all_distinct(output, "NO LOG FILE! -  ", "");
+    Logger::Info() << output << Logger::endl;
+}
