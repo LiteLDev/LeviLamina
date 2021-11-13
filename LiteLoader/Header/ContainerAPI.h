ee9d802e5ca7b53c27bc7c6bbb20754db240f378
yqs112358 <1425321705@qq.com>
2021-11-13 16:06:39 +0800

    change fucking clang format

--- LiteLoader/Header/ContainerAPI.h
+++ LiteLoader/Header/ContainerAPI.h
@@ -6,13 +6,13 @@
 struct ContainerObj : public Container {
     LIAPI std::string getTypeName();
 
-    LIAPI bool               addItem(ItemStack* item);
-    LIAPI bool               addItemToFirstEmptySlot(ItemStack* item);
-    LIAPI bool               removeItem(int slot, unsigned int number);
+    LIAPI bool addItem(ItemStack* item);
+    LIAPI bool addItemToFirstEmptySlot(ItemStack* item);
+    LIAPI bool removeItem(int slot, unsigned int number);
     LIAPI ItemStack* getSlot(int slot);
     LIAPI std::vector<const ItemStack*> getAllSlots();
-    LIAPI int                       getContainerSize();
+    LIAPI int getSize();
 
-    LIAPI bool  hasContainer(FloatVec4 pos);
+    LIAPI bool hasContainer(FloatVec4 pos);
     LIAPI Container* getContainer(FloatVec4 pos);
 };
