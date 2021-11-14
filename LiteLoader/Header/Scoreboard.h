46c92ce9643ffeca06b19582a7845cd677eb60f6
qingyu <62042544+dreamguxiang@users.noreply.github.com>
2021-11-14 16:20:15 +0800

    add symdb2

--- LiteLoader/Header/Scoreboard.h
+++ LiteLoader/Header/Scoreboard.h
@@ -15,25 +15,25 @@ struct ScoreInfo {
 
 struct PlayerScore {};
 struct ScoreboardObj : public Scoreboard {
-    LIAPI Objective* newObjective(const std::string& objname, const std::string& displayName);
-    LIAPI bool setDisplayObjective(const std::string& objname, const std::string& slot, int sort);
-    LIAPI Objective* clearDisplayObjective(const std::string& slot);
-    LIAPI Objective* getDisplayObjective(const std::string& slot);
-    LIAPI bool removeFromObjective(const std::string& objname, const std::string& id);
-    LIAPI bool removeFromObjective(const std::string& objname, Player* player);
+    LIAPI static Objective* newObjective(const std::string& objname, const std::string& displayName);
+    LIAPI static bool setDisplayObjective(const std::string& objname, const std::string& slot, int sort);
+    LIAPI static Objective* clearDisplayObjective(const std::string& slot);
+    LIAPI static Objective* getDisplayObjective(const std::string& slot);
+    LIAPI static bool removeFromObjective(const std::string& objname, const std::string& id);
+    LIAPI static bool removeFromObjective(const std::string& objname, Player* player);
 
-    LIAPI int getScore(const std::string& objname, const std::string& id);
-    LIAPI std::optional<int> setScore(const std::string& objname, const std::string& id, int score);
-    LIAPI std::optional<int> addScore(const std::string& objname, const std::string& id, int score);
-    LIAPI std::optional<int>reduceScore(const std::string& objname, const std::string& id, int score);
+    LIAPI static int getScore(const std::string& objname, const std::string& id);
+    LIAPI static std::optional<int> setScore(const std::string& objname, const std::string& id, int score);
+    LIAPI static std::optional<int> addScore(const std::string& objname, const std::string& id, int score);
+    LIAPI static std::optional<int> reduceScore(const std::string& objname, const std::string& id, int score);
 
-    LIAPI int getScore(Player* player, const std::string& key);
-    LIAPI bool setScore(Player* player, const std::string& key, int value);
-    LIAPI bool addScore(Player* player, const std::string& key, int value);
-    LIAPI bool reduceScore(Player* player, const std::string& key, int value);
-    LIAPI bool deleteScore(Player* player, const std::string& objname);
+    LIAPI static int getScore(Player* player, const std::string& key);
+    LIAPI static bool setScore(Player* player, const std::string& key, int value);
+    LIAPI static bool addScore(Player* player, const std::string& key, int value);
+    LIAPI static bool reduceScore(Player* player, const std::string& key, int value);
+    LIAPI static bool deleteScore(Player* player, const std::string& objname);
 
-    LIAPI bool scoreboardIdIsValid(ScoreboardId* id);
+    LIAPI static bool scoreboardIdIsValid(ScoreboardId* id);
 };
 ScoreboardObj* globalScoreBoard = nullptr;
 THook(ScoreboardObj*, "??0ServerScoreboard@@QEAA@VCommandSoftEnumRegistry@@PEAVLevelStorage@@@Z",
