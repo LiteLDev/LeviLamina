eb4f3892e28bba4c214c8a7e7c31332c5c06b662
ShrBox <shrbox@outlook.com>
2021-12-05 17:15:23 +0800

    Update leveldb

--- LiteLoader/Header/third-party/leveldb/filter_policy.h
+++ LiteLoader/Header/third-party/leveldb/filter_policy.h
@@ -25,29 +25,30 @@ namespace leveldb {
 class Slice;
 
 class LEVELDB_EXPORT FilterPolicy {
-  public:
-    virtual ~FilterPolicy();
+ public:
+  virtual ~FilterPolicy();
 
-    // Return the name of this policy.  Note that if the filter encoding
-    // changes in an incompatible way, the name returned by this method
-    // must be changed.  Otherwise, old incompatible filters may be
-    // passed to methods of this type.
-    virtual const char *Name() const = 0;
+  // Return the name of this policy.  Note that if the filter encoding
+  // changes in an incompatible way, the name returned by this method
+  // must be changed.  Otherwise, old incompatible filters may be
+  // passed to methods of this type.
+  virtual const char* Name() const = 0;
 
-    // keys[0,n-1] contains a list of keys (potentially with duplicates)
-    // that are ordered according to the user supplied comparator.
-    // Append a filter that summarizes keys[0,n-1] to *dst.
-    //
-    // Warning: do not change the initial contents of *dst.  Instead,
-    // append the newly constructed filter to *dst.
-    virtual void CreateFilter(const Slice *keys, int n, std::string *dst) const = 0;
+  // keys[0,n-1] contains a list of keys (potentially with duplicates)
+  // that are ordered according to the user supplied comparator.
+  // Append a filter that summarizes keys[0,n-1] to *dst.
+  //
+  // Warning: do not change the initial contents of *dst.  Instead,
+  // append the newly constructed filter to *dst.
+  virtual void CreateFilter(const Slice* keys, int n,
+                            std::string* dst) const = 0;
 
-    // "filter" contains the data appended by a preceding call to
-    // CreateFilter() on this class.  This method must return true if
-    // the key was in the list of keys passed to CreateFilter().
-    // This method may return true or false if the key was not on the
-    // list, but it should aim to return false with a high probability.
-    virtual bool KeyMayMatch(const Slice &key, const Slice &filter) const = 0;
+  // "filter" contains the data appended by a preceding call to
+  // CreateFilter() on this class.  This method must return true if
+  // the key was in the list of keys passed to CreateFilter().
+  // This method may return true or false if the key was not on the
+  // list, but it should aim to return false with a high probability.
+  virtual bool KeyMayMatch(const Slice& key, const Slice& filter) const = 0;
 };
 
 // Return a new filter policy that uses a bloom filter with approximately
@@ -64,7 +65,7 @@ class LEVELDB_EXPORT FilterPolicy {
 // ignores trailing spaces, it would be incorrect to use a
 // FilterPolicy (like NewBloomFilterPolicy) that does not ignore
 // trailing spaces in keys.
-LEVELDB_EXPORT const FilterPolicy *NewBloomFilterPolicy(int bits_per_key);
+LEVELDB_EXPORT const FilterPolicy* NewBloomFilterPolicy(int bits_per_key);
 
 }  // namespace leveldb
 
