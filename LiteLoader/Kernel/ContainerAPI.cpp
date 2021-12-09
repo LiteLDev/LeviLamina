7576f192dab240d0542ce9205a27df7757968695
yqs112358 <1425321705@qq.com>
2021-12-09 17:03:33 +0800

    Update ContainerAPI.cpp

--- LiteLoader/Kernel/ContainerAPI.cpp
+++ LiteLoader/Kernel/ContainerAPI.cpp
@@ -5,39 +5,39 @@
 
 LIAPI std::string Container::getTypeName() {
     //ContainerType type = dAccess<ContainerType>(this, 8); //IDA Container::Container
-    ContainerType type = this->getContainerType();
-    return this->getContainerTypeName(type);
+    ContainerType type = getContainerType();
+    return getContainerTypeName(type);
 }
 
 //Safely add items to the container
 LIAPI bool Container::addItem(ItemStack* item) {
-    if (!this->hasRoomForItem(*item))
+    if (!hasRoomForItem(*item))
         return false;
-    ((Container*)this)->addItem(*((ItemStack*)item)->clone_s());
+   addItem(*((ItemStack*)item)->clone_s());
     return true;
 }
 
 LIAPI bool Container::addItemToFirstEmptySlot(ItemStack* item) {
-    return ((Container*)this)->addItemToFirstEmptySlot(*((ItemStack*)item)->clone_s());
+    return addItemToFirstEmptySlot(*((ItemStack*)item)->clone_s());
 
 }
 //Safely remove items to the container
 LIAPI bool Container::removeItem(int slot, unsigned int number) {
-    if (slot > this->getSize())
+    if (slot > getSize())
         return false;
-    ((Container*)this)->removeItem(slot, number);
+    removeItem(slot, number);
     return true;
 }
 
 LIAPI ItemStack* Container::getSlot(int slot) {
-    if (slot > this->getSize())
+    if (slot > getSize())
         return nullptr;
-    auto list = this->getAllSlots();
+    auto list = getAllSlots();
     return const_cast<ItemStack*>(list[slot]);
 }
 
 LIAPI std::vector<const ItemStack*> Container::getAllSlots() {
-    return this->getSlots();
+    return getSlots();
 }
 
 LIAPI int Container::getSize() {
@@ -52,8 +52,8 @@ LIAPI bool Container::hasContainer(Vec3& pos, int dim) {
 class DropperBlockActor;
 LIAPI Container* Container::getContainer(Vec3& pos, int dim) {
     Vec3 vec{pos.x, pos.y, pos.z};
+
     // This function didn't use 'this' pointer
-    
     Container* container = SymCall("?_getContainerAt@DropperBlockActor@@AEAAPEAVContainer@@AEAVBlockSource@@AEBVVec3@@@Z",
                                    Container*, DropperBlockActor*, BlockSource*, Vec3*)(nullptr, Level::getBlockSource(dim), &vec);
 
