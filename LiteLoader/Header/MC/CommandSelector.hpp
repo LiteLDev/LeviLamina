86987a1366ebc702b3b87b4dd82fdcf871d97dbb
xiaoqch <xiaoqch@foxmail.com>
2021-12-09 16:18:14 +0800

    fix commandselector

--- LiteLoader/Header/MC/CommandSelector.hpp
+++ LiteLoader/Header/MC/CommandSelector.hpp
@@ -7,7 +7,7 @@ class CommandSelector : public CommandSelectorBase {
 public:
     LIAPI CommandSelector() : CommandSelectorBase(std::is_same_v<T, Player>) {}
 
-    inline CommandSelectorResults<T> results(CommandOrigin const& a0)
+    inline CommandSelectorResults<T> results(CommandOrigin const& a0) const
     {
         CommandSelectorResults<T>(CommandSelector<T>:: * rv)(CommandOrigin const& a0);
         if constexpr (std::is_same_v<class Actor, T>) {
