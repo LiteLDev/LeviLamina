10079c024fa21a7097af00889c886d4d64808c85
RimuruChan <179793046@qq.com>
2021-12-12 20:25:06 +0800

    default logger no title and fixed warning

--- LiteLoader/LiteLoader.cpp
+++ LiteLoader/LiteLoader.cpp
@@ -75,7 +75,6 @@ void LLMain() {
     FixPluginsLibDir();
 
     //Init LL Logger
-    PluginOwnData::set<string>(LOGGER_DEFAULT_TITLE, "LiteLoader");
     Logger::setFile("logs/LiteLoader-latest.log", false);
 
     //Load Config
--- LiteLoader/ModifyInfomation.cpp
+++ LiteLoader/ModifyInfomation.cpp
@@ -67,7 +67,7 @@ THook(void*, "?send@CommandOutputSender@@UEAAXAEBVCommandOrigin@@AEBVCommandOutp
         while (getline(iss, line)) {
             str.erase(str.find_last_of('\n'), str.find_last_not_of('\n'));
             size_t pos = 0;
-            while ((pos = str.find('§')) != string::npos) {
+            while ((pos = str.find("§")) != string::npos) {
                 str.erase(pos, 2);
             }
             serverLogger.info << line << Logger::endl;
