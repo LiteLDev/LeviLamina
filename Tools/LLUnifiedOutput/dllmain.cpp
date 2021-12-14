1a37af688c4ed2db41c5f66b80b1d123b91599c0
qingyu <62042544+dreamguxiang@users.noreply.github.com>
2021-12-14 21:13:27 +0800

    add LLUnifiedOutput

---
+++ Tools/LLUnifiedOutput/dllmain.cpp
@@ -0,0 +1,162 @@
+﻿#include "pch.h"
+#include <LoggerAPI.h>
+#include <EventAPI.h>
+
+bool is(std::string str) {
+	transform(str.begin(), str.end(), str.begin(), ::tolower);
+
+	if (str.find(" info][") != str.npos) {
+		return true;
+	}
+	else    if (str.find(" debug][") != str.npos) {
+		return true;
+	}
+	else    if (str.find(" error][") != str.npos) {
+		return true;
+	}
+	else     if (str.find(" warn][") != str.npos) {
+		return true;
+	}
+	else if (str.find(" fatal][") != str.npos) {
+		return true;
+	}
+	return false;
+}
+
+
+//////////// Hook Data ////////////
+HANDLE hConsole;
+
+BOOL(WINAPI* WriteFile_Ptr)(HANDLE hFile, LPCVOID lpBuffer,
+	DWORD   nNumberOfBytesToWrite, LPDWORD lpNumberOfBytesWritten,
+	LPOVERLAPPED lpOverlapped) = WriteFile;
+
+
+//////////// Process ////////////
+//extern bool OutputProcess(std::string& line);
+std::ostringstream buffer;
+
+bool inline SendLine(HANDLE hOutput)
+{
+	DWORD dwOutput;
+	std::string line = buffer.str();
+	bool success = true;
+
+	if (!is(line))
+	{
+		if (line.find("\n") != line.npos) {
+			line = "\b\b" + line;
+			success = WriteFile_Ptr(hOutput, line.data(), line.size(), &dwOutput, NULL);
+		}
+		else {
+			buffer.str("");
+			buffer.clear();
+			Logger("Plugin").info(line);
+		}
+	}else {
+		line = "\b\b" + line + "> ";
+		success = WriteFile_Ptr(hOutput, line.data(), line.size(), &dwOutput, NULL);
+	}
+	buffer.str("");
+	buffer.clear();
+	return success;
+}
+
+void inline AddText(const char* text, size_t len)
+{
+	buffer.write(text, len);
+}
+
+BOOL WINAPI WriteFile_Process(HANDLE hFile, LPCVOID lpBuffer,
+	DWORD   nNumberOfBytesToWrite, LPDWORD lpNumberOfBytesWritten,
+	LPOVERLAPPED lpOverlapped)
+{
+	if (hFile == hConsole)
+	{
+		char* fromPos = (char*)lpBuffer;
+		int nowLen = 0;
+		for (size_t i = 0; i < nNumberOfBytesToWrite; ++i)
+		{
+			if (fromPos[nowLen++] == '\n')
+			{
+				AddText(fromPos, nowLen);
+				SendLine(hFile);
+				fromPos += nowLen;
+				nowLen = 0;
+			}
+		}
+		if (nowLen > 0)
+			AddText(fromPos, nowLen);
+		*lpNumberOfBytesWritten = nNumberOfBytesToWrite;
+		return TRUE;
+	}
+	else
+		return WriteFile_Ptr(hFile, lpBuffer, nNumberOfBytesToWrite,
+			lpNumberOfBytesWritten, lpOverlapped);
+}
+
+//////////// Init ////////////
+bool UnHook();
+
+bool Hook()
+{
+	Event::ServerStoppedEvent::subscribe([](Event::ServerStoppedEvent) {
+		UnHook();
+		putchar('\b');
+		putchar('\b');
+		putchar(' ');
+		putchar(' ');
+		putchar('\b');
+		putchar('\b');
+		return true;
+		});
+	bool success = true;
+	if ((__int64)(hConsole = GetStdHandle(STD_OUTPUT_HANDLE)) <= 0)
+		success = false;
+	else
+	{
+		DetourRestoreAfterWith();
+		if (DetourTransactionBegin() != NO_ERROR)
+			success = false;
+		else if (DetourUpdateThread(GetCurrentThread()) != NO_ERROR)
+			success = false;
+		else if (DetourAttach((PVOID*)&WriteFile_Ptr, WriteFile_Process) != NO_ERROR)
+			success = false;
+		else if (DetourTransactionCommit() != NO_ERROR)
+			success = false;
+	}
+	return success;
+}
+
+bool UnHook() {
+	bool success = true;
+	if (DetourTransactionBegin() != NO_ERROR)
+		success = false;
+	else if (DetourUpdateThread(GetCurrentThread()) != NO_ERROR)
+		success = false;
+	else if (DetourDetach((PVOID*)&WriteFile_Ptr, WriteFile_Process) != NO_ERROR)
+		success = false;
+	else if (DetourTransactionCommit() != NO_ERROR)
+		success = false;
+	return success;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		if (!Hook())
+			return false;
+		break;
+	case DLL_THREAD_ATTACH:
+		break;
+	case DLL_THREAD_DETACH:
+		break;
+	case DLL_PROCESS_DETACH:
+		UnHook();
+		break;
+	}
+	return TRUE;
+}
+
