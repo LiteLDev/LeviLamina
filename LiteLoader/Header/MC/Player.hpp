24877e770deaa31931a4424fd2f0d6e05ccc0f5d
xiaoqch <xiaoqch@foxmail.com>
2021-12-20 21:51:08 +0800

    remove getUserEntityIdentifierComponent in Player and Mob

--- LiteLoader/Header/MC/Player.hpp
+++ LiteLoader/Header/MC/Player.hpp
@@ -39,7 +39,6 @@ public:
     LIAPI std::string getIP();
     LIAPI string getLanguageCode();
     LIAPI string getServerAddress();
-    LIAPI UserEntityIdentifierComponent* getUserEntityIdentifierComponent();
     LIAPI NetworkIdentifier* getNetworkIdentifier();
     LIAPI Certificate* getCertificate();
     LIAPI Container* getEnderChestContainer();
--- LiteLoader/Kernel/MobAPI.cpp
+++ LiteLoader/Kernel/MobAPI.cpp
@@ -5,10 +5,6 @@
 #include <MC/ActorDefinitionIdentifier.hpp>
 #include <MC/PathNavigation.hpp>
 
-UserEntityIdentifierComponent* Mob::getUserEntityIdentifierComponent() {
-    return ((Actor*)this)->getUserEntityIdentifierComponent();
-}
-
 bool Mob::refreshInventory() {
     sendInventory(true);
     std::bitset<4> bits("1111");
