ba4f6a9d4629068cb8b1c8137aa1645717fc790f
ShrBox <53301243+ShrBox@users.noreply.github.com>
2021-12-22 19:59:06 +0800

    Update PackSDK.cmd
    remove pack pdb

--- PackSDK.cmd
+++ PackSDK.cmd
@@ -1,7 +1,6 @@
 @echo off
 
 rmdir SDK\Header /S /Q 2>nul
-rmdir SDK\Pdb /S /Q 2>nul
 rmdir SDK\Lib /S /Q 2>nul
 rmdir SDK\Tools /S /Q 2>nul
 
@@ -9,7 +8,6 @@ echo [INFO] Packing LL-SDK...
 echo.
 
 mkdir SDK\Header 2>nul
-mkdir SDK\Pdb 2>nul
 mkdir SDK\Lib 2>nul
 mkdir SDK\Tools 2>nul
 mkdir SDK\Tools\llvm-dlltool-msys2 2>nul
@@ -18,7 +16,6 @@ xcopy LiteLoader\Header\* SDK\Header /E /Q /Y
 
 copy /Y LiteLoader\Lib\SymDBHelper.lib SDK\Lib
 copy /Y x64\Release\*.lib SDK\Lib
-copy /Y x64\Release\*.pdb SDK\Pdb
 
 xcopy Tools\llvm-dlltool-msys2\* SDK\Tools\llvm-dlltool-msys2 /E /Q /Y
 copy /Y Tools\LibraryBuilder.exe SDK\Tools
