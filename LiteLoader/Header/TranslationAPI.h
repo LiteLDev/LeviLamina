dd5182a799336a5535fe57142834e02deb7ef38e
ShrBox <shrbox@outlook.com>
2022-01-10 22:06:38 +0800

    Update SDK

--- LiteLoader/Header/TranslationAPI.h
+++ LiteLoader/Header/TranslationAPI.h
@@ -47,8 +47,8 @@ inline std::string trImpl(HMODULE hPlugin, const S& formatStr, const Args&... ar
 
     auto res = json.find(formatStr); //改成模糊匹配
     if (res == json.end()) {
-        logger.error("Fail to find translation string \"{}\" !", formatStr);
-        logger.error("In file {}", PluginOwnData::getImpl<std::string>(hPlugin, TRANSLATION_DATA_FILE));
+        LL::logger.error("Fail to find translation string \"{}\" !", formatStr);
+        LL::logger.error("In file {}", PluginOwnData::getImpl<std::string>(hPlugin, TRANSLATION_DATA_FILE));
     } else {
         realFormatStr = res.value();
     }
