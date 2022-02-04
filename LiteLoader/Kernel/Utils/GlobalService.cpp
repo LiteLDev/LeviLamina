75f9502877c313cd9812b50838b823efecefbfb0
Jasonzyt <2126931871@qq.com>
2022-02-04 13:34:59 +0800

    Update GlobalService.cpp
    Use `TInstanceHook`

--- LiteLoader/Kernel/Utils/GlobalService.cpp
+++ LiteLoader/Kernel/Utils/GlobalService.cpp
@@ -1,66 +1,87 @@
 #include <Global.h>
 #include <MC/Level.hpp>
 #include <MC/Minecraft.hpp>
+#include <MC/MinecraftCommands.hpp>
+#include <MC/DBStorage.hpp>
+#include <MC/RakNetServerLocator.hpp>
+#include <MC/RakNet.hpp>
+#include <MC/Scoreboard.hpp>
+#include <MC/AllowListFile.hpp>
 #include <GlobalServiceAPI.h>
 
-THook(void, "?initAsDedicatedServer@Minecraft@@QEAAXXZ", Minecraft* mc) {
-    Global<Minecraft> = mc;
-    original(mc);
-}
-
-//?initCoreEnums@MinecraftCommands@@QEAAX_NAEBVBaseGameVersion@@@Z
-THook(void, "?initCoreEnums@MinecraftCommands@@QEAAXAEBVIWorldRegistriesProvider@@AEBVActorFactory@@AEBVExperiments@@AEBVBaseGameVersion@@@Z",
-      MinecraftCommands* a0, void* a1, void* a2, void* a3, void* a4) {
-    original(a0, a1, a2, a3, a4);
-    Global<MinecraftCommands> = a0;
-}
-
-THook(DBStorage*, "??0DBStorage@@QEAA@UDBStorageConfig@@@Z",
-      DBStorage* a, struct DBStorageConfig* config)
+// Minecraft
+TInstanceHook(void, "?initAsDedicatedServer@Minecraft@@QEAAXXZ", Minecraft)
 {
-    auto rtn = original(a, config);
-    Global<LevelStorage> = (LevelStorage*)a;
-    Global<DBStorage> = a;
-    return rtn;
+    Global<Minecraft> = this;
+    original(this);
+}
+
+// MinecraftCommands
+// ?initCoreEnums@MinecraftCommands@@QEAAX_NAEBVBaseGameVersion@@@Z
+TInstanceHook(void, "?initCoreEnums@MinecraftCommands@@QEAAXAEBVIWorldRegistriesProvider@@AEBVActorFactory@@AEBVExperiments@@AEBVBaseGameVersion@@@Z",
+      MinecraftCommands, void* a1, void* a2, void* a3, void* a4)
+{
+    original(this, a1, a2, a3, a4);
+    Global<MinecraftCommands> = this;
+}
+
+// LevelStorage & DBStorage
+TInstanceHook(DBStorage*, "??0DBStorage@@QEAA@UDBStorageConfig@@@Z",
+      DBStorage, struct DBStorageConfig* config)
+{
+    auto ret = original(this, config);
+    Global<LevelStorage> = (LevelStorage*)this;
+    Global<DBStorage> = this;
+    return ret;
 }
 
 #if 0
-THook(void*, "??0ChunkSource@@QEAA@V?$unique_ptr@VChunkSource@@U?$default_delete@VChunkSource@@@std@@@std@@@Z", ChunkSource* a1, void** a2) {
+// ChunkSource
+THook(void*, "??0ChunkSource@@QEAA@V?$unique_ptr@VChunkSource@@U?$default_delete@VChunkSource@@@std@@@std@@@Z", ChunkSource* a1, void** a2) 
+{
 	Global<ChunkSource> = a1);
 	return original(a1, a2);
 }
 #endif
 
-//?activate@RakNetServerLocator@@AEAAXXZ
-THook(void*, "?_activate@RakNetServerLocator@@AEAAXXZ", RakNetServerLocator* thi) {
-    static bool inited = false;
-    if (!inited)
-        Global<RakNetServerLocator> = thi, inited = true;
-    return original(thi);
-}
-
-THook(void*, "??0RakPeer@RakNet@@QEAA@XZ", RakNet::RakPeer* p) {
-    static bool inited = false;
-    if (!inited) {
-        inited = 1;
-        Global<RakNet::RakPeer> = p;
+// RakNetServerLocator
+// ?activate@RakNetServerLocator@@AEAAXXZ
+TInstanceHook(void*, "?_activate@RakNetServerLocator@@AEAAXXZ", RakNetServerLocator)
+{
+    static bool set = false;
+    if (!set)
+    {
+        set = true;
+        Global<RakNetServerLocator> = this;
     }
-    return original(p);
+    return original(this);
 }
-
-THook(Scoreboard*, "??0ServerScoreboard@@QEAA@VCommandSoftEnumRegistry@@PEAVLevelStorage@@@Z",
-      Scoreboard* _this, void* a2, void* a3) {
-    Scoreboard* sc = original(_this, a2, a3);
+// RakNet::RakPeer
+TInstanceHook(void*, "??0RakPeer@RakNet@@QEAA@XZ", RakNet::RakPeer)
+{
+    static bool set = false;
+    if (!set)
+    {
+        set = true;
+        Global<RakNet::RakPeer> = this;
+    }
+    return original(this);
+}
+// Scoreboard
+TInstanceHook(Scoreboard*, "??0ServerScoreboard@@QEAA@VCommandSoftEnumRegistry@@PEAVLevelStorage@@@Z",
+      Scoreboard, void* a2, void* a3)
+{
+    Scoreboard* sc = original(this, a2, a3);
     Global<Scoreboard> = sc;
     return sc;
 }
-
-THook(int, "?reload@AllowListFile@@QEAA?AW4FileReadResult@@XZ", AllowListFile* _this)
+// AllowListFile
+TInstanceHook(int, "?reload@AllowListFile@@QEAA?AW4FileReadResult@@XZ", AllowListFile)
 {
     static bool set = false;
     if (!set) {
-        Global<AllowListFile> = _this;
+        Global<AllowListFile> = this;
         set = true;
     }
-    return original(_this);
+    return original(this);
 }
