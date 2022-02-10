aec0557331ee716e82e53873c42b286b175bec11
qingyu <62042544+dreamguxiang@users.noreply.github.com>
2022-02-10 17:29:42 +0800

    Update Config.cpp

--- LiteLoader/Main/Config.cpp
+++ LiteLoader/Main/Config.cpp
@@ -125,6 +125,11 @@ namespace LL {
                 const nlohmann::json& setting = modules.at("ErrorStackTraceback");
                 conf.enableErrorStackTraceback = setting.value("enabled", true);
             }
+            if (modules.find("WelcomeText") != modules.end())
+            {
+                const nlohmann::json& setting = modules.at("WelcomeText");
+                conf.enableWelcomeText = setting.value("enabled", true);
+            }
         }
     }
 } // namespace LL
