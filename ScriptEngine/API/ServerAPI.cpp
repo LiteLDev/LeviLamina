9fac9edb7775a48fb011ccafec911fd95b9c7408
StarsDream00 <starsdream00@icloud.com>
2022-03-18 11:41:54 +0800

    add mc.setMaxPlayers(num)

--- ScriptEngine/API/ServerAPI.cpp
+++ ScriptEngine/API/ServerAPI.cpp
@@ -20,8 +20,18 @@ Local<Value> McClass::setMotd(const Arguments& args)
 Local<Value> McClass::crashBDS(const Arguments& args)               //===========???
 {
     RecordOperation(ENGINE_OWN_DATA()->pluginName, "Crash Server", "Execute mc.crash() to crash server.");
-    char* c = new char[10];
-    delete c;
-    delete c;
+    throw;
     return Boolean::newBoolean(true);
+}
+
+Local<Value> McClass::setMaxNumPlayers(const Arguments& args)
+{
+    CHECK_ARGS_COUNT(args, 1)
+    CHECK_ARG_TYPE(args[0], ValueKind::kNumber)
+
+    try
+    {
+        return Boolean::newBoolean(Global<ServerNetworkHandler>->setMaxNumPlayers(args[0].asNumber().toInt32()));
+    }
+    CATCH("Fail in setMaxPlayers!")
 }
