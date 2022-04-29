0b4d85d663cd4070a8a75978874c5597d1e1a1e1
xiaoqch <xiaoqch@foxmail.com>
2022-04-29 21:35:52 +0800

    fix version

--- ScriptEngine/resource.h
+++ ScriptEngine/resource.h
@@ -1,25 +1,12 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ 生成的包含文件。
-// 供 ScriptEngine.rc 使用
-//
-#define LITELOADER_VERSION_DEV          0
-#define LITELOADER_VERSION_ACTIONS      0
-#define _STL_COMPILER_PREPROCESSOR      0
-#define LITELOADER_VERSION_BETA         1
-#define LITELOADER_VERSION_RELEASE      2
-#define LITELOADER_VERSION_MAJOR        2
-#define LITELOADER_VERSION_MINOR        2
-#define LITELOADER_VERSION_REVISION     2
-#define LLSE_POOL_THREAD_COUNT          4
-#define LLSE_MESSAGE_SYSTEM_WAIT_CHECK_INTERVAL 5
+// Microsoft Visual C++ generated include file.
+// Used by LiteLoader.rc
 
-// Next default values for new objects
-// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
+#define _APS_NEXT_RESOURCE_VALUE 101
+#define _APS_NEXT_COMMAND_VALUE 40001
+#define _APS_NEXT_CONTROL_VALUE 1001
+#define _APS_NEXT_SYMED_VALUE 101
 #endif
 #endif
