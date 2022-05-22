a3ce9f949f70f1e77df0707912c84506254ca966
WangYneos <wangyneos@outlook.com>
2022-05-22 09:28:34 +0800

    BlockPos Hash

--- LiteLoader/Header/MC/BlockPos.hpp
+++ LiteLoader/Header/MC/BlockPos.hpp
@@ -74,3 +74,20 @@ public:
     LIAPI Vec3 center() const;
 
 };
+
+namespace std {
+
+    template <>
+    struct hash<BlockPos> {
+        std::size_t operator()(BlockPos const& pos) const noexcept {
+            //??$hash3@HHH@Math@mce@@SA_KAEBH00@Z
+            unsigned __int64 t1; // r8
+            unsigned __int64 t2; // r8
+
+            t1 = *((unsigned __int8*)&pos.x + 3) ^ (0x100000001B3i64 * (*((unsigned __int8*)&pos.x + 2) ^ (0x100000001B3i64 * (*((unsigned __int8*)&pos.x + 1) ^ (0x100000001B3i64 * (*(unsigned __int8*)&pos.x ^ 0xCBF29CE484222325ui64))))));
+            t2 = (((0x100000001B3i64 * t1 + 2654435769u) >> 2) + 2654435769u + ((0x100000001B3i64 * t1 + 2654435769u) << 6) + 0x100000001B3i64 * (*((unsigned __int8*)&pos.y + 3) ^ (0x100000001B3i64 * (*((unsigned __int8*)&pos.y + 2) ^ (0x100000001B3i64 * (*((unsigned __int8*)&pos.y + 1) ^ (0x100000001B3i64 * (*(unsigned __int8*)&pos.y ^ 0xCBF29CE484222325ui64)))))))) ^ (0x100000001B3i64 * t1 + 2654435769u);
+            return t2 ^ ((t2 << 6) + 0x100000001B3i64 * (*((unsigned __int8*)&pos.z + 3) ^ (0x100000001B3i64 * (*((unsigned __int8*)&pos.z + 2) ^ (0x100000001B3i64 * (*((unsigned __int8*)&pos.z + 1) ^ (0x100000001B3i64 * (*(unsigned __int8*)&pos.z ^ 0xCBF29CE484222325ui64))))))) + (t2 >> 2) + 2654435769u);
+        }
+    };
+
+} // namespace std
