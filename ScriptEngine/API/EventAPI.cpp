cf2a04d7243f0a338a2a5953e845d4a514f3e784
xiaoqch <xiaoqch@foxmail.com>
2022-06-08 18:12:29 +0800

    Revert "Fix LLSE onMobHurt event"
    This reverts commit e3a786895b0ccf65a72c5c24ec28f671afb6fe40.

--- ScriptEngine/API/EventAPI.cpp
+++ ScriptEngine/API/EventAPI.cpp
@@ -862,8 +862,9 @@ void EnableEventListener(int eventId)
                 Actor* source = nullptr;
                 if (ev.mDamageSource->isEntitySource())
                 {
+                    auto getEntityUniqueID = (decltype(&ActorDamageSource::getDamagingEntityUniqueID))&ActorDamageSource::__unk_vfn_11;
                     if (ev.mDamageSource->isChildEntitySource())
-                        source = Level::getEntity(ev.mDamageSource->getEntityUniqueID());
+                        source = Level::getEntity((ev.mDamageSource->*getEntityUniqueID)());
                     else
                         source = Level::getEntity(ev.mDamageSource->getDamagingEntityUniqueID());
                 }
