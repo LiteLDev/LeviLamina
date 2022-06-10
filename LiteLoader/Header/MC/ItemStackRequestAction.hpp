25fbc89d9c6107a31a7428571507eed26cdeeb2c
xiaoqch <xiaoqch@foxmail.com>
2022-06-11 00:35:27 +0800

    Update ItemStackRequestAction.hpp

--- LiteLoader/Header/MC/ItemStackRequestAction.hpp
+++ LiteLoader/Header/MC/ItemStackRequestAction.hpp
@@ -13,10 +13,13 @@ class ItemStackRequestAction {
 #define AFTER_EXTRA
 // Add Member There
 public:
+    ItemStackRequestActionType mType;
+    
     static inline std::unordered_map<enum ItemStackRequestActionType, std::string> const& getActionTypeMap()
     {
         return actionTypeMap.mMap1;
     }
+    
 #undef AFTER_EXTRA
 
 #ifndef DISABLE_CONSTRUCTOR_PREVENTION_ITEMSTACKREQUESTACTION
