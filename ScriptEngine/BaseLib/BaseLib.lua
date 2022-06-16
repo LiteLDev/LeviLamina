7d94e2b027dc97a0322991fa51cfdad10e18aa74
PeterXiong720 <59733854+PeterXiong720@users.noreply.github.com>
2022-06-16 16:47:45 +0800

    update

--- ScriptEngine/BaseLib/BaseLib.lua
+++ ScriptEngine/BaseLib/BaseLib.lua
@@ -3,6 +3,59 @@
 -- ---------------------
 package.path = "plugins/lib/?.lua;"..package.path
 
+-- ---------------------
+-- Directory Class
+-- ---------------------
+Directory = {}
+
+function Directory:new(o, patn)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    if (File.checkIsDir(path)) 
+    then
+        File.createDir(path)
+        self.dirPath = path
+    else
+        return nil
+    end
+    return o
+end
+
+function Directory:name()
+    s = string.reverse(self.dirPath)
+    _, i = string.find(s, '/')
+    return string.reverse(string.sub(s, 0, i))
+end
+
+function Directory:children()
+    return File.getFilesList(self.dirPath)
+end
+
+function Directory:rename(newName)
+    s = string.reverse(self.dirPath)
+    _, i = string.find(s, '/')
+    str = string.reverse(string.sub(s, i))
+    newPath = str..newName
+    result = File.rename(self.dirPath, newPath);
+    if (result)
+    then
+        self.dirPath = newPath
+    end
+    return result
+end
+
+function Directory:remove()
+    return File.delete(self.dirPath)
+end
+
+function Directory:copyTo(pathTo)
+    return File.copy(self.dirPath, pathTo)
+end
+
+function Directory:moveTo(pathTo)
+    return File.move(self.dirPath, pathTo)
+end
 
 -- ---------------------
 -- For Compatibility
