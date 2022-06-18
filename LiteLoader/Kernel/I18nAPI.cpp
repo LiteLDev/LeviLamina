14fa7897b1183917328f9b0d4851358bb9e6a873
Jasonzyt <2126931871@qq.com>
2022-06-18 15:28:35 +0800

    Improve I18nAPI
    Update clang-format file

--- LiteLoader/Kernel/I18nAPI.cpp
+++ LiteLoader/Kernel/I18nAPI.cpp
@@ -2,11 +2,75 @@
 #include <Utils/StringHelper.h>
 #include <Main/LiteLoader.h>
 using namespace std;
+namespace fs = std::filesystem;
+
+void I18N::load(const std::string& fileName) {
+    if (!fs::exists(fileName)) {
+        fs::create_directories(fs::path(fileName).parent_path());
+        std::fstream file(fileName, std::ios::out | std::ios::app);
+        nlohmann::json j = defaultLangData;
+        file << std::setw(4) << j; // Dump default language data
+        file.close();
+        langData = defaultLangData;
+        return; // Skip parsing
+    }
+    std::fstream file(fileName, std::ios::in);
+    nlohmann::json j;
+    file >> j;
+    langData = j.get<LangData>();
+    file.close();
+    // Replenish the missing keys
+    for (auto& [lang, dat] : langData) {
+        if (defaultLangData.count(lang)) {
+            for (auto& [k, v] : defaultLangData[lang]) {
+                if (!dat.count(k)) { dat[k] = v; }
+            }
+        }
+    }
+    save();
+}
+
+void I18N::save() {
+    std::fstream file;
+    if (fs::exists(filePath))
+        file.open(filePath, std::ios::out | std::ios::ate);
+    else
+        file.open(filePath, std::ios::out | std::ios::app);
+    nlohmann::json j = langData;
+    file << std::setw(4) << j;
+    file.close();
+}
+
+std::string I18N::get(const std::string& key, const std::string& langCode) {
+    auto& langc = (langCode.empty() ? defaultLangCode : langCode);
+    auto langType = langc.substr(0, 2);
+    if (langData.count(langc)) {
+        auto& lang = langData[langc];
+        if (lang.count(key)) return lang[key];
+        // Search for the similar language in langData
+        for (auto& [lc, ld] : langData) {
+            if (lc.substr(0, 2) == langType) {
+                if (ld.count(key)) { return ld[key]; }
+            }
+        }
+    }
+    // If not found, try falling back to the default language data
+    if (defaultLangData.count(langc)) {
+        if (defaultLangData[langc].count(key)) return defaultLangData[langc][key]; // Fall back
+    }
+    // Search for the similar language
+    for (auto& [lc, ld] : defaultLangData) {
+        if (lc.substr(0, 2) == langType) {
+            if (ld.count(key)) { return ld[key]; }
+        }
+    }
+    // Finally, not found, return the key
+    return key;
+}
 
 ///////////////////////////// Encoding-CodePage Map /////////////////////////////
 #undef UNICODE
-namespace TextEncoding
-{
+namespace TextEncoding {
     const std::unordered_map<Encoding, UINT> Encoding_CodePage_Map = {
         {Encoding::ISO_8859_1, 28591},
         {Encoding::ISO_8859_2, 28592},
@@ -41,7 +105,7 @@ namespace TextEncoding
         {Encoding::ISO_8859_15, 28605},
         {Encoding::MSFT_CP1254, 1254},
         {Encoding::MSFT_CP1257, 1257},
-        {Encoding::ISO_8859_11, 10021},         //?
+        {Encoding::ISO_8859_11, 10021}, //?
         {Encoding::MSFT_CP874, 874},
         {Encoding::MSFT_CP1256, 1256},
         {Encoding::MSFT_CP1255, 1255},
@@ -57,9 +121,9 @@ namespace TextEncoding
         {Encoding::GB18030, 54936},
         {Encoding::BIG5_HKSCS, 950},
         {Encoding::ISO_2022_CN, 50227},
-        {Encoding::TSCII, 57004},               //?
-        {Encoding::TAMIL_MONO, 57004},          //?
-        {Encoding::TAMIL_BI, 57004},            //?
+        {Encoding::TSCII, 57004},      //?
+        {Encoding::TAMIL_MONO, 57004}, //?
+        {Encoding::TAMIL_BI, 57004},   //?
         //{Encoding::JAGRAN, },                 //?
         {Encoding::MACINTOSH_ROMAN, 10000},
         {Encoding::UTF7, CP_UTF7},
@@ -86,17 +150,15 @@ namespace TextEncoding
 ///////////////////////////// Encoding-CodePage Map /////////////////////////////
 
 namespace Translation {
-    bool loadImpl(HMODULE hPlugin, const std::string &filePath) {
-        try {
-            auto content = ReadAllFile(filePath);
-            if (!content)
-                throw std::exception("Fail to open translation file!");
+    bool loadImpl(HMODULE hPlugin, const std::string& filePath) {
+        return loadImpl(hPlugin, filePath, "en_US", {});
+    }
 
-            PluginOwnData::setImpl<nlohmann::json>(hPlugin, TRANSLATION_DATA_NAME,
-                nlohmann::json::parse(*content, nullptr, true, true));
-            PluginOwnData::setImpl<std::string>(hPlugin, TRANSLATION_DATA_FILE, filePath);
-        }
-        catch (const nlohmann::json::exception &e) {
+    bool loadImpl(HMODULE hPlugin, const std::string& filePath, const std::string& defaultLangCode,
+                  const I18N::LangData& defaultLangData) {
+        try {
+            I18N i18n(filePath, defaultLangCode, defaultLangData, hPlugin);
+        } catch (const std::exception& e) {
             logger.error("Fail to load translation file <{}> !", filePath);
             logger.error("{}", TextEncoding::toUTF8(e.what()));
             return false;
@@ -108,77 +170,49 @@ namespace Translation {
     }
 }; // namespace Translation
 
-namespace TextEncoding
-{
-    Encoding getLocalEncoding()
-    {
+namespace TextEncoding {
+    Encoding getLocalEncoding() {
         UINT page = GetACP();
-        for (auto& [k, v] : Encoding_CodePage_Map)
-        {
-            if (v == page)
-                return k;
+        for (auto& [k, v] : Encoding_CodePage_Map) {
+            if (v == page) return k;
         }
         return default_encoding();
     }
 
-    Encoding detectEncoding(const std::string& text, bool* isReliable)
-    {
+    Encoding detectEncoding(const std::string& text, bool* isReliable) {
         bool temp;
         int bytes_consumed;
 
-        return DetectEncoding(
-            text.c_str(), (int)text.size(),
-            nullptr, nullptr, nullptr,
-            UNKNOWN_ENCODING,
-            UNKNOWN_LANGUAGE,
-            CompactEncDet::WEB_CORPUS,
-            false,
-            &bytes_consumed,
-            isReliable ? isReliable : &temp);
+        return DetectEncoding(text.c_str(), (int)text.size(), nullptr, nullptr, nullptr, UNKNOWN_ENCODING,
+                              UNKNOWN_LANGUAGE, CompactEncDet::WEB_CORPUS, false, &bytes_consumed,
+                              isReliable ? isReliable : &temp);
     }
 
-    std::string fromUnicode(const std::wstring &text, Encoding encoding)
-    {
-        try
-        {
+    std::string fromUnicode(const std::wstring& text, Encoding encoding) {
+        try {
             return wstr2str(text, Encoding_CodePage_Map.at(encoding));
-        }
-        catch (...)
-        {
-            return "";
-        }
+        } catch (...) { return ""; }
     }
 
-    std::wstring toUnicode(const std::string &text, Encoding encoding)
-    {
-        try
-        {
+    std::wstring toUnicode(const std::string& text, Encoding encoding) {
+        try {
             return str2wstr(text, Encoding_CodePage_Map.at(encoding));
-        }
-        catch (...)
-        {
-            return L"";
-        }
+        } catch (...) { return L""; }
     }
 
-    std::string toUTF8(const std::string& text)
-    {
+    std::string toUTF8(const std::string& text) {
         return convert(text, detectEncoding(text), Encoding::UTF8);
     }
 
-    std::string toUTF8(const std::string& text, Encoding from)
-    {
+    std::string toUTF8(const std::string& text, Encoding from) {
         return convert(text, from, Encoding::UTF8);
     }
 
-    std::string convert(const std::string &text, Encoding from, Encoding to)
-    {
-        if (text.empty() || from == to)
-            return text;
+    std::string convert(const std::string& text, Encoding from, Encoding to) {
+        if (text.empty() || from == to) return text;
 
         wstring uni = toUnicode(text, from);
-        if (uni.empty())
-            return "";
+        if (uni.empty()) return "";
 
         return fromUnicode(uni, to);
     }
