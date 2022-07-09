c3c3e3bf95019b842d12b8f4e92058c778f4c5d6
twoone3 <3197653242@qq.com>
2022-07-09 12:45:13 +0800

    fix NOMINMAX error

--- LiteLoader/Header/LLAPI.h
+++ LiteLoader/Header/LLAPI.h
@@ -5,7 +5,12 @@
 
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
-#endif // ! WIN32_LEAN_AND_MEAN
+#endif
+
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
 #include <Windows.h>
 
 #include "Global.h"
