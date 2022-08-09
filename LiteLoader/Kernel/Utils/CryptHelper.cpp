4c28a9c6947a3b90a5f5c8b677f5a120897b729e
ShrBox <53301243+ShrBox@users.noreply.github.com>
2022-08-09 21:27:42 +0800

    Upgrade OpenSSL to 3.0.5 (#654)
    #650

--- LiteLoader/Kernel/Utils/CryptHelper.cpp
+++ LiteLoader/Kernel/Utils/CryptHelper.cpp
@@ -1,14 +1,35 @@
 #include <Utils/CryptHelper.h>
 #include <openssl/md5.h>
+#include <openssl/evp.h>
 #include <openssl/sha.h>
 using namespace std;
+
+unsigned char *oldMD5(const unsigned char *d, size_t n, unsigned char *md)
+{
+    EVP_MD_CTX *c;
+    static unsigned char *m;
+    unsigned int md5_digest_len = EVP_MD_size(EVP_md5());
+
+
+    if (md == NULL)
+        md = m;
+    c = EVP_MD_CTX_new();
+    if (!EVP_DigestInit_ex(c, EVP_md5(), NULL))
+        return NULL;
+    EVP_DigestUpdate(c, d, n);
+    m = (unsigned char *)OPENSSL_malloc(md5_digest_len);
+    EVP_DigestFinal_ex(c, md, &md5_digest_len);
+    EVP_MD_CTX_free(c);
+    OPENSSL_cleanse(&c, sizeof(c)); /* security consideration */
+    return md;
+}
 
 string CalcMD5(const string& str) {
     unsigned char md5[MD5_DIGEST_LENGTH];
     const char map[] = "0123456789abcdef";
     string hexmd5;
 
-    MD5((const unsigned char*)str.c_str(), str.length(), md5);
+    oldMD5((const unsigned char*)str.c_str(), str.length(), md5);
     for (size_t i = 0; i < MD5_DIGEST_LENGTH; ++i) {
         hexmd5 += map[md5[i] / 16];
         hexmd5 += map[md5[i] % 16];
