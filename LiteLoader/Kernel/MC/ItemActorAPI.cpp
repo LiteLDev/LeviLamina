f7af0a7355634a42018933c5678b04d30b1f3db7
qingyu <62042544+dreamguxiang@users.noreply.github.com>
2022-08-10 01:35:37 +0800

    Fix ItemActor offset

--- LiteLoader/Kernel/MC/ItemActorAPI.cpp
+++ LiteLoader/Kernel/MC/ItemActorAPI.cpp
@@ -2,18 +2,18 @@
 #include <MC/ItemActor.hpp>
 
 ItemStack* ItemActor::getItemStack() {
-    return (ItemStack*)((uintptr_t)this + 1768); // IDA Player::take
+    return (ItemStack*)((uintptr_t)this + 1776); // IDA Player::take
 }
 
 int ItemActor::getDespawnTime() {
-    return dAccess<int, 487 * 4>(this); // ItemActor::postNormalTick
+    return dAccess<int, 489 * 4>(this); // ItemActor::postNormalTick
 }
 
 bool ItemActor::setDespawnTime(int a1) {
-    dAccess<int, 487 * 4>(this) = a1;
+    dAccess<int, 489 * 4>(this) = a1;
     return true;
 }
 
 int ItemActor::getLatestSpawnTime() {
-    return dAccess<int, 482 * 4>(this);
+    return dAccess<int, 484 * 4>(this);
 }
