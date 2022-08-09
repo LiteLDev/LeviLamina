21f5e2ef8f22a5daec0cb8b50f7447bd69ef9f84
ShrBox <shrbox@outlook.com>
2022-08-09 20:02:58 +0800

    Add `oldMD5` function because of deprecated MD5

--- LiteLoader/Kernel/Utils/CryptHelper.cpp
+++ LiteLoader/Kernel/Utils/CryptHelper.cpp
@@ -1,14 +1,45 @@
 #include <Utils/CryptHelper.h>
 #include <openssl/md5.h>
+#include <openssl/crypto.h>
 #include <openssl/sha.h>
 using namespace std;
+
+unsigned char *oldMD5(const unsigned char *d, size_t n, unsigned char *md)
+{
+    MD5_CTX c;
+    static unsigned char m[MD5_DIGEST_LENGTH];
+
+    if (md == NULL)
+        md = m;
+    if (!MD5_Init(&c))
+        return NULL;
+#ifndef CHARSET_EBCDIC
+    MD5_Update(&c, d, n);
+#else
+    {
+        char temp[1024];
+        unsigned long chunk;
+
+        while (n > 0) {
+            chunk = (n > sizeof(temp)) ? sizeof(temp) : n;
+            ebcdic2ascii(temp, d, chunk);
+            MD5_Update(&c, temp, chunk);
+            n -= chunk;
+            d += chunk;
+        }
+    }
+#endif
+    MD5_Final(md, &c);
+    OPENSSL_cleanse(&c, sizeof(c)); /* security consideration */
+    return md;
+}
 
 string CalcMD5(const string& str) {
     unsigned char md5[MD5_DIGEST_LENGTH];
     const char map[] = "0123456789abcdef";
     string hexmd5;
 
-    MD5((const unsigned char*)str.c_str(), str.length(), md5);
+    oldMD5((const unsigned char*)str.c_str(), str.length(), md5);
     for (size_t i = 0; i < MD5_DIGEST_LENGTH; ++i) {
         hexmd5 += map[md5[i] / 16];
         hexmd5 += map[md5[i] % 16];
