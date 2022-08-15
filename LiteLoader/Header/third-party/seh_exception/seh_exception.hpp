a6446f966cc83a278404f7515c4987b6a596dc42
WangYneos <wangyneos@outlook.com>
2022-08-15 13:05:57 +0800

    Fix SEH handler decare

--- LiteLoader/Header/third-party/seh_exception/seh_exception.hpp
+++ LiteLoader/Header/third-party/seh_exception/seh_exception.hpp
@@ -18,18 +18,21 @@
 #pragma comment(lib, "ntdll")
 
 namespace {
+namespace WinAPI {
 // import windows api manually to avoid windows headers import
 extern "C" {
 long __stdcall RtlNtStatusToDosError(long Status);
-__declspec(dllimport) unsigned long __stdcall FormatMessageA(unsigned long dwFlags, const char* lpSource, unsigned long dwMessageId, unsigned long dwLanguageId, char* lpBuffer, unsigned long nSize, char* Arguments);
+__declspec(dllimport) unsigned long __stdcall FormatMessageA(unsigned long dwFlags, const void* lpSource, unsigned long dwMessageId, unsigned long dwLanguageId, char* lpBuffer, unsigned long nSize, char** Arguments);
 __declspec(dllimport) void* __stdcall LocalFree(void* hMem);
 }
-namespace WinFormatMessageFlags {
+namespace FormatMessageFlags {
 constexpr int FROM_HMODULE = 0x00000800;
 constexpr int FROM_SYSTEM = 0x00001000;
 constexpr int ALLOCATE_BUFFER = 0x00000100;
-}; // namespace WinFormatMessageFlags
+}; // namespace FormatMessageFlags
+}; // namespace WinAPI
 }; // namespace
+
 class seh_exception : std::exception {
 public:
     const unsigned int _expCode = 0;
@@ -42,17 +45,17 @@ public:
 
     virtual ~seh_exception() {
         if (_expMsg) {
-            LocalFree((char*)_expMsg);
+            WinAPI::LocalFree((char*)_expMsg);
             _expMsg = nullptr;
         }
     };
 
     const char* what() const noexcept {
 
-        unsigned long messageLength = FormatMessageA(
-            WinFormatMessageFlags::ALLOCATE_BUFFER | WinFormatMessageFlags::FROM_SYSTEM | WinFormatMessageFlags::FROM_HMODULE,
+        unsigned long messageLength = WinAPI::FormatMessageA(
+            WinAPI::FormatMessageFlags::ALLOCATE_BUFFER | WinAPI::FormatMessageFlags::FROM_SYSTEM | WinAPI::FormatMessageFlags::FROM_HMODULE,
             nullptr,
-            RtlNtStatusToDosError(_expCode),
+            WinAPI::RtlNtStatusToDosError(_expCode),
             0,
             (char*)&_expMsg,
             0,
@@ -76,7 +79,7 @@ public:
     /*
     * @brief Translate SEH Expection to C++ Expection, enable /EHa(Enable SEH Expection) and call _set_se_translator(seh_exception::TranslateSEHtoCE) in new thread
     */
-    static void __cdecl TranslateSEHtoCE(unsigned int ExpCode, SEH_EXP_INFO_POINTER ExpInfo) {
+    static void __cdecl TranslateSEHtoCE(unsigned int ExpCode, struct _EXCEPTION_POINTERS* ExpInfo) {
         throw seh_exception(ExpCode, ExpInfo);
     }
 };
