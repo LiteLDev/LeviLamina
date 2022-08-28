c03b4af0badde87f1007b8826407106827f2ec5f
qingyu <62042544+dreamguxiang@users.noreply.github.com>
2022-08-28 23:07:16 +0800

    Fix ModifyPack Bugs

--- LiteLoader/Main/ModifyPack.cpp
+++ LiteLoader/Main/ModifyPack.cpp
@@ -16,17 +16,15 @@
 #include <MC/PackIdVersion.hpp>
 
 namespace ModifyPack {
-    vector<PackIdVersion> TempPackList;
+    vector<PackIdVersion> PackListCache;
 }
 
 THook(ResourcePack*, "??0ResourcePack@@QEAA@AEAVPack@@@Z",
       ResourcePack* self, Pack* a2) {
     auto Pack = original(self, a2);
-    auto& loc = Pack->getResourceLocation();
         auto packOrigin = Pack->getPackOrigin();
         if (packOrigin == PackOrigin::PackOrigin_Dev) {
-            std::cout << loc.getFullPath().get() << std::endl;
-            ModifyPack::TempPackList.push_back(a2->getManifest().getIdentity());  
+            ModifyPack::PackListCache.push_back(a2->getManifest().getIdentity());  
         }
     return Pack;
 }
@@ -34,7 +32,7 @@ THook(ResourcePack*, "??0ResourcePack@@QEAA@AEAVPack@@@Z",
 THook(std::unique_ptr<ResourcePackStack>, "?deserialize@ResourcePackStack@@SA?AV?$unique_ptr@VResourcePackStack@@U?$default_delete@VResourcePackStack@@@std@@@std@@AEAV?$basic_istream@DU?$char_traits@D@std@@@3@AEBVIResourcePackRepository@@@Z",
       __int64 a2, ResourcePackRepository* a3) {	
     auto Stack = original( a2, a3);
-    for (auto& id : ModifyPack::TempPackList) {
+    for (auto& id : ModifyPack::PackListCache) {
         auto Pack = a3->getResourcePackForPackId(id);
         auto& SettingsFactory = a3->getPackSettingsFactory();
         auto settings = SettingsFactory.getPackSettings(Pack->getManifest());
@@ -46,5 +44,6 @@ THook(std::unique_ptr<ResourcePackStack>, "?deserialize@ResourcePackStack@@SA?AV
                                 PackInstance*, char*, void*, int, bool, PackSettings*)(size, gslPath, 0, false, settings);
         Stack->add(*Instance, *(IResourcePackRepository*)a3, 0);
     }
+    ModifyPack::PackListCache.clear();
     return Stack;
 }
