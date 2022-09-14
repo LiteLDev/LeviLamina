8b07ff1a47d7df0ec02cd59e5b404ddb1fea5b90
RimuruChan <RealSprite233@outlook.com>
2022-09-14 20:08:54 +0800

    Fix llse engine and rename dir to lowercase

---
+++ ScriptEngine/src/engine/TimeTaskSystem.cpp
@@ -0,0 +1,319 @@
+#include "../api/APIHelp.h"
+#include "TimeTaskSystem.h"
+#include "MessageSystem.h"
+#include "EngineOwnData.h"
+#include "EngineManager.h"
+#include <llapi/ScheduleAPI.h>
+#include <llapi/utils/STLHelper.h>
+#include <map>
+#include <vector>
+using namespace std;
+
+std::atomic_uint timeTaskId = 0;
+CsLock locker;
+struct TimeTaskData {
+    ScheduleTask task;
+    script::Global<Function> func;
+    vector<script::Global<Value>> paras;
+    script::Global<String> code;
+    ScriptEngine* engine;
+    inline void swap(TimeTaskData& rhs) {
+        std::swap(rhs.task, task);
+        std::swap(rhs.engine, engine);
+        rhs.code.swap(code);
+        rhs.paras.swap(paras);
+        rhs.func.swap(func);
+    }
+};
+std::unordered_map<int, TimeTaskData> timeTaskMap;
+
+#define TIMETASK_CATCH(TASK_TYPE)                                          \
+    catch (const Exception& e) {                                           \
+        EngineScope scope(engine);                                         \
+        logger.error("Error occurred in {}", TASK_TYPE);                   \
+        PrintException(e);                                                 \
+        logger.error("In Plugin: " + ENGINE_GET_DATA(engine)->pluginName); \
+    }                                                                      \
+    catch (const seh_exception& e) {                                       \
+        logger.error("Error occurred in {}", TASK_TYPE);                   \
+        logger.error("SEH Uncaught Exception Detected!");                  \
+        logger.error(TextEncoding::toUTF8(e.what()));                      \
+        logger.error("In Plugin: " + ENGINE_GET_DATA(engine)->pluginName); \
+    }                                                                      \
+    catch (const std::exception& e) {                                      \
+        logger.error("Error occurred in {}", TASK_TYPE);                   \
+        logger.error("C++ Uncaught Exception Detected!");                  \
+        logger.error(TextEncoding::toUTF8(e.what()));                      \
+        logger.error("In Plugin: " + ENGINE_GET_DATA(engine)->pluginName); \
+    }                                                                      \
+    catch (...) {                                                          \
+        logger.error("Error occurred in {}", TASK_TYPE);                   \
+        logger.error("Uncaught Exception Detected!");                      \
+        logger.error("In Plugin: " + ENGINE_GET_DATA(engine)->pluginName); \
+    }
+
+
+//////////////////// API ////////////////////
+
+// void NewTimeout_s(script::Global<Function> func, vector<script::Local<Value>> paras, int timeout, ScriptEngine* engine)
+//{
+//     std::vector<script::Global<Value>> tmp;
+//     if (paras.size() > 0) {
+//         EngineScope enter(engine);
+//         for (auto& para : paras)
+//             tmp.emplace_back(std::move(para));
+//     }
+//     Schedule::delay(
+//         [engine, func = std::move(func), paras = std::move(tmp)]() {
+//             if (LL::isServerStopping())
+//                 return;
+//             if (!EngineManager::isValid(engine))
+//                 return;
+//             EngineScope enter(engine);
+//             if (paras.empty()) {
+//                 func.get().call();
+//             }
+//             else
+//             {
+//                 vector<Local<Value>> args;
+//                 for (auto& para : paras)
+//                     if (para.isEmpty())
+//                         return;
+//                     else
+//                         args.emplace_back(para.get());
+//                 func.get().call({}, args);
+//             }
+//         },
+//         timeout / 50);
+// }
+
+
+int NewTimeout(Local<Function> func, vector<Local<Value>> paras, int timeout) {
+    int tid = ++timeTaskId;
+    TimeTaskData data;
+
+    data.func = func;
+    data.engine = EngineScope::currentEngine();
+    for (auto& para : paras)
+        data.paras.emplace_back(std::move(para));
+
+    data.task = Schedule::delay(
+        [engine{EngineScope::currentEngine()}, id{tid}]() {
+            try {
+                if (LL::isServerStopping())
+                    return;
+                if (!EngineManager::isValid(engine))
+                    return;
+                // lock after enter EngineScope to prevent deadlock
+                EngineScope scope(engine);
+                TimeTaskData taskData;
+                {
+                    CsLockHolder lock(locker);
+
+                    auto t = timeTaskMap.find(id);
+                    if (t == timeTaskMap.end())
+                        return;
+                    t->second.swap(taskData);
+                    timeTaskMap.erase(id);
+                }
+
+                if (taskData.func.isEmpty())
+                    return;
+                auto func = taskData.func.get();
+                if (taskData.paras.empty()) {
+                    func.call();
+                } else {
+                    vector<Local<Value>> args;
+                    for (auto& para : taskData.paras)
+                        if (para.isEmpty())
+                            return;
+                        else
+                            args.emplace_back(para.get());
+                    func.call({}, args);
+                }
+            }
+            TIMETASK_CATCH("setTimeout-Function");
+        },
+        timeout / 50);
+    CsLockHolder lock(locker);
+    data.swap(timeTaskMap[tid]);
+    return tid;
+}
+
+int NewTimeout(Local<String> func, int timeout) {
+    int tid = ++timeTaskId;
+    TimeTaskData data;
+
+    data.code = func;
+    data.engine = EngineScope::currentEngine();
+
+    data.task = Schedule::delay(
+        [engine{EngineScope::currentEngine()}, id{tid}]() {
+            try {
+                if (LL::isServerStopping())
+                    return;
+                if (!EngineManager::isValid(engine))
+                    return;
+                EngineScope scope(engine);
+                TimeTaskData taskData;
+                {
+                    CsLockHolder lock(locker);
+
+                    auto t = timeTaskMap.find(id);
+                    if (t == timeTaskMap.end())
+                        return;
+                    t->second.swap(taskData);
+                    timeTaskMap.erase(id);
+                }
+
+                if (taskData.code.isEmpty())
+                    return;
+                auto code = taskData.code.get().toString();
+                engine->eval(code);
+            }
+            TIMETASK_CATCH("setTimeout-String");
+        },
+        timeout / 50);
+
+    CsLockHolder lock(locker);
+    data.swap(timeTaskMap[tid]);
+    return tid;
+}
+
+int NewInterval(Local<Function> func, vector<Local<Value>> paras, int timeout) {
+    int tid = ++timeTaskId;
+    TimeTaskData data;
+
+    data.func = func;
+    data.engine = EngineScope::currentEngine();
+    for (auto& para : paras)
+        data.paras.emplace_back(std::move(para));
+
+    data.task = Schedule::repeat(
+        [engine{EngineScope::currentEngine()}, id{tid}]() {
+            try {
+                if (LL::isServerStopping())
+                    return;
+                if (!EngineManager::isValid(engine)) {
+                    ClearTimeTask(id);
+                    return;
+                }
+                EngineScope scope(engine);
+                Local<Value> func = Local<Value>();
+                vector<Local<Value>> args;
+                {
+                    CsLockHolder lock(locker);
+
+                    auto t = timeTaskMap.find(id);
+                    if (t == timeTaskMap.end())
+                        return;
+
+                    TimeTaskData& taskData = t->second;
+
+                    if (taskData.func.isEmpty())
+                        return;
+                    func = taskData.func.get();
+                    if (!taskData.paras.empty()) {
+                        vector<Local<Value>> args;
+                        for (auto& para : taskData.paras)
+                            if (para.isEmpty())
+                                return;
+                            else
+                                args.emplace_back(para.get());
+                    }
+                }
+                if (!func.isFunction())
+                    return;
+                if (args.size() > 0)
+                    func.asFunction().call({}, args);
+                else
+                    func.asFunction().call();
+            }
+            TIMETASK_CATCH("setInterval-Function");
+        },
+        timeout / 50);
+
+    CsLockHolder lock(locker);
+    data.swap(timeTaskMap[tid]);
+    return tid;
+}
+
+int NewInterval(Local<String> func, int timeout) {
+    int tid = ++timeTaskId;
+    TimeTaskData data;
+
+    data.code = func;
+    data.engine = EngineScope::currentEngine();
+
+    data.task = Schedule::repeat(
+        [engine{EngineScope::currentEngine()}, id{tid}]() {
+            try {
+                if (LL::isServerStopping())
+                    return;
+                if (!EngineManager::isValid(engine)) {
+                    ClearTimeTask(id);
+                    return;
+                }
+                EngineScope scope(engine);
+                std::string code;
+                {
+                    CsLockHolder lock(locker);
+
+                    auto t = timeTaskMap.find(id);
+                    if (t == timeTaskMap.end())
+                        return;
+                    TimeTaskData& taskData = t->second;
+
+                    if (taskData.code.isEmpty())
+                        return;
+                    code = taskData.code.get().toString();
+                }
+                if (!code.empty())
+                    engine->eval(code);
+            }
+            TIMETASK_CATCH("setInterval-String");
+        },
+        timeout / 50);
+
+    CsLockHolder lock(locker);
+    data.swap(timeTaskMap[tid]);
+    return tid;
+}
+
+bool ClearTimeTask(int id) {
+    assert(EngineScope::currentEngine() != nullptr);
+    TimeTaskData data;
+    try {
+        CsLockHolder lock(locker);
+        auto it = timeTaskMap.find(id);
+        if (it != timeTaskMap.end()) {
+            data.swap(timeTaskMap[id]);
+            timeTaskMap.erase(id);
+        }
+    } catch (...) {
+        logger.error("Fail in ClearTimeTask");
+    }
+    return true;
+}
+
+
+///////////////////////// Func /////////////////////////
+
+void LLSERemoveTimeTaskData(ScriptEngine* engine) {
+    // enter scope to prevent script::Global::~Global() from crashing
+    EngineScope enter(engine);
+    std::unordered_map<int, TimeTaskData> tmpMap;
+    try {
+        CsLockHolder lock(locker);
+        for (auto it = timeTaskMap.begin(); it != timeTaskMap.end();) {
+            if (it->second.engine == engine) {
+                it->second.swap(tmpMap[it->first]);
+                it = timeTaskMap.erase(it);
+            } else
+                ++it;
+        }
+    } catch (...) {
+        logger.info("Fail in LLSERemoveTimeTaskData");
+    }
+    tmpMap.clear();
+}
---
+++ ScriptEngine/src/main/Loader.cpp
@@ -0,0 +1,165 @@
+#include "../api/APIHelp.h"
+#include "../engine/GlobalShareData.h"
+#include "../engine/EngineOwnData.h"
+#include "../engine/EngineManager.h"
+#include "../utils/IniHelper.h"
+#include "PluginManager.h"
+#include "NodeJsHelper.h"
+#include <list>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <filesystem>
+#include <exception>
+#include <filesystem>
+#include <memory>
+#include "Configs.h"
+
+extern ::Logger logger;
+
+// Baselibs dependency 
+std::unordered_map<std::string, std::string> depends;
+
+// Debug engine
+ScriptEngine* debugEngine;
+bool globalDebug = false;
+
+// Pre-declared
+extern void BindAPIs(ScriptEngine* engine);
+
+// Pre-load dependency
+void LoadDepends() {
+#ifdef LLSE_BACKEND_NODEJS
+    return; // NodeJs backend load depends code in another place
+#endif
+
+    std::filesystem::directory_iterator deps(LLSE_DEPENDS_DIR);
+    for (auto& i : deps) {
+        if (i.is_regular_file() && i.path().filename() == string("BaseLib") + LLSE_PLUGINS_EXTENSION) {
+            try {
+                auto path = UTF82String(i.path().generic_u8string());
+                auto content = ReadAllFile(path);
+                if (!content)
+                    throw("Fail to open plugin file!");
+                depends.emplace(path, *content);
+                logger.info(tr("llse.loader.loadDepends.success", UTF82String(i.path().filename().u8string())));
+            } catch (std::exception e) {
+                logger.warn(tr("llse.loader.loadDepends.fail", UTF82String(i.path().filename().u8string())));
+                logger.warn(TextEncoding::toUTF8(e.what()));
+            } catch (...) {
+                logger.warn(tr("llse.loader.loadDepends.fail", UTF82String(i.path().filename().u8string())));
+            }
+        }
+    }
+}
+
+// Load debug engine
+void LoadDebugEngine() {
+#ifdef LLSE_BACKEND_NODEJS
+    return; // NodeJs backend didn't enable debug engine now
+#endif
+
+    //  Create engine
+    debugEngine = EngineManager::newEngine(LLSE_DEBUG_ENGINE_NAME);
+    EngineScope enter(debugEngine);
+
+    // Bind APIs
+    try {
+        BindAPIs(debugEngine);
+    } catch (const Exception& e) {
+        logger.error("Fail in binding Debug Engine!\n");
+        throw;
+    }
+
+    // Load baselibs
+    try {
+        for (auto& [path, content] : depends) {
+            debugEngine->eval(content, path);
+        }
+    } catch (const Exception& e) {
+        logger.error("Fail in Loading Dependence Lib!\n");
+        throw;
+    }
+}
+
+
+// Main load procedure
+void LoadMain_Package();
+void LoadMain() {
+
+#if LLSE_IS_PLUGIN_PACKAGE
+    // Load plugin package
+    LoadMain_Package();
+
+#else
+
+    // Load simple file plugin
+    logger.info(tr("llse.loader.loadMain.start", fmt::arg("type", LLSE_MODULE_TYPE)));
+    int count = 0;
+    std::filesystem::directory_iterator files(LLSE_PLUGINS_LOAD_DIR);
+    for (auto& i : files) {
+        if (i.is_regular_file() && i.path().extension() == LLSE_PLUGINS_EXTENSION) {
+            if (PluginManager::loadPlugin(UTF82String(i.path().generic_u8string()), false, true))
+                ++count;
+        }
+    }
+    logger.info(tr("llse.loader.loadMain.done",
+                   fmt::arg("count", count),
+                   fmt::arg("type", LLSE_MODULE_TYPE)));
+#endif
+}
+
+
+
+#ifdef LLSE_BACKEND_NODEJS
+// NodeJs后端 - 主加载
+void LoadMain_NodeJs() {
+    logger.info(tr("llse.loader.loadMain.start", fmt::arg("type", "Node.js")));
+    int installCount = 0;
+    int count = 0;
+
+    // Load plugins in NODEJS_ROOT_DIR
+    std::filesystem::directory_iterator files(LLSE_PLUGINS_ROOT_DIR);
+    for (auto& i : files) {
+        std::filesystem::path pth = i.path();
+        if (i.is_directory() && pth.filename() != "node_modules") {
+            if (std::filesystem::exists(pth / "package.json")) {
+                if (PluginManager::loadPlugin(UTF82String(pth.u8string()), false, true)) {
+                    ++count;
+                }
+            }
+            else {
+                logger.warn(tr("llse.loader.loadMain.nodejs.ignored", UTF82String(pth.filename().u8string())));
+            }
+        }
+    }
+
+    // Unpack .llplugin & install
+    // Tips: Must after plugins loaded in NODEJS_ROOT_DIR
+    files = std::filesystem::directory_iterator(LLSE_PLUGINS_LOAD_DIR);
+    for (auto& i : files) {
+        std::filesystem::path pth = i.path();
+        if (i.is_regular_file() && EndsWith(UTF82String(pth.u8string()), LLSE_PLUGIN_PACKAGE_EXTENSION)) {
+            logger.info(tr("llse.loader.loadMain.nodejs.installPack.start",
+                           fmt::arg("path", UTF82String(pth.u8string()))));
+            if (!PluginManager::loadPlugin(UTF82String(pth.u8string()), false, true)) {
+                logger.error(tr("llse.loader.loadMain.nodejs.installPack.fail"));
+            }
+            ++installCount;
+        }
+    }
+
+    logger.info(tr("llse.loader.loadMain.done",
+                   fmt::arg("count", count),
+                   fmt::arg("type", "Node.js")));
+}
+#endif
+
+void LoadMain_Package()
+{
+#ifdef LLSE_BACKEND_NODEJS
+    // Process NodeJs backend's plugin load separately
+    LoadMain_NodeJs();
+    return;
+#endif
+}
