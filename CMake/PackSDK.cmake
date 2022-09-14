818cb1e19ee3e86a03703dc2de36c100f9b46067
RimuruChan <RealSprite233@outlook.com>
2022-09-15 01:34:52 +0800

    Update PackSDK.cmake

--- CMake/PackSDK.cmake
+++ CMake/PackSDK.cmake
@@ -7,6 +7,7 @@ add_custom_target(PackSDK)
 add_custom_command(
         TARGET PackSDK POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_directory ${LiteLoader_SOURCE_DIR}/include/llapi ${CMAKE_BINARY_DIR}/sdk/Header
+        COMMAND ${CMAKE_COMMAND} -E copy_directory ${LiteLoader_SOURCE_DIR}/third-party/include ${CMAKE_BINARY_DIR}/sdk/third-party
         COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/Tools/LibraryBuilder.exe ${CMAKE_BINARY_DIR}/sdk/Tools/
         COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/Tools/LLPeEditor.exe ${CMAKE_BINARY_DIR}/sdk/Tools/
         COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/Tools/SDK-submodule-add.cmd ${CMAKE_BINARY_DIR}/sdk/Tools/
