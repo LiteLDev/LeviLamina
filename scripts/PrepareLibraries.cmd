654dbf649cad32cf7511e371171ba2d018922d2b
雪碧 <42002296+RimuruChan@users.noreply.github.com>
2022-09-17 23:25:25 +0800

    Fix CMake build library (#806)

--- scripts/PrepareLibraries.cmd
+++ scripts/PrepareLibraries.cmd
@@ -1,11 +1,14 @@
 @echo off
 
-if not exist %1\LiteLoader\Lib\bedrock_server_api.lib goto process
-if not exist %1\LiteLoader\Lib\bedrock_server_var.lib goto process
+if not exist %1\LiteLoader\lib\bedrock_server_api.lib goto process
+if not exist %1\LiteLoader\lib\bedrock_server_var.lib goto process
 goto end
 
 :process
 cd /d %1\Tools
 LibraryBuilder.exe
+if not exist %1\LiteLoader\lib\ mkdir %1\LiteLoader\lib
+move %1\Tools\bedrock_server_api.lib %1\LiteLoader\lib\
+move %1\Tools\bedrock_server_var.lib %1\LiteLoader\lib\
 
 :end
