68df1b7549052537dd1d2acfb8e560bb7a65c395
ShrBox <shrbox@outlook.com>
2022-09-25 08:59:48 +0800

    Update installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Install wine
 codename=$(lsb_release -c --short)
-BDSVER="1.19.22.01"
-LLVER="2.6.2"
+BDSVER="1.19.30.04"
+LLVER="2.7.0-release.2"
 sudo apt install -y wget unzip
 sudo dpkg --add-architecture i386
 sudo wget -nc -O /usr/share/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
