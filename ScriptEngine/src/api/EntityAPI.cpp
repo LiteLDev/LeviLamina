19d93aa74d6934ea69a742b55900a17cbdedc5f4
Redbeanw44602 <redbeana44945@gmail.com>
2023-01-15 14:05:12 +0800

    Update `entity.teleport` with `player.teleport`.

--- ScriptEngine/src/api/EntityAPI.cpp
+++ ScriptEngine/src/api/EntityAPI.cpp
@@ -617,18 +617,18 @@ Local<Value> EntityClass::getDirection() {
 }
 
 Local<Value> EntityClass::teleport(const Arguments& args) {
-    CHECK_ARGS_COUNT(args, 1);
-    if (args.size() == 4) {
-        CHECK_ARG_TYPE(args[0], ValueKind::kNumber);
-        CHECK_ARG_TYPE(args[1], ValueKind::kNumber);
-        CHECK_ARG_TYPE(args[2], ValueKind::kNumber);
-        CHECK_ARG_TYPE(args[3], ValueKind::kNumber);
-    }
+    CHECK_ARGS_COUNT(args, 1)
 
     try {
+        Actor* entity = get();
+        if (!entity)
+            return Boolean::newBoolean(false);
+        float pitch;
+        float yaw;
         FloatVec4 pos;
+        bool rotationIsValid = false;
 
-        if (args.size() == 1) {
+        if (args.size() <= 2) {
             if (IsInstanceOf<IntPos>(args[0])) {
                 // IntPos
                 IntPos* posObj = IntPos::extractPos(args[0]);
@@ -650,9 +650,15 @@ Local<Value> EntityClass::teleport(const Arguments& args) {
                 }
             } else {
                 LOG_WRONG_ARG_TYPE();
-                return Local<Value>();
+                return Boolean::newBoolean(false);
             }
-        } else if (args.size() == 4) {
+            if (args.size() == 2 && IsInstanceOf<DirectionAngle>(args[1])) {
+                auto angle = DirectionAngle::extract(args[1]);
+                pitch = angle->pitch;
+                yaw = angle->yaw;
+                rotationIsValid = true;
+            }
+        } else if (args.size() <= 5) { // teleport(x,y,z,dimid[,rot])
             // number pos
             CHECK_ARG_TYPE(args[0], ValueKind::kNumber);
             CHECK_ARG_TYPE(args[1], ValueKind::kNumber);
@@ -663,19 +669,24 @@ Local<Value> EntityClass::teleport(const Arguments& args) {
             pos.y = args[1].asNumber().toFloat();
             pos.z = args[2].asNumber().toFloat();
             pos.dim = args[3].toInt();
+            if (args.size() == 5 && IsInstanceOf<DirectionAngle>(args[4])) {
+                auto angle = DirectionAngle::extract(args[4]);
+                pitch = angle->pitch;
+                yaw = angle->yaw;
+                rotationIsValid = true;
+            }
         } else {
-            LOG_WRONG_ARGS_COUNT();
-            return Local<Value>();
+            LOG_WRONG_ARG_TYPE();
+            return Boolean::newBoolean(false);
         }
-
-        Actor* entity = get();
-        if (!entity)
-            return Local<Value>();
-
-        entity->teleport(pos.getVec3(), pos.dim);
-        return Boolean::newBoolean(true);
+        if (!rotationIsValid) {
+            auto ang = entity->getRotation();
+            pitch = ang.x;
+            yaw = ang.y;
+        }
+        return Boolean::newBoolean(entity->teleport(pos.getVec3(), pos.dim, pitch, yaw));
     }
-    CATCH("Fail in teleportEntity!")
+    CATCH("Fail in TeleportEntity!")
 }
 
 Local<Value> EntityClass::distanceTo(const Arguments& args) {
