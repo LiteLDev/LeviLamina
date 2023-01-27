9f19fa934b8be9a62b2d590abdacf0ffc740e614
RimuruChan <RealSprite233@outlook.com>
2023-01-27 14:11:26 +0800

    fix: correct export symbol

--- src/liteloader/dllmain.cpp
+++ src/liteloader/dllmain.cpp
@@ -1,6 +1,6 @@
-﻿#pragma comment(linker, "/export:HookFunction=LLPreLoader.HookFunction")
-#pragma comment(linker, "/export:dlsym_real=LLPreLoader.dlsym_real")
-#pragma comment(linker, "/export:?dlsym_reverse@@YA?AV?$vector@V?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@V?$allocator@V?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@@2@@std@@H@Z=LLPreLoader.?dlsym_reverse@@YA?AV?$vector@V?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@V?$allocator@V?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@@2@@std@@H@Z")
+﻿#pragma comment(linker, "/export:HookFunction=PreLoader.HookFunction")
+#pragma comment(linker, "/export:dlsym_real=PreLoader.dlsym_real")
+#pragma comment(linker, "/export:?dlsym_reverse@@YA?AV?$vector@V?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@V?$allocator@V?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@@2@@std@@H@Z=PreLoader.?dlsym_reverse@@YA?AV?$vector@V?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@V?$allocator@V?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@@2@@std@@H@Z")
 
 
 #include <windows.h>
