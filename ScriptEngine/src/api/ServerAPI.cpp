d9d53fd1d9a34fdcf473c490933ddcf54bb5f370
CanXue <104246753+ACX66@users.noreply.github.com>
2023-02-25 10:38:28 +0100

    feat: add get/setTime() get/setWeather() api (#1087)
    * add weather/time api
    
    * fix error
    
    * Update ScriptEngine/src/api/ServerAPI.cpp
    
    Co-authored-by: Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
    
    * change switch
    
    ---------
    
    Co-authored-by: Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
    Co-authored-by: Futrime <futrime@outlook.com>

--- ScriptEngine/src/api/ServerAPI.cpp
+++ ScriptEngine/src/api/ServerAPI.cpp
@@ -4,6 +4,9 @@
 #include <llapi/ServerAPI.h>
 #include "api/McAPI.h"
 #include <llapi/mc/ServerNetworkHandler.hpp>
+#include <llapi/mc/LevelData.hpp>
+#include <llapi/mc/SetTimePacket.hpp>
+#include <llapi/mc/LoopbackPacketSender.hpp>
 #include "main/SafeGuardRecord.h"
 
 Local<Value> McClass::setMotd(const Arguments& args) {
@@ -36,4 +39,90 @@ Local<Value> McClass::setMaxNumPlayers(const Arguments& args) {
         return Boolean::newBoolean(back == 0 ? true : false);
     }
     CATCH("Fail in setMaxPlayers!")
+}
+
+Local<Value> McClass::getTime(const Arguments& args) {
+    int option = 0; // option: 0: daytime, 1: gametime, 2: day
+
+    if(args.size() > 0) {
+        CHECK_ARG_TYPE(args[0], ValueKind::kNumber)
+        option = args[0].asNumber().toInt32();
+    }
+
+    switch (option) {
+        case 0:
+            return Number::newNumber(Global<Level>->getTime() % 24000);
+            break;
+        case 1:
+            return Number::newNumber((int)Global<Level>->getCurrentTick());
+            break;
+        case 2:
+            return Number::newNumber(Global<Level>->getTime() / 24000);
+            break;
+        default:
+            throw script::Exception("The range of this argument is between 0 and 2");
+    }
+}
+
+Local<Value> McClass::setTime(const Arguments& args) {
+    CHECK_ARGS_COUNT(args, 1)
+    CHECK_ARG_TYPE(args[0], ValueKind::kNumber)
+
+    try {
+        int currentTime = Global<Level>->getTime();
+        int targetTime = args[0].asNumber().toInt32(); // Tick
+
+        int newTime = currentTime;
+        int currentTimeOfDay = currentTime % 24000;
+
+        if (targetTime > currentTime % 24000)
+            newTime = currentTime + targetTime - currentTimeOfDay;
+        else if (targetTime < currentTimeOfDay)
+            newTime = currentTime + targetTime + 24000 - currentTimeOfDay;
+
+        Global<Level>->setTime(newTime);
+        SetTimePacket pkt = SetTimePacket(newTime);
+        LoopbackPacketSender* pktSender = (LoopbackPacketSender*)Global<Level>->getPacketSender();
+        pktSender->send(pkt);
+    }
+    CATCH("Fail in setTime!")
+
+    return Boolean::newBoolean(true);
+}
+
+Local<Value> McClass::getWeather(const Arguments& args) { // weather: 0: Clear, 1: Rain, 2: Thunder
+    if (Global<Level>->getLevelData().isLightning())
+        return Number::newNumber(2);
+    else if (Global<Level>->getLevelData().isRaining())
+        return Number::newNumber(1);
+
+    return Number::newNumber(0);
+}
+
+Local<Value> McClass::setWeather(const Arguments& args) {
+    CHECK_ARGS_COUNT(args, 1)
+    CHECK_ARG_TYPE(args[0], ValueKind::kNumber)
+
+    int duration = 0;
+    int weather = args[0].asNumber().toInt32(); // weather: 0: Clear, 1: Rain, 2: Thunder
+
+    try {
+        if (args.size() > 1) {
+            CHECK_ARG_TYPE(args[1], ValueKind::kNumber);
+            duration = args[1].asNumber().toInt32();
+        } else {
+            Random& random = Global<Level>->getRandom();
+            duration = 20 * (SymCall("?nextInt@Random@@UEAAHH@Z", int, Random&, int)(random, 600) + 300);
+        }
+
+        if (weather == 1)
+            Global<Level>->updateWeather(1.0, duration, 0.0, duration);
+        else if (weather == 2)
+            Global<Level>->updateWeather(1065353216.0, duration, 1065353216.0, duration);
+        else
+            Global<Level>->updateWeather(0.0, duration, 0.0, duration);
+    }
+    CATCH("Fail in setWeather!")
+
+    return Boolean::newBoolean(true);
 }
