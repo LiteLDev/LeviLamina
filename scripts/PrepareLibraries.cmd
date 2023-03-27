0bb3a5b8b7e4988ec47aebf98dc3a2bf0a48e0b6
ShrBox <shrbox@outlook.com>
2023-03-27 19:53:31 +0800

    chore: update PeEditor to v3.1.1 chore: update actions

--- scripts/PrepareLibraries.cmd
+++ scripts/PrepareLibraries.cmd
@@ -6,7 +6,7 @@ goto end
 
 :process
 cd /d %1\Tools
-LibraryBuilder.exe
+LLPeEditor.exe -c -l
 if not exist %1\LiteLoader\lib\ mkdir %1\LiteLoader\lib
 move %1\Tools\bedrock_server_api.lib %1\LiteLoader\lib\
 move %1\Tools\bedrock_server_var.lib %1\LiteLoader\lib\
