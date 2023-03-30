55036df32d916d5ce51a3d47935790e7f8850c19
ShrBox <53301243+ShrBox@users.noreply.github.com>
2023-03-30 22:14:30 +0800

    chore: fix installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-codename=$(lsb_release -c --short)
 lip_version=0.14.0
 
-# Install wine
-sudo apt install -y wget unzip
-sudo dpkg --add-architecture i386
-sudo mkdir -pm755 /etc/apt/keyrings
-sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/${codename}/winehq-${codename}.sources
-sudo apt update
-sudo apt install winehq-stable -y
-
 # Install Lip
-wget https://github.com/LiteLDev/Lip/releases/download/v0.12.0/lip-${lip_version}-linux-amd64.tar.gz
+wget https://github.com/LiteLDev/Lip/releases/download/v${lip_version}/lip-${lip_version}-linux-amd64.tar.gz
 tar -xvf lip-${lip_version}-linux-amd64.tar.gz
 chmod +x lip-${lip_version}-linux-amd64/lip
 
