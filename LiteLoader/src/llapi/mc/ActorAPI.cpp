24596e5ebbebb809dabb54f7de64f98882c797c9
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2023-05-14 21:19:36 +0800

    feat: add entity source in Actor::hurtEntity  #1224 (#1256)
    * feat #1224
    
    * feat #1224
    
    * feat #1224
    
    * feat #1224
    
    * Update ActorDamageSource.cpp
    
    * feat #1224
    
    * Update ActorAPI.cpp

--- LiteLoader/src/llapi/mc/ActorAPI.cpp
+++ LiteLoader/src/llapi/mc/ActorAPI.cpp
@@ -30,6 +30,7 @@
 #include "llapi/mc/RotationCommandUtils.hpp"
 #include "llapi/mc/WeakEntityRef.hpp"
 #include "llapi/mc/WeakStorageEntity.hpp"
+#include "llapi/mc/ActorDamageByActorSource.hpp"
 
 class UserEntityIdentifierComponent;
 
@@ -93,6 +94,14 @@ bool Actor::hurtEntity(float damage, ActorDamageCause damageCause) {
     auto ads = new ActorDamageSource(damageCause);
     auto res = ((Mob*)this)->_hurt(*ads, damage, true, false);
     ads->~ActorDamageSource();
+    delete ads;
+    return res;
+}
+
+bool Actor::hurtEntity(float damage, ActorDamageCause damageCause, Actor* actor) {
+    auto ads = new ActorDamageByActorSource(*actor, damageCause);
+    auto res = ((Mob*)this)->_hurt(*(ActorDamageSource*)ads, damage, true, false);
+    ads->~ActorDamageByActorSource();
     delete ads;
     return res;
 }
--- LiteLoader/src/llapi/mc/ActorDamageSource.cpp
+++ LiteLoader/src/llapi/mc/ActorDamageSource.cpp
@@ -1,7 +1,17 @@
 #include "llapi/mc/ActorDamageSource.hpp"
 #include "llapi/mc/Level.hpp"
+#include "llapi/HookAPI.h"
+#include "llapi/mc/ActorUniqueID.hpp"
 
 Actor* ActorDamageSource::getEntity() {
     ActorUniqueID v6 = getDamagingEntityUniqueID();
     return Global<Level>->getEntity(v6);
+}
+
+bool ActorDamageSource::setEntity(ActorUniqueID uid) {
+    if (Global<Level>->getEntity(uid)) {
+        dAccess<ActorUniqueID, 32>(this) = uid;
+        return true;
+    }
+    return false;
 }
