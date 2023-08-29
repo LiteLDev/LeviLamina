1932bce0f44ed6919d0e9e609139def4ac102a22
dofes <91889957+dofes@users.noreply.github.com>
2023-08-29 20:18:35 +0800

    refactor: update parameter names for methods in TextPacket

--- src/mc/network/packet/TextPacket.h
+++ src/mc/network/packet/TextPacket.h
@@ -1,17 +1,13 @@
 #pragma once
 
 #include "mc/_HeaderOutputPredefine.h"
+#include "mc/config/IPlayerData.h"
+#include "mc/config/ISharedController.h"
 #include "mc/enums/TextPacketType.h"
 
 // auto generated inclusion list
 #include "mc/deps/core/common/bedrock/Result.h"
 #include "mc/network/packet/Packet.h"
-
-// auto generated forward declare list
-// clang-format off
-namespace PlayerCapabilities { struct IPlayerData; }
-namespace PlayerCapabilities { struct ISharedController; }
-// clang-format on
 
 class TextPacket : public ::Packet {
 
@@ -68,68 +64,95 @@ public:
      * @symbol
      * ?_shouldHandleTextPacketForPlayer\@TextPacket\@\@SA?B_NAEBV1\@AEAUIPlayerData\@PlayerCapabilities\@\@AEBUISharedController\@3\@\@Z
      */
-    MCAPI static bool const
-    _shouldHandleTextPacketForPlayer(class TextPacket const&, struct PlayerCapabilities::IPlayerData&, struct PlayerCapabilities::ISharedController const&);
+    MCAPI static bool const _shouldHandleTextPacketForPlayer(
+        class TextPacket const&                             packet,
+        struct PlayerCapabilities::IPlayerData&             playerData,
+        struct PlayerCapabilities::ISharedController const& sharedController
+    );
     /**
      * @symbol
      * ?createAnnouncement\@TextPacket\@\@SA?AV1\@AEBV?$basic_string\@DU?$char_traits\@D\@std\@\@V?$allocator\@D\@2\@\@std\@\@000\@Z
      */
-    MCAPI static class TextPacket
-    createAnnouncement(std::string const&, std::string const&, std::string const&, std::string const&);
+    MCAPI static class TextPacket createAnnouncement(
+        std::string const& author,
+        std::string const& message,
+        std::string const& xuid,
+        std::string const& platformId
+    );
     /**
      * @symbol
      * ?createChat\@TextPacket\@\@SA?AV1\@AEBV?$basic_string\@DU?$char_traits\@D\@std\@\@V?$allocator\@D\@2\@\@std\@\@000\@Z
      */
-    MCAPI static class TextPacket
-    createChat(std::string const&, std::string const&, std::string const&, std::string const&);
+    MCAPI static class TextPacket createChat(
+        std::string const& author,
+        std::string const& message,
+        std::string const& xuid,
+        std::string const& platformId
+    );
     /**
      * @symbol
      * ?createJukeboxPopup\@TextPacket\@\@SA?AV1\@AEBV?$basic_string\@DU?$char_traits\@D\@std\@\@V?$allocator\@D\@2\@\@std\@\@AEBV?$vector\@V?$basic_string\@DU?$char_traits\@D\@std\@\@V?$allocator\@D\@2\@\@std\@\@V?$allocator\@V?$basic_string\@DU?$char_traits\@D\@std\@\@V?$allocator\@D\@2\@\@std\@\@\@2\@\@3\@\@Z
      */
-    MCAPI static class TextPacket createJukeboxPopup(std::string const&, std::vector<std::string> const&);
+    MCAPI static class TextPacket
+    createJukeboxPopup(std::string const& message, std::vector<std::string> const& params);
     /**
      * @symbol
      * ?createRawJsonObjectMessage\@TextPacket\@\@SA?AV1\@AEBV?$basic_string\@DU?$char_traits\@D\@std\@\@V?$allocator\@D\@2\@\@std\@\@\@Z
      */
-    MCAPI static class TextPacket createRawJsonObjectMessage(std::string const&);
+    MCAPI static class TextPacket createRawJsonObjectMessage(std::string const& rawJson);
     /**
      * @symbol
      * ?createSystemMessage\@TextPacket\@\@SA?AV1\@AEBV?$basic_string\@DU?$char_traits\@D\@std\@\@V?$allocator\@D\@2\@\@std\@\@\@Z
      */
-    MCAPI static class TextPacket createSystemMessage(std::string const&);
+    MCAPI static class TextPacket createSystemMessage(std::string const& message);
     /**
      * @symbol
      * ?createTextObjectMessage\@TextPacket\@\@SA?AV1\@AEBVResolvedTextObject\@\@V?$basic_string\@DU?$char_traits\@D\@std\@\@V?$allocator\@D\@2\@\@std\@\@1\@Z
      */
-    MCAPI static class TextPacket createTextObjectMessage(class ResolvedTextObject const&, std::string, std::string);
+    MCAPI static class TextPacket createTextObjectMessage(
+        class ResolvedTextObject const& resolvedTextObject,
+        std::string                     fromXuid,
+        std::string                     fromPlatformId
+    );
     /**
      * @symbol
      * ?createTextObjectWhisperMessage\@TextPacket\@\@SA?AV1\@AEBVResolvedTextObject\@\@AEBV?$basic_string\@DU?$char_traits\@D\@std\@\@V?$allocator\@D\@2\@\@std\@\@1\@Z
      */
-    MCAPI static class TextPacket
-    createTextObjectWhisperMessage(class ResolvedTextObject const&, std::string const&, std::string const&);
+    MCAPI static class TextPacket createTextObjectWhisperMessage(
+        class ResolvedTextObject const& resolvedTextObject,
+        std::string const&              xuid,
+        std::string const&              platformId
+    );
     /**
      * @symbol
      * ?createTextObjectWhisperMessage\@TextPacket\@\@SA?AV1\@AEBV?$basic_string\@DU?$char_traits\@D\@std\@\@V?$allocator\@D\@2\@\@std\@\@00\@Z
      */
     MCAPI static class TextPacket
-    createTextObjectWhisperMessage(std::string const&, std::string const&, std::string const&);
+    createTextObjectWhisperMessage(std::string const& message, std::string const& xuid, std::string const& platformId);
     /**
      * @symbol
      * ?createTranslated\@TextPacket\@\@SA?AV1\@AEBV?$basic_string\@DU?$char_traits\@D\@std\@\@V?$allocator\@D\@2\@\@std\@\@AEBV?$vector\@V?$basic_string\@DU?$char_traits\@D\@std\@\@V?$allocator\@D\@2\@\@std\@\@V?$allocator\@V?$basic_string\@DU?$char_traits\@D\@std\@\@V?$allocator\@D\@2\@\@std\@\@\@2\@\@3\@\@Z
      */
-    MCAPI static class TextPacket createTranslated(std::string const&, std::vector<std::string> const&);
+    MCAPI static class TextPacket createTranslated(std::string const& message, std::vector<std::string> const& params);
     /**
      * @symbol
      * ?createTranslatedAnnouncement\@TextPacket\@\@SA?AV1\@AEBV?$basic_string\@DU?$char_traits\@D\@std\@\@V?$allocator\@D\@2\@\@std\@\@000\@Z
      */
-    MCAPI static class TextPacket
-    createTranslatedAnnouncement(std::string const&, std::string const&, std::string const&, std::string const&);
+    MCAPI static class TextPacket createTranslatedAnnouncement(
+        std::string const& author,
+        std::string const& message,
+        std::string const& xuid,
+        std::string const& platformId
+    );
     /**
      * @symbol
      * ?createWhisper\@TextPacket\@\@SA?AV1\@AEBV?$basic_string\@DU?$char_traits\@D\@std\@\@V?$allocator\@D\@2\@\@std\@\@000\@Z
      */
-    MCAPI static class TextPacket
-    createWhisper(std::string const&, std::string const&, std::string const&, std::string const&);
+    MCAPI static class TextPacket createWhisper(
+        std::string const& author,
+        std::string const& message,
+        std::string const& xuid,
+        std::string const& platformId
+    );
     // NOLINTEND
 };
