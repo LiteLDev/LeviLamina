64bb9b0cf833cdaee9537a6202871cc503d5b551
OEOTYAN <1124718975@qq.com>
2023-09-09 22:19:38 +0800

    feat: add NonOwnerPointer API

--- src/mc/deps/core/common/bedrock/NonOwnerPointer.h
+++ src/mc/deps/core/common/bedrock/NonOwnerPointer.h
@@ -5,11 +5,36 @@
 
 namespace Bedrock {
 
-template <typename T0>
+template <typename T>
 class NonOwnerPointer {
 public:
     std::shared_ptr<Bedrock::EnableNonOwnerReferences::ControlBlock> mControlBlock;
-    NonOwnerPointer(std::nullptr_t null) : mControlBlock(null) {}
+    NonOwnerPointer(std::nullptr_t) noexcept {} // NOLINT
+    T* get() const { return reinterpret_cast<T*>(mControlBlock.get()); }
+    explicit operator bool() const noexcept { return get() != nullptr; }
+
+    constexpr                operator T*() const { return get(); }
+    constexpr T*             operator->() const { return get(); }
+    constexpr decltype(auto) operator*() const { return *get(); }
 };
+
+template <class T>
+[[nodiscard]] bool operator==(const NonOwnerPointer<T>& self, nullptr_t) noexcept {
+    return self.get() == nullptr;
+}
+template <class T>
+[[nodiscard]] std::strong_ordering operator<=>(const NonOwnerPointer<T>& self, nullptr_t) noexcept {
+    return self.get() <=> static_cast<T*>(nullptr);
+}
+
+template <class T1, class T2>
+[[nodiscard]] bool operator==(const NonOwnerPointer<T1>& l, const NonOwnerPointer<T2>& r) noexcept {
+    return l.get() == r.get();
+}
+
+template <class T1, class T2>
+[[nodiscard]] std::strong_ordering operator<=>(const NonOwnerPointer<T1>& l, const NonOwnerPointer<T2>& r) noexcept {
+    return l.get() <=> r.get();
+}
 
 }; // namespace Bedrock
