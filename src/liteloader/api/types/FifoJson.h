ec4dea43e9bf3a238cde06ae5beb02a76e065e3b
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2023-09-10 22:01:24 +0800

    refactor: move StdInt.h and FifoJson.h to types

--- src/liteloader/api/types/FifoJson.h
+++ src/liteloader/api/types/FifoJson.h
@@ -1,6 +1,10 @@
 #include <fifo_map/fifo_map.hpp>
 #include <nlohmann/json.hpp>
 
-template <class Key, class T, class dummy_compare, class Allocator>
+namespace ll {
+
+template <class Key, class T, class DummyCompare, class Allocator>
 using workaround_fifo_map = nlohmann::fifo_map<Key, T, nlohmann::fifo_map_compare<Key>, Allocator>;
 using fifo_json           = nlohmann::basic_json<workaround_fifo_map>;
+
+}; // namespace ll
