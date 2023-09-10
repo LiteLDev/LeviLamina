9608c49e617199f2fde234ed7230fd3d05d26c6d
RimuruChan <RealSprite233@outlook.com>
2023-09-10 17:20:11 +0800

    refactor: add standard integer

---
+++ src/liteloader/api/StdInt.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <cstdint>
+
+using ushort = uint16_t;
+using uint   = uint32_t;
+using ulong  = unsigned long;
+
+using uchar = uint8_t;
+using schar = int8_t;
+using byte  = uchar;
+
+using int64 = int64_t;
+using int32 = int32_t;
+using int16 = int16_t;
+using int8  = int8_t;
+
+using uint64 = uint64_t;
+using uint32 = uint32_t;
+using uint16 = uint16_t;
+using uint8  = uint8_t;
+
+using i64 = int64_t;
+using i32 = int32_t;
+using i16 = int16_t;
+using i8  = int8_t;
+
+using u64 = uint64_t;
+using u32 = uint32_t;
+using u16 = uint16_t;
+using u8  = uint8_t;
+
+using f32 = float;
+using f64 = double;
