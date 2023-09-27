61c1e42fb13a5ee4e685ac434fc407cbe3dc2b6c
OEOTYAN <1124718975@qq.com>
2023-09-27 20:26:38 +0800

    fix: fix errors with extra class

--- src/mc/deps/core/common/bedrock/ThreadLocalObject.h
+++ src/mc/deps/core/common/bedrock/ThreadLocalObject.h
@@ -4,23 +4,12 @@
 
 namespace Bedrock::Threading {
 
-template <typename T0, typename T1>
-class ThreadLocalObjectImplementation {
+template <typename T, typename Alloc>
+class ThreadLocalObject {
 
 public:
-    T0* mPtr; // this+0x0
-
-    // prevent constructor by default
-    ThreadLocalObjectImplementation& operator=(ThreadLocalObjectImplementation const&) = delete;
-    ThreadLocalObjectImplementation(ThreadLocalObjectImplementation const&)            = delete;
-    ThreadLocalObjectImplementation()                                                  = delete;
-};
-
-template <typename T0, typename T1 = std::allocator<T0>>
-class ThreadLocalObject : public ThreadLocalObjectImplementation<T0, T1> {
-
-public:
-    std::function<void(T0*)> mConstructor; // this+0x8
+    T*                      mPtr;         // this+0x0
+    std::function<void(T*)> mConstructor; // this+0x8
 
     // prevent constructor by default
     ThreadLocalObject& operator=(ThreadLocalObject const&);
