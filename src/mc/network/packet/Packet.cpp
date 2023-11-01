31bcdc363093af51204ad79d00e58c91798422dc
OEOTYAN <1124718975@qq.com>
2023-11-01 14:25:50 +0800

    fix: fix packet send bug

--- src/mc/network/packet/Packet.cpp
+++ src/mc/network/packet/Packet.cpp
@@ -12,7 +12,8 @@
 void Packet::sendTo(Player const& player) { player.sendNetworkPacket(*this); }
 
 void Packet::sendTo(BlockPos const& pos, DimensionType type, optional_ref<Player const> except) const {
-    ll::Global<Minecraft>->getLevel()->getDimension(type)->sendPacketForPosition(pos, *this, except.as_ptr());
+    if (!ll::Global<Level>) { return; }
+    ll::Global<Level>->getDimension(type)->sendPacketForPosition(pos, *this, except.as_ptr());
 }
 
 void Packet::sendTo(Actor const& actor, optional_ref<Player const> except) const {
@@ -20,11 +21,13 @@ void Packet::sendTo(Actor const& actor, optional_ref<Player const> except) const
 }
 
 void Packet::sendToClient(NetworkIdentifier const& id, SubClientId clientId) const {
-    ll::Global<Minecraft>->getLevel()->getPacketSender()->sendToClient(id, *this, clientId);
+    if (!ll::Global<Level>) { return; }
+    ll::Global<Level>->getPacketSender()->sendToClient(id, *this, clientId);
 }
 
 void Packet::sendToClients() {
-    ll::Global<Minecraft>->getLevel()->forEachPlayer([this](Player const& player) -> bool {
+    if (!ll::Global<Level>) { return; }
+    ll::Global<Level>->forEachPlayer([this](Player const& player) -> bool {
         player.sendNetworkPacket(*this);
         return true;
     });
