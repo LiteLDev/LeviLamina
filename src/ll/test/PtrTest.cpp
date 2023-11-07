a54e066e34b18b70576ec8e9ffbec7fdb60903a2
OEOTYAN <1124718975@qq.com>
2023-11-07 18:01:09 +0800

    fix: fix hashedString

--- src/ll/test/PtrTest.cpp
+++ src/ll/test/PtrTest.cpp
@@ -1,13 +1,17 @@
 #include "ll/api/memory/Hook.h"
 #include "mc/server/ServerInstance.h"
 #include "mc/world/events/ServerInstanceEventCoordinator.h"
-#include "mc/world/level/material/Material.h"
 #include "mc/world/level/block/Block.h"
 #include "mc/world/level/block/BlockLegacy.h"
 #include "mc/world/level/block/registry/BlockTypeRegistry.h"
+#include "mc/world/level/material/Material.h"
 
 auto test() {
     auto exists = BlockTypeRegistry::lookupByName("minecraft:stone");
+
+    std::cout << HashedString("").hash << std::endl;
+    std::cout << HashedString::computeHash("") << std::endl;
+
     std::cout << "123" << std::endl;
     if (exists) { std::cout << exists->getTypeName() << std::endl; }
     return exists;
