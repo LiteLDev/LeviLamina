355a146a13ce385f1b3ab892bbd9390e37803d08
killcerr <kill_cerr@outlook.com>
2023-12-02 15:03:40 +0800

    fix: add missing functions

--- src/mc/world/level/levelgen/structure/StructureRedstoneSaveMode.h
+++ src/mc/world/level/levelgen/structure/StructureRedstoneSaveMode.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #include "mc/_HeaderOutputPredefine.h"
+#include "mc/deps/core/common/bedrock/typeid_t.h"
 
 enum class StructureRedstoneSaveMode : schar {
     SavesToMemory = 0x0,
     SavesToDisk   = 0x1,
 };
 
-#include "mc/deps/core/common/bedrock/typeid_t.h"
 MCTAPI class Bedrock::typeid_t<CommandRegistry>
 Bedrock::type_id<class CommandRegistry, enum class StructureRedstoneSaveMode>(void);
