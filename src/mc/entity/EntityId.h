947da41919c8b5476972627ba27365421ecaefb9
OEOTYAN <1124718975@qq.com>
2023-12-04 00:10:53 +0800

    fix: fix EntityId constants

--- src/mc/entity/EntityId.h
+++ src/mc/entity/EntityId.h
@@ -1,15 +1,20 @@
 #pragma once
 
 #include "mc/_HeaderOutputPredefine.h"
+#include "mc/entity/EntityIdTraits.h"
 
-class EntityId : public entt::entt_traits<entt::id_type> {
+class EntityId : public entt::basic_entt_traits<EntityIdTraits> {
 public:
-    entity_type mRawId;
+    entity_type mRawId{};
 
     template <std::integral T>
-    [[nodiscard]] constexpr EntityId(T rawId) : mRawId(static_cast<entity_type>(rawId)) {}
+        requires(!std::is_same_v<std::remove_cvref_t<T>, bool>)
+    [[nodiscard]] constexpr EntityId(T rawId) : mRawId(static_cast<entity_type>(rawId)) {} // NOLINT
+
+    [[nodiscard]] constexpr bool isNull() const { return *this == entt::null; }
 
     template <std::integral T>
+        requires(!std::is_same_v<std::remove_cvref_t<T>, bool>)
     [[nodiscard]] constexpr operator T() const {
         return static_cast<T>(mRawId);
     }
