c239b2b2de4f64307ceec78a8b500739b83905f8
Lovelylavender4 <MiracleForest@Outlook.com>
2024-01-26 21:25:49 +0800

    feat: to complete the member variables for Pos2d, the size for Block, and the parent class list for DividedPos2d.

--- src/mc/deps/core/data/DividedPos2d.h
+++ src/mc/deps/core/data/DividedPos2d.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include "mc/_HeaderOutputPredefine.h"
+#include "mc/world/level/Pos2d.h"
 
 template <int T0>
-class DividedPos2d {
+class DividedPos2d : public ::Pos2d {
 public:
     // prevent constructor by default
     DividedPos2d& operator=(DividedPos2d const&);
