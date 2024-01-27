a2ff8dec3bd39c274881a92304edccdc3a7458b6
NahidaChan <contact@nahidachan.com>
2024-01-27 14:00:20 +0800

    feat: add member variables to Aquifer::FluidSample

--- src/mc/world/level/levelgen/v1/Aquifer.h
+++ src/mc/world/level/levelgen/v1/Aquifer.h
@@ -1,52 +1,53 @@
 #pragma once
 
 #include "mc/_HeaderOutputPredefine.h"
+#include "mc/world/level/BlockPos.h"
+#include "mc/world/level/block/Block.h"
 #include "mc/world/level/levelgen/synth/AquiferNoises.h"
 #include "mc/world/level/levelgen/synth/OverworldNoises3d.h"
 #include "mc/world/level/levelgen/v1/SurfaceLevelCache.h"
-#include "mc/world/level/block/Block.h"
+#include <cstddef>
 
 class Aquifer {
 public:
-    // Aquifer inner types declare
-    AquiferNoises*     mAquiferNoises;             // 0
-    OverworldNoises3d* mOverworldNoises3d;         // 8
-    SurfaceLevelCache* mSurfaceLevelCache;         // 16
-    int                seaLevel;                   // 24
-    int                mLastFluidLevel;            // 28
-    float              mLastBarrier;               // 32
-    char               filler1[1];                 // 36
-    bool               mShouldScheduleFluidUpdate; // 37
-    char               filler2[50];                // 38
-    Block*             mWaterBlock;                // 88
-    Block*             mLavaBlock;                 // 96
-    Block*             mFlowingWaterBlock;         // 104
-    Block*             mFlowingLavaBlock;          // 112
-    // clang-format off
-    struct FluidSample;
-    // clang-format on
-
     // Aquifer inner types define
     struct FluidSample {
     public:
-        char filler[20];
-    };
+        enum FluidType : int8_t {};
 
-public:
-    // prevent constructor by default
-    Aquifer& operator=(Aquifer const&);
-    Aquifer(Aquifer const&);
-    Aquifer();
+        BlockPos  pos;   // 0
+        int       level; // 12
+        FluidType type;  // 16
+    };
+    static_assert(sizeof(FluidSample) == 20, "assert failed: sizeof(FluidSample) != 20");
+
+    // Aquifer inner types declare
+    AquiferNoises*                    mAquiferNoises;             // 0
+    OverworldNoises3d*                mOverworldNoises3d;         // 8
+    SurfaceLevelCache*                mSurfaceLevelCache;         // 16
+    int                               seaLevel;                   // 24
+    int                               mLastFluidLevel;            // 28
+    float                             mLastBarrier;               // 32
+    Aquifer::FluidSample::FluidType   fluidType;                  // 36
+    bool                              mShouldScheduleFluidUpdate; // 37
+    char                              filler_38[2];               // 38
+    BlockPos                          unk_pos_40;                 // 40
+    std::vector<Aquifer::FluidSample> unk_vec_52;                 // 52
+    char                              filler_76[8];               // 76
+    Block*                            mWaterBlock;                // 88 Aquifer::getLastFluidBlockType
+    Block*                            mLavaBlock;                 // 96
+    Block*                            mFlowingWaterBlock;         // 104
+    Block*                            mFlowingLavaBlock;          // 112
 
 public:
     // NOLINTBEGIN
     // symbol: ??0Aquifer@@QEAA@AEBVChunkPos@@AEBVAquiferNoises@@AEBUOverworldNoises3d@@AEBVSurfaceLevelCache@@HHH@Z
     MCAPI Aquifer(
-        class ChunkPos const& chunkPos,
-        class AquiferNoises const&,
+        class ChunkPos const&      chunkPos,
+        class AquiferNoises const& aquiferNoises,
         struct OverworldNoises3d const&,
-        class SurfaceLevelCache const&,
-        int,
+        class SurfaceLevelCache const& surfaceLevelCache,
+        int                            minHeight,
         int,
         int seaLevel
     );
@@ -89,3 +90,6 @@ public:
 
     // NOLINTEND
 };
+
+static_assert(offsetof(Aquifer, Aquifer::unk_pos_40) == 40, "assert failed: offsetof(Aquifer::unk_pos_40) != 40");
+static_assert(offsetof(Aquifer, Aquifer::mWaterBlock) == 88, "assert failed: offsetof(Aquifer::mWaterBlock) != 88");
