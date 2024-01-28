93428b2517afb5a1f3a6fa2a998532547a4e9485
OEOTYAN <OEOTYAN@outlook.com>
2024-01-28 14:15:26 +0800

    refactor: remove DimensionUtils

--- src/ll/core/dimension/FakeDimensionId.cpp
+++ src/ll/core/dimension/FakeDimensionId.cpp
@@ -30,9 +30,6 @@
 #include "mc/world/level/ChangeDimensionRequest.h"
 #include "mc/world/level/Level.h"
 
-#include "ll/api/dimension/DimensionUtils.h"
-
-
 // ChangeDimensionPacket.java
 // ClientboundMapItemDataPacket.java
 // RemoveVolumeEntityPacket.java
@@ -45,6 +42,58 @@
 namespace ll::dimension {
 
 static ll::Logger logger("FakeDimensionId");
+
+static void sendEmptyChunk(const NetworkIdentifier& netId, int chunkX, int chunkZ, bool forceUpdate) {
+    std::array<uchar, 4096> biome{};
+    LevelChunkPacket        levelChunkPacket;
+    BinaryStream            binaryStream{levelChunkPacket.mSerializedChunk, false};
+    VarIntDataOutput        varIntDataOutput(&binaryStream);
+
+    varIntDataOutput.writeBytes(&biome, 4096); // write void biome
+    for (int i = 1; i < 8; i++) {
+        varIntDataOutput.writeByte((127 << 1) | 1);
+    }
+    varIntDataOutput.mStream->writeUnsignedChar(0); // write border blocks
+
+    levelChunkPacket.mPos.x          = chunkX;
+    levelChunkPacket.mPos.z          = chunkZ;
+    levelChunkPacket.mCacheEnabled   = false;
+    levelChunkPacket.mSubChunksCount = 0;
+
+    ll::service::getLevel()->getPacketSender()->sendToClient(netId, levelChunkPacket, SubClientId::PrimaryClient);
+
+    if (forceUpdate) {
+        NetworkBlockPosition pos{chunkX << 4, 80, chunkZ << 4};
+        UpdateBlockPacket    blockPacket;
+        blockPacket.mPos         = pos;
+        blockPacket.mLayer       = UpdateBlockPacket::BlockLayer::Standard;
+        blockPacket.mUpdateFlags = BlockUpdateFlag::Neighbors;
+        ll::service::getLevel()->getPacketSender()->sendToClient(netId, blockPacket, SubClientId::PrimaryClient);
+    }
+}
+
+static void sendEmptyChunks(const NetworkIdentifier& netId, const Vec3& position, int radius, bool forceUpdate) {
+    int chunkX = (int)(position.x) >> 4;
+    int chunkZ = (int)(position.z) >> 4;
+    for (int x = -radius; x <= radius; x++) {
+        for (int z = -radius; z <= radius; z++) {
+            sendEmptyChunk(netId, chunkX + x, chunkZ + z, forceUpdate);
+        }
+    }
+}
+
+static void fakeChangeDimension(
+    const NetworkIdentifier& netId,
+    ActorRuntimeID           runtimeId,
+    DimensionType            fakeDimId,
+    const Vec3&              pos
+) {
+    ChangeDimensionPacket changeDimensionPacket{fakeDimId, pos, true};
+    ll::service::getLevel()->getPacketSender()->sendToClient(netId, changeDimensionPacket, SubClientId::PrimaryClient);
+    PlayerActionPacket playerActionPacket{PlayerActionType::ChangeDimensionAck, runtimeId};
+    ll::service::getLevel()->getPacketSender()->sendToClient(netId, playerActionPacket, SubClientId::PrimaryClient);
+    sendEmptyChunks(netId, pos, 3, true);
+}
 
 namespace CustomDimensionHookList {
 
@@ -314,7 +363,7 @@ LL_TYPE_INSTANCE_HOOK(
         || player.isDead()) {
         return origin(player, std::move(changeRequest));
     }
-    DimensionUtils::fakeChangeDimension(
+    fakeChangeDimension(
         player.getNetworkIdentifier(),
         player.getRuntimeID(),
         ll::dimension::FakeDimensionId::temporaryDimId,
