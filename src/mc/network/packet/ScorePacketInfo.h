7b8a86df4be6e4fe408a5ddc6cb1e1706a98229f
ShrBox <shrbox@outlook.com>
2024-01-30 15:18:49 +0800

    refactor: remove constructor of ScorePacketInfo

--- src/mc/network/packet/ScorePacketInfo.h
+++ src/mc/network/packet/ScorePacketInfo.h
@@ -16,19 +16,5 @@ public:
     ActorUniqueID            mEntityId;       // this+0x40
     std::string              mFakePlayerName; // this+0x48
 
-public:
-    // prevent constructor by default
-    ScorePacketInfo& operator=(ScorePacketInfo const&);
-    ScorePacketInfo(ScorePacketInfo const&);
-    ScorePacketInfo();
-
-public:
-    // NOLINTBEGIN
-    // symbol: ??0ScorePacketInfo@@QEAA@$$QEAU0@@Z
-    MCAPI ScorePacketInfo(struct ScorePacketInfo&&);
-
-    // symbol: ??1ScorePacketInfo@@QEAA@XZ
-    MCAPI ~ScorePacketInfo();
-
     // NOLINTEND
 };
