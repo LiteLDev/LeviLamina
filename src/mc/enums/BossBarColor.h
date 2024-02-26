7fa0a5aab20e23e2b60a11febdceb5f2dd782407
dofes <91889957+dofes@users.noreply.github.com>
2024-02-26 19:26:23 +0800

    feat: add new BossBarColor enum value

--- src/mc/enums/BossBarColor.h
+++ src/mc/enums/BossBarColor.h
@@ -3,11 +3,12 @@
 #include "mc/_HeaderOutputPredefine.h"
 
 enum class BossBarColor : int {
-    Pink   = 0x0,
-    Blue   = 0x1,
-    Red    = 0x2,
-    Green  = 0x3,
-    Yellow = 0x4,
-    Purple = 0x5,
-    White  = 0x6,
+    Pink          = 0x0,
+    Blue          = 0x1,
+    Red           = 0x2,
+    Green         = 0x3,
+    Yellow        = 0x4,
+    Purple        = 0x5,
+    RebeccaPurple = 0x5,
+    White         = 0x6,
 };
