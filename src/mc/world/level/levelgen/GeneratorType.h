a81af59a43087cdbc36655d57337f18c464e7c44
banchen19 <banchen19@outlook.com>
2024-02-28 03:53:10 +0800

    Update GeneratorType.h
    添加一点点的注释

--- src/mc/world/level/levelgen/GeneratorType.h
+++ src/mc/world/level/levelgen/GeneratorType.h
@@ -3,11 +3,11 @@
 #include "mc/_HeaderOutputPredefine.h"
 
 enum class GeneratorType : int {
-    Legacy    = 0x0,
-    Overworld = 0x1,
-    Flat      = 0x2,
-    Nether    = 0x3,
-    TheEnd    = 0x4,
-    Void      = 0x5,
-    Undefined = 0x6,
+    Legacy    = 0x0,       // 经典（遗留）模式
+    Overworld = 0x1,       // 主世界
+    Flat      = 0x2,       // 平坦世界
+    Nether    = 0x3,       // 地狱世界
+    TheEnd    = 0x4,       // 末地世界
+    Void      = 0x5,       // 虚空世界
+    Undefined = 0x6,       // 未定义
 };
