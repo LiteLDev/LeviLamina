8513cfae630c7ccc7f7f9c0aea72d9c6ef96330c
ShrBox <shrbox@outlook.com>
2024-02-29 16:58:03 +0800

    refactor(command): reactive -> reactivate

--- src/ll/core/command/PluginManage.cpp
+++ src/ll/core/command/PluginManage.cpp
@@ -22,7 +22,7 @@ namespace ll::command {
 using namespace ll::i18n_literals;
 
 enum LeviCommandOperation : int { load = 0, unload = 1, reload = 2 };
-enum LeviCommandOperation2 : int { enable = 0, disable = 1, reactive = 2, show = 3 };
+enum LeviCommandOperation2 : int { enable = 0, disable = 1, reactivate = 2, show = 3 };
 struct LeviCommand {
     LeviCommandOperation operation;
     std::string          plugin;
@@ -135,13 +135,13 @@ void registerPluginManageCommand() {
                 }
                 break;
             }
-            case LeviCommandOperation2::reactive:
+            case LeviCommandOperation2::reactivate:
                 if (ll::plugin::PluginManagerRegistry::getInstance().hasPlugin(param.plugin)) {
                     if (ll::plugin::PluginRegistrar::getInstance().disablePlugin(param.plugin)
                         && ll::plugin::PluginRegistrar::getInstance().enablePlugin(param.plugin)) {
-                        output.success("Reactive plugin {0} successfully"_tr(param.plugin));
+                        output.success("Reactivate plugin {0} successfully"_tr(param.plugin));
                     } else {
-                        output.error("Failed to reactive plugin {0}"_tr(param.plugin));
+                        output.error("Failed to reactivate plugin {0}"_tr(param.plugin));
                     }
                 } else {
                     output.error("Plugin {0} not found"_tr(param.plugin));
