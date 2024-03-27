c2291c2a122a2a99c8e9600d1b9e98cec85c1d54
ShrBox <shrbox@outlook.com>
2024-03-27 13:09:08 +0800

    feat: add member to GenerateMessageResult

--- src/mc/common/wrapper/GenerateMessageResult.h
+++ src/mc/common/wrapper/GenerateMessageResult.h
@@ -1,9 +1,13 @@
 #pragma once
 
 #include "mc/_HeaderOutputPredefine.h"
+#include "mc/server/commands/CommandSelector.h"
 
 struct GenerateMessageResult {
 public:
+    std::string                                   string;
+    std::unique_ptr<CommandSelector<class Actor>> selection;
+
     // prevent constructor by default
     GenerateMessageResult& operator=(GenerateMessageResult const&);
     GenerateMessageResult(GenerateMessageResult const&);
