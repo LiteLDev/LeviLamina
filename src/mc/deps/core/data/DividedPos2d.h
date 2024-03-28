0f9193fe681f27a69692e5ee73e81d6730f72ccb
Qiuzhizhe <42761326+quizhizhe@users.noreply.github.com>
2024-03-28 02:21:13 -0700

    fix: fix #1500 and complete `DividedPos2d`

--- src/mc/deps/core/data/DividedPos2d.h
+++ src/mc/deps/core/data/DividedPos2d.h
@@ -7,7 +7,14 @@ template <int T0>
 class DividedPos2d : public ::Pos2d {
 public:
     // prevent constructor by default
-    DividedPos2d& operator=(DividedPos2d const&);
-    DividedPos2d(DividedPos2d const&);
+    DividedPos2d<T0>& operator=(DividedPos2d<T0> const& other) {
+        x = other.x;
+        z = other.x;
+        return *this;
+    };
+    DividedPos2d<T0>(DividedPos2d<T0> const& other) {
+        x = other.x;
+        z = other.x;
+    }
     DividedPos2d() = default;
 };
