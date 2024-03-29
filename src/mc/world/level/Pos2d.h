6ecf8432f318c0eb3c5cff77930b7f253ffeb013
OEOTYAN <OEOTYAN@outlook.com>
2024-03-29 21:00:06 +0800

    refactor: remove useless code

--- src/mc/world/level/Pos2d.h
+++ src/mc/world/level/Pos2d.h
@@ -2,14 +2,7 @@
 
 #include "mc/_HeaderOutputPredefine.h"
 
-class Pos2d {
+class Pos2d : public ll::math::intN2<Pos2d> {
 public:
-    int x; // this+0x0
-    int z; // this+0x4
-
-public:
-    // prevent constructor by default
-    Pos2d& operator=(Pos2d const&);
-    Pos2d(Pos2d const&);
-    Pos2d() = default;
+    using intN2::intN2;
 };
