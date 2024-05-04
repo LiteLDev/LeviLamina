792529c63d4049dea0fd6f46c12ac735f70dd8c0
Lovelylavender4 <MiracleForest@Outlook.com>
2024-05-04 20:32:12 +0800

    feat: add parameter names to VillageFeature to make its member function formatting look better.

--- src/mc/world/level/levelgen/structure/VillageFeature.h
+++ src/mc/world/level/levelgen/structure/VillageFeature.h
@@ -27,24 +27,36 @@ public:
 
     // vIndex: 3, symbol:
     // ?getNearestGeneratedFeature@VillageFeature@@UEAA_NAEAVDimension@@AEBVBiomeSource@@AEBVBlockPos@@AEAV4@AEBVIPreliminarySurfaceProvider@@_NAEBV?$optional@VHashedString@@@std@@@Z
-    virtual bool
-    getNearestGeneratedFeature(class Dimension&, class BiomeSource const&, class BlockPos const&, class BlockPos&, class IPreliminarySurfaceProvider const&, bool, std::optional<class HashedString> const&);
+    virtual bool getNearestGeneratedFeature(
+        class Dimension&                         dim,
+        class BiomeSource const&                 biomeSource,
+        class BlockPos const&                    pos,
+        class BlockPos&                          nearestPos,
+        class IPreliminarySurfaceProvider const& provider,
+        bool                                     b,
+        std::optional<class HashedString> const& hs
+    );
 
     // vIndex: 5, symbol:
     // ?isFeatureChunk@VillageFeature@@UEAA_NAEBVBiomeSource@@AEAVRandom@@AEBVChunkPos@@IAEBVIPreliminarySurfaceProvider@@AEBVDimension@@@Z
     virtual bool isFeatureChunk(
-        class BiomeSource const& biomeSource,
-        class Random&            random,
-        class ChunkPos const&    chunkPos,
-        uint                     levelSeed,
-        class IPreliminarySurfaceProvider const&,
-        class Dimension const& dimension
+        class BiomeSource const&                 biomeSource,
+        class Random&                            random,
+        class ChunkPos const&                    chunkPos,
+        uint                                     levelSeed,
+        class IPreliminarySurfaceProvider const& provider,
+        class Dimension const&                   dimension
     );
 
     // vIndex: 6, symbol:
     // ?createStructureStart@VillageFeature@@MEAA?AV?$unique_ptr@VStructureStart@@U?$default_delete@VStructureStart@@@std@@@std@@AEAVDimension@@AEBVBiomeSource@@AEAVRandom@@AEBVChunkPos@@AEBVIPreliminarySurfaceProvider@@@Z
-    virtual std::unique_ptr<class StructureStart>
-    createStructureStart(class Dimension& generator, class BiomeSource const& biomeSource, class Random& random, class ChunkPos const& chunkPos, class IPreliminarySurfaceProvider const&);
+    virtual std::unique_ptr<class StructureStart> createStructureStart(
+        class Dimension&                         generator,
+        class BiomeSource const&                 biomeSource,
+        class Random&                            random,
+        class ChunkPos const&                    chunkPos,
+        class IPreliminarySurfaceProvider const& provider
+    );
 
     // symbol: ??0VillageFeature@@QEAA@IHH@Z
     MCAPI VillageFeature(uint seed, int townSpacing, int minTownSeparation);
