192e1c9120454621c92f97611f97ac9f8183f9c3
Lovelylavender4 <MiracleForest@Outlook.com>
2024-06-10 16:32:43 +0800

    style: alignment.

--- src/mc/network/packet/ActorLink.h
+++ src/mc/network/packet/ActorLink.h
@@ -5,9 +5,9 @@
 #include "mc/world/ActorUniqueID.h"
 
 struct ActorLink {
-    ActorLinkType mType;                // this+0x0
-    ActorUniqueID mA;                   // this+0x8
-    ActorUniqueID mB;                   // this+0x10
+    ActorLinkType mType;               // this+0x0
+    ActorUniqueID mA;                  // this+0x8
+    ActorUniqueID mB;                  // this+0x10
     bool          mImmediate;          // this+0x18
     bool          mPassengerInitiated; // this+0x19
 
