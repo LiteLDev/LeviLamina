446a1e060501980946c2f0258feb09b103501728
Lovelylavender4 <MiracleForest@Outlook.com>
2024-07-13 23:29:19 +0800

    feat: completed the parent class of MultidimensionalArray.

--- src/mc/deps/core/utility/MultidimensionalArray.h
+++ src/mc/deps/core/utility/MultidimensionalArray.h
@@ -5,7 +5,7 @@
 namespace Util {
 
 template <typename T0, int T1, int T2, int T3>
-class MultidimensionalArray {
+class MultidimensionalArray : public std::array<T0, T1 * T2 * T3> {
 public:
     // prevent constructor by default
     MultidimensionalArray& operator=(MultidimensionalArray const&);
