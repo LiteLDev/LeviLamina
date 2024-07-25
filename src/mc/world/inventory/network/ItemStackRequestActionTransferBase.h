f673095d44424f67674e9883205fd3b53551ae28
Lovelylavender4 <MiracleForest@Outlook.com>
2024-07-25 16:08:01 +0800

    feat: completes the member variables of the ItemStackRequestActionTransferBase class.

--- src/mc/world/inventory/network/ItemStackRequestActionTransferBase.h
+++ src/mc/world/inventory/network/ItemStackRequestActionTransferBase.h
@@ -1,12 +1,20 @@
 #pragma once
 
 #include "mc/_HeaderOutputPredefine.h"
+#include "mc/world/inventory/network/ItemStackRequestSlotInfo.h"
 
 // auto generated inclusion list
 #include "mc/deps/core/common/bedrock/Result.h"
 #include "mc/world/inventory/network/ItemStackRequestAction.h"
 
 class ItemStackRequestActionTransferBase : public ::ItemStackRequestAction {
+public:
+    bool                     mIsDstSerialized;
+    bool                     mIsAmountSerialized;
+    uchar                    mAmount;
+    ItemStackRequestSlotInfo mSrc;
+    ItemStackRequestSlotInfo mDst;
+
 public:
     // prevent constructor by default
     ItemStackRequestActionTransferBase& operator=(ItemStackRequestActionTransferBase const&);
