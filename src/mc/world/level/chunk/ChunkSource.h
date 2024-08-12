53f8682cb51977da167722481b7302245ebd8d72
Tozic <70832788+xNotTozic@users.noreply.github.com>
2024-08-12 15:47:38 +0400

    feat: Enhanced Chunk classes with additional member variables (#1567)
    * Finalize InventoryTransaction member variable implementation
    
    * Rectify mc class member nomenclature in ItemReleaseInventoryTransaction
    
    * Rectify mc class member nomenclature in ItemUseInventoryTransaction
    
    * Rectify mc class member nomenclature in ItemUseOnActorInventoryTransaction
    
    * fix: correct mc member name
    
    * Enhanced Chunk class with additional member variables for improved functionality
    
    ---------
    
    Co-authored-by: OEOTYAN <oeotyan@outlook.com>

--- src/mc/world/level/chunk/ChunkSource.h
+++ src/mc/world/level/chunk/ChunkSource.h
@@ -37,7 +37,7 @@ public:
     LevelChunkBuilderData*                                  mLevelChunkBuilderData; // this+0x40
     std::atomic_bool                                        mShuttingDown;          // this+0x48
     char                                                    pad_0x0049[0x17];       // the other 3 member variables
-    std::unordered_map<ChunkPos, std::weak_ptr<LevelChunk>> chunkStorage;           // this+0x60
+    std::unordered_map<ChunkPos, std::weak_ptr<LevelChunk>> mChunkStorage;          // this+0x60
 
     // uchar filler[0x70 - sizeof(Bedrock::EnableNonOwnerReferences)];
 
--- src/mc/world/level/chunk/SubChunkStorage.h
+++ src/mc/world/level/chunk/SubChunkStorage.h
@@ -5,6 +5,9 @@
 template <typename T0>
 class SubChunkStorage {
 public:
+    uint16_t mElements[1368]; // this+0x8
+    T0*      mTypes;          // this+0xAB8
+public:
     // prevent constructor by default
     SubChunkStorage& operator=(SubChunkStorage const&);
     SubChunkStorage(SubChunkStorage const&);
