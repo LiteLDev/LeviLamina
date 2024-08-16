556d973550dd035c5763c590f2a00ec7fbeb4c66
OEOTYAN <OEOTYAN@outlook.com>
2024-08-16 18:03:26 +0800

    refactor: refactoring StdoutRedirector

--- src/ll/api/io/StdoutRedirector.cpp
+++ src/ll/api/io/StdoutRedirector.cpp
@@ -8,21 +8,22 @@
 
 namespace ll::io {
 
-StdoutRedirector::StdoutRedirector(ProcessChannel channels) : m_channels(channels) {
+StdoutRedirector::StdoutRedirector(void* outputHandle, ProcessChannel channels)
+: outputHandle(outputHandle),
+  m_channels(channels) {
     if (m_channels & StandardOutput) {
         setvbuf(stdout, NULL, _IONBF, 0);
-        ::SetStdHandle(STD_OUTPUT_HANDLE, hWrite);
+        ::SetStdHandle(STD_OUTPUT_HANDLE, outputHandle);
     }
     if (m_channels & StandardError) {
         setvbuf(stderr, NULL, _IONBF, 0);
-        ::SetStdHandle(STD_ERROR_HANDLE, hWrite);
+        ::SetStdHandle(STD_ERROR_HANDLE, outputHandle);
     }
-    // int fd = _open_osfhandle((intptr_t)hWrite, _O_WRONLY | _O_U8TEXT);
-    // if (m_channels & StandardOutput) _dup2(fd, 1);
-    // if (m_channels & StandardError) _dup2(fd, 2);
-    // _close(fd);
+    int fd = _open_osfhandle((intptr_t)outputHandle, _O_WRONLY | _O_U8TEXT);
+    if (m_channels & StandardOutput) _dup2(fd, 1);
+    if (m_channels & StandardError) _dup2(fd, 2);
+    _close(fd);
 }
-
 StdoutRedirector::~StdoutRedirector() {
     if (m_channels & StandardOutput) {
         FILE* s;
--- src/ll/api/io/StdoutRedirector.h
+++ src/ll/api/io/StdoutRedirector.h
@@ -2,18 +2,20 @@
 
 #include <string>
 
-#include "ll/api/io/Pipe.h"
-
 namespace ll::io {
 
-class StdoutRedirector : public Pipe {
+class StdoutRedirector {
 public:
     enum ProcessChannel { StandardOutput = 1, StandardError = 2 };
 
-    LLNDAPI explicit StdoutRedirector(ProcessChannel channels = (ProcessChannel)(StandardOutput | StandardError));
+    LLNDAPI explicit StdoutRedirector(
+        void*          outputHandle,
+        ProcessChannel channels = (ProcessChannel)(StandardOutput | StandardError)
+    );
 
     LLAPI ~StdoutRedirector();
 
     ProcessChannel m_channels;
+    void*          outputHandle;
 };
 } // namespace ll::io
