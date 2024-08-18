27a65fa4fdde6a9df0607019e401a543dac30e3b
Tozic <70832788+xNotTozic@users.noreply.github.com>
2024-08-18 11:08:50 +0400

    chore: updated ItemUse

--- src/mc/world/inventory/transaction/ItemUseInventoryTransaction.h
+++ src/mc/world/inventory/transaction/ItemUseInventoryTransaction.h
@@ -17,14 +17,16 @@ public:
         Destroy = 2,
     };
 
-    ActionType                 mActionType{};           // this+0x0
-    BlockPos                   mBlockPos{};             // this+0x4
-    uint32_t                   mTargetBlockRuntimeId{}; // this+0x10
-    uint8_t                    mFace{};                 // this+0x14
-    int32_t                    mSlot{};                 // this+0x18
-    NetworkItemStackDescriptor mItemInHand{};           // this+0x20
-    Vec3                       mPlayerPos{};            // this+0x30
-    Vec3                       mClickPos{};             // this+0x3C
+    ActionType                 mActionType{};                // this+0x0
+    int8_t                     mUnko{}                       // this+0x4
+    BlockPos                   mBlockPos{};                  // this+0x8
+    int32_t                    mTargetBlockRuntimeId{};      // this+0x14
+    uint32_t                   mFace{};                      // this+0x18
+    uint32_t                   mSlot{};                      // this+0x1C
+    NetworkItemStackDescriptor mItemInHand{};                // this+0x20
+    Vec3                       mPlayerPos{};                 // this+0x30
+    Vec3                       mClickPos{};                  // this+0x3C
+    int32_t                    mPre{};                       // this+0x48
 
 public:
     // prevent constructor by default
