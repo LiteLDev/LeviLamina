ea5c3393503e1c8c6c7fd9d7d7618b6fd703d48f
Redbeanw44602 <redbeana44945@gmail.com>
2024-08-19 15:44:29 +0800

    refactor: avoid using non-primitive types in StdInt.h

--- src/ll/api/base/StdInt.h
+++ src/ll/api/base/StdInt.h
@@ -14,12 +14,12 @@ using byte  = uchar;
 
 using ldouble = long double;
 
-using int64 = int64_t;
-using int32 = int32_t;
-using int16 = int16_t;
-using int8  = int8_t;
+using int64 = long long;
+using int32 = int;
+using int16 = short;
+using int8  = char;
 
-using uint64 = uint64_t;
-using uint32 = uint32_t;
-using uint16 = uint16_t;
-using uint8  = uint8_t;
+using uint64 = unsigned long long;
+using uint32 = unsigned int;
+using uint16 = unsigned short;
+using uint8  = unsigned char;
