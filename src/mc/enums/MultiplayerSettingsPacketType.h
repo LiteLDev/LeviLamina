a9f99474fdd515de2ca5c78e8b40d868e0bfdb71
Tozic <70832788+xNotTozic@users.noreply.github.com>
2024-08-27 20:23:06 +0400

    Update MultiplayerSettingsPacketType.h

--- src/mc/enums/MultiplayerSettingsPacketType.h
+++ src/mc/enums/MultiplayerSettingsPacketType.h
@@ -2,4 +2,8 @@
 
 #include "mc/_HeaderOutputPredefine.h"
 
-enum class MultiplayerSettingsPacketType {};
+enum class MultiplayerSettingsPacketType {
+    EnableMultiplayer  = 0,
+    DisableMultiplayer = 1,
+    RefreshJoincode    = 2
+};
