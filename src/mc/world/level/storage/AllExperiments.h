5839b78874c3091e151fe98dc36e6d68b0f82a58
KobeBryant114514 <116721335+KobeBryant114514@users.noreply.github.com>
2024-10-17 15:57:28 +0800

    feat: fill enum AllExperiments

--- src/mc/world/level/storage/AllExperiments.h
+++ src/mc/world/level/storage/AllExperiments.h
@@ -2,4 +2,10 @@
 
 #include "mc/_HeaderOutputPredefine.h"
 
-enum class AllExperiments {};
+enum class AllExperiments {
+    DataDrivenItems         = 0x6,
+    DataDrivenBiomes        = 0x7,
+    UpcomingCreatorFeatures = 0x8,
+    Gametest                = 0x9,
+    VillagerTradesRebalance = 0xE,
+};
