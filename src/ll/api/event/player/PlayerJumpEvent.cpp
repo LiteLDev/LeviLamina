0c5a2e6d183630be5edadbaf0e9fa33c864df75d
QingYu <62042544+dreamguxiang@users.noreply.github.com>
2024-12-29 20:12:25 +0800

    fix: fix PlayerJumpEvent

--- src/ll/api/event/player/PlayerJumpEvent.cpp
+++ src/ll/api/event/player/PlayerJumpEvent.cpp
@@ -2,18 +2,31 @@
 #include "ll/api/event/Emitter.h"
 #include "ll/api/memory/Hook.h"
 
+#include "mc/network/ServerNetworkHandler.h"
+#include "mc/network/packet/PlayerAuthInputPacket.h"
 namespace ll::event::inline player {
-// TODO:
-//  LL_TYPE_INSTANCE_HOOK(PlayerJumpEventHook, HookPriority::Normal, Player, &Player::handleJumpEffects, void) {
-//      EventBus::getInstance().publish(PlayerJumpEvent(*this));
-//      return origin();
-//  }
+LL_AUTO_TYPE_INSTANCE_HOOK(
+    PlayerJumpEventHook,
+    HookPriority::Normal,
+    ServerNetworkHandler,
+    &ServerNetworkHandler::$handle,
+    void,
+    ::NetworkIdentifier const&     source,
+    ::PlayerAuthInputPacket const& packet
+) {
+    if (ServerPlayer* player = _getServerPlayer(source, packet.mClientSubId)) {
+        if (packet.getInput(::PlayerAuthInputPacket::InputData::StartJumping)) {
+            EventBus::getInstance().publish(PlayerJumpEvent(*(Player*)player));
+        }
+    }
+    return origin(source, packet);
+}
 
-// static std::unique_ptr<EmitterBase> emitterFactory();
-// class PlayerJumpEventEmitter : public Emitter<emitterFactory, PlayerJumpEvent> {
-//     memory::HookRegistrar<PlayerJumpEventHook> hook;
-// };
+static std::unique_ptr<EmitterBase> emitterFactory();
+class PlayerJumpEventEmitter : public Emitter<emitterFactory, PlayerJumpEvent> {
+    memory::HookRegistrar<PlayerJumpEventHook> hook;
+};
 
-// static std::unique_ptr<EmitterBase> emitterFactory() { return std::make_unique<PlayerJumpEventEmitter>(); }
+static std::unique_ptr<EmitterBase> emitterFactory() { return std::make_unique<PlayerJumpEventEmitter>(); }
 
 } // namespace ll::event::inline player
