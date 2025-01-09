5e691a2d70e93feea39c49b54ba160fffd3ceb02
Dofes <91889957+Dofes@users.noreply.github.com>
2025-01-10 03:36:30 +0800

    fix: fix save command enum error

--- src/ll/core/command/BuiltinCommands.cpp
+++ src/ll/core/command/BuiltinCommands.cpp
@@ -1,16 +1,16 @@
 #include "ll/core/command/BuiltinCommands.h"
 #include "ll/api/memory/Hook.h"
 
-#include "mc/server/commands/ServerCommands.h"
-#include "mc/server/commands/standard/TeleportCommand.h"
+#include "mc/server/DedicatedServerCommands.h"
 #include "mc/world/events/ServerInstanceEventCoordinator.h"
+
 
 namespace ll::command {
 LL_TYPE_INSTANCE_HOOK(
     RegisterBuiltinCommands,
     ll::memory::HookPriority::Highest,
-    ServerInstanceEventCoordinator,
-    &ServerInstanceEventCoordinator::sendServerInitializeEnd,
+    DedicatedServerCommands,
+    &DedicatedServerCommands::setupStandaloneServer,
     void,
     ::ServerInstance& ins
 ) {
