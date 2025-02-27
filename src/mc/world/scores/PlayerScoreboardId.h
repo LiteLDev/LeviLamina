f24a6882945fe60c156a541ae480526da3cd8c39
ShrBox <shrbox@outlook.com>
2025-02-27 15:47:46 +0800

    feat: add operator== and hash for PlayerScoreboardId

--- src/mc/world/scores/PlayerScoreboardId.h
+++ src/mc/world/scores/PlayerScoreboardId.h
@@ -10,8 +10,18 @@ public:
     // NOLINTEND
 
 public:
+    bool operator==(PlayerScoreboardId const& rhs) const { return mActorUniqueId == rhs.mActorUniqueId; }
+
+public:
     // static variables
     // NOLINTBEGIN
     MCAPI static ::PlayerScoreboardId const& INVALID();
     // NOLINTEND
 };
+
+namespace std {
+template <>
+struct hash<PlayerScoreboardId> {
+    int64 operator()(PlayerScoreboardId const& d) const noexcept { return d.mActorUniqueId; }
+};
+} // namespace std
