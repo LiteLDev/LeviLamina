311f30d330de0fa420c4c05f9740151bb3ebfb07
RimuruChan <RealSprite233@outlook.com>
2025-04-29 22:27:35 +0800

    chore: improve formatter

--- format_all.py
+++ format_all.py
@@ -2,6 +2,22 @@ import os
 import subprocess
 from concurrent.futures import ThreadPoolExecutor
 import multiprocessing
+import threading
+
+FILE_COUNT_PER_TASK = 50
+
+# use atomic counter to count the number of files formatted
+lock = threading.Lock()
+formatted_count = 0
+total_count = 0
+
+
+def finish_count(count):
+    """原子计数器，增加格式化文件的数量，返回格式化的文件数量"""
+    global formatted_count
+    with lock:
+        formatted_count += count
+        return formatted_count
 
 
 def get_all_code_files(directory, extensions):
@@ -14,34 +30,40 @@ def get_all_code_files(directory, extensions):
     return code_files
 
 
-def format_file(file_path, clang_format_path):
-    """使用 clang-format 格式化单个文件"""
+def format_file(file_paths, clang_format_path):
+    """使用 clang-format 格式化若干文件"""
+    args = [clang_format_path, "-i"]
+    for file_path in file_paths:
+        args.append(file_path)
     try:
-        subprocess.run([clang_format_path, "-i", file_path], check=True)
-        subprocess.run([clang_format_path, "-i", file_path], check=True)
-        with open(file_path, "rb") as f:
-            content = f.read()
-        with open(file_path, "wb") as f:
-            f.write(content.replace(b"\n", b"\r\n").replace(b"\r\r", b"\r"))
-        print(f"Formatted: {file_path}")
+        subprocess.run(args, check=True)
+        subprocess.run(args, check=True)
+        msg = f"Formatted: {[os.path.basename(file) for file in file_paths]}"
+        if len(msg) > 50:
+            msg = msg[:50] + "..."
+        count = finish_count(len(file_paths))
+        print(f"{msg} ({count}/{total_count})")
     except subprocess.CalledProcessError as e:
-        print(f"Failed to format {file_path}: {e}")
+        print(f"Failed to format {file_paths}: {e}")
 
 
 def format_code_files(
     directory, clang_format_path, extensions=(".cpp", ".c", ".h"), threads=None
 ):
     """格式化指定目录及其子目录下的所有代码文件"""
+    global total_count
     code_files = get_all_code_files(directory, extensions)
+    total_count = len(code_files)
     print(f"Found {len(code_files)} files to format.")
 
     if threads is None:
-        threads = multiprocessing.cpu_count()
+        threads = max(1, multiprocessing.cpu_count() - 2)
         print(f"Using {threads} threads (number of CPU cores).")
 
     with ThreadPoolExecutor(max_workers=threads) as executor:
-        executor.map(lambda file: format_file(
-            file, clang_format_path), code_files)
+        for i in range(0, len(code_files), FILE_COUNT_PER_TASK):
+            file_batch = code_files[i: i + FILE_COUNT_PER_TASK]
+            executor.submit(format_file, file_batch, clang_format_path)
 
 
 if __name__ == "__main__":
