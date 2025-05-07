f24d4eb6451016c4b1bb1d6f155bb6e9f89113f0
xiaoqch <xiaoqch@foxmail.com>
2025-05-07 19:07:22 +0800

    fix: fix format of NetworkIdentifier::getIPAndPort result
    (cherry picked from commit 6e6a7439830f9d50f310cff8f95ae60f6bf84f27)

--- src/mc/network/NetworkIdentifier.cpp
+++ src/mc/network/NetworkIdentifier.cpp
@@ -1,7 +1,6 @@
 #include "mc/network/NetworkIdentifier.h"
 #include "mc/deps/raknet/RakNet.h"
 #include "mc/deps/raknet/RakPeer.h"
-#include "mc/deps/raknet/SystemAddress.h"
 
 #include "ll/api/service/Bedrock.h"
 
@@ -10,7 +9,18 @@ std::string NetworkIdentifier::getIPAndPort() const {
         .and_then([&](auto& peer) -> std::optional<std::string> {
             auto address = peer.GetSystemAddressFromGuid(mGuid);
             if (address != RakNet::UNASSIGNED_SYSTEM_ADDRESS()) {
-                return address.ToString(':');
+                auto ipAndPort = address.ToString('|');
+                auto result =
+                    std::ranges::views::split(ipAndPort, '|')
+                    | std::views::transform([](auto&& part) { return std::string_view{part.begin(), part.end()}; })
+                    | std::views::take(2) | std::ranges::to<std::vector>();
+                auto& ip   = result[0];
+                auto& port = result[1];
+                if (ip.find(':') != std::string::npos) {
+                    // IPv6 address
+                    return fmt::format("[{0}]:{1}", ip, port);
+                }
+                return fmt::format("{0}:{1}", ip, port);
             }
             return std::nullopt;
         })
