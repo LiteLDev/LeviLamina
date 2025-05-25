c173fe6916554f243d87c9cb3228ef4fea059da7
Dofes <91889957+Dofes@users.noreply.github.com>
2025-05-26 01:27:37 +0800

    feat: complete NetworkedItemComponent

--- src/mc/world/item/components/NetworkedItemComponent.h
+++ src/mc/world/item/components/NetworkedItemComponent.h
@@ -1,6 +1,12 @@
 #pragma once
 
 #include "mc/_HeaderOutputPredefine.h"
+#include "mc/world/item/components/ItemComponent.h"
 
-template <typename T0>
-class NetworkedItemComponent {};
+template <typename T>
+class NetworkedItemComponent : public ItemComponent {
+public:
+    [[nodiscard]] bool                         isNetworkComponent() const override;
+    [[nodiscard]] std::unique_ptr<CompoundTag> buildNetworkTag(const cereal::ReflectionCtx&) const override;
+    bool initializeFromNetwork(const CompoundTag&, const cereal::ReflectionCtx&) override;
+};
