a6c8b62a8c7e6757dba888174319b39a2c640a53
xiaoqch <xiaoqch@foxmail.com>
2025-06-08 00:45:07 +0800

    chore: remove unused hook

--- src/ll/api/event/player/PlayerJumpEvent.cpp
+++ src/ll/api/event/player/PlayerJumpEvent.cpp
@@ -5,50 +5,20 @@
 
 #include "mc/deps/ecs/strict/StrictEntityContext.h"
 #include "mc/entity/components/MoveInputComponent.h"
-#include "mc/entity/components/ServerAuthMovementMode.h"
 #include "mc/entity/components/ServerPlayerCurrentMovementComponent.h"
 #include "mc/input/MoveInputState.h"
 #include "mc/network/ServerNetworkHandler.h"
-#include "mc/network/packet/PlayerActionPacket.h"
 #include "mc/network/packet/PlayerAuthInputPacket.h"
-#include "mc/server/PropertiesSettings.h"
 #include "mc/server/ServerPlayer.h"
 #include "mc/util/ServerMoveInputHandlerSystemUtils.h"
 #include "mc/world/actor/Actor.h"
-#include "mc/world/actor/player/PlayerMovementSettings.h"
 #include "mc/world/level/Level.h"
 
 
 namespace ll::event::inline player {
 
-// client-auth
-LL_TYPE_INSTANCE_HOOK(
-    PlayerJumpEventHook1,
-    HookPriority::Normal,
-    ServerNetworkHandler,
-    &ServerNetworkHandler::$handle,
-    void,
-    NetworkIdentifier const&  source,
-    PlayerActionPacket const& packet
-) {
-    if (auto settings = ll::service::getPropertiesSettings(); settings) {
-        auto& mode = settings->mPlayerMovementSettings->AuthorityMode;
-        // TODO:
-        //  if (mode == ServerAuthMovementMode::LegacyClientAuthoritativeV1) {
-        //      if (packet.mAction == PlayerActionType::StartJump) {
-        //          if (auto player = thisFor<NetEventCallback>()->_getServerPlayer(source, packet.mSenderSubId);
-        //          player) {
-        //              EventBus::getInstance().publish(PlayerJumpEvent(*player));
-        //          }
-        //      }
-        //  }
-    }
-    return origin(source, packet);
-}
-
-// server-auth
 LL_STATIC_HOOK(
-    PlayerJumpEventHook2,
+    PlayerJumpEventHook,
     HookPriority::Normal,
     &ServerMoveInputHandlerSystemUtils::_tickServerMoveInputHandler,
     void,
@@ -95,8 +65,7 @@ LL_STATIC_HOOK(
 
 static std::unique_ptr<EmitterBase> emitterFactory();
 class PlayerJumpEventEmitter : public Emitter<emitterFactory, PlayerJumpEvent> {
-    memory::HookRegistrar<PlayerJumpEventHook1> hook1;
-    memory::HookRegistrar<PlayerJumpEventHook2> hook2;
+    memory::HookRegistrar<PlayerJumpEventHook> hook;
 };
 
 static std::unique_ptr<EmitterBase> emitterFactory() { return std::make_unique<PlayerJumpEventEmitter>(); }
