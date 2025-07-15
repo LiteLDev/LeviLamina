bc79572c3ff299d1ef044410045bb7a6f03ca586
ShrBox <shrbox@outlook.com>
2025-07-15 18:45:37 +0800

    feat: complete CopperBlock

--- src/mc/world/level/block/CopperBlock.h
+++ src/mc/world/level/block/CopperBlock.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include "mc/world/level/block/CopperBehavior.h"
 #include "mc/_HeaderOutputPredefine.h"
 
 template <typename T0>
-class CopperBlock {};
+class CopperBlock : public T0 {
+public:
+    CopperBehavior mCopperBehavior;
+};
