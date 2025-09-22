9bbb8c312bb7944d60a8fb68d04724031af1c020
ShrBox <shrbox@outlook.com>
2025-09-22 22:56:43 +0800

    chore: bump bedrockdata to v1.21.102-server.1

--- xmake.lua
+++ xmake.lua
@@ -58,7 +58,7 @@ end
 
 if is_server then
     if is_windows then
-        add_requires("bedrockdata v1.21.93-server.6")
+        add_requires("bedrockdata v1.21.102-server.1")
     end
 else
     if is_windows then
