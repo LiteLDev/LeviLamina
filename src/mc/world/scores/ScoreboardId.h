59d5bf0412e26c41576e9be1667d48b552a766d9
ShrBox <shrbox@outlook.com>
2025-10-02 15:43:25 +0800

    feat: add std::hash<::ScoreboardId>

--- src/mc/world/scores/ScoreboardId.h
+++ src/mc/world/scores/ScoreboardId.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "mc/_HeaderOutputPredefine.h"
+#include "mc/legacy/ActorUniqueID.h"
 
 // auto generated forward declare list
 // clang-format off
@@ -16,8 +17,22 @@ public:
     // NOLINTEND
 
 public:
+    bool operator==(ScoreboardId const& other) const { return mRawID == other.mRawID; }
+
+public:
     // static variables
     // NOLINTBEGIN
     MCAPI static ::ScoreboardId& INVALID();
     // NOLINTEND
 };
+
+namespace std {
+template <>
+struct hash<::ScoreboardId> {
+    size_t operator()(::ScoreboardId const& id) const noexcept {
+        ActorUniqueID uId{};
+        uId.rawID = id.mRawID;
+        return uId.getHash();
+    }
+};
+} // namespace std
