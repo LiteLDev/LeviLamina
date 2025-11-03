b769e4e5a878c823c7494948570b1be2e495fd79
ShrBox <shrbox@outlook.com>
2025-11-04 00:39:42 +0800

    fix: fix OwnerPtrFactory

--- src-server/mc/util/OwnerPtrFactory.h
+++ src-server/mc/util/OwnerPtrFactory.h
@@ -4,9 +4,10 @@
 #include "mc/deps/game_refs/OwnerPtr.h"
 
 template <typename T0, typename T1>
-class OwnerPtrFactory {public:
-    using FactoryMap = std::unordered_map<std::string, std::function<OwnerPtr<T0>(T1, T2)>>;
+class OwnerPtrFactory {
+public:
+    using FactoryMap = std::unordered_map<std::string, std::function<OwnerPtr<T0>(T1)>>;
     FactoryMap mFactoryMap;
 
-    MCAPI void registerFactory(std::string const&, std::function<OwnerPtr<T0>(T1, T2)>);
+    MCAPI void registerFactory(std::string const&, std::function<OwnerPtr<T0>(T1)>);
 };
