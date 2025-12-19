672fb50d75cd2ad8d62169e1db828bf844df9104
Esczvefs <guxiang@litebds.com>
2025-12-19 10:34:54 +0800

    chore: update config

---
+++ src-client/mc/client/module/GameModuleClientArgs.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "mc/_HeaderOutputPredefine.h"
+
+struct GameModuleClientArgs {
+public:
+    // member variables
+    // NOLINTBEGIN
+    ::ll::UntypedStorage<1, 1> mUnk7f037d;
+    ::ll::UntypedStorage<1, 1> mUnkc871e0;
+    // NOLINTEND
+
+public:
+    // prevent constructor by default
+    GameModuleClientArgs& operator=(GameModuleClientArgs const&);
+    GameModuleClientArgs(GameModuleClientArgs const&);
+    GameModuleClientArgs();
+};
