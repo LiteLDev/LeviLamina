dc422f88babf2f0593f35f5d99bcb0b8739b16fb
ShrBox <shrbox@outlook.com>
2026-02-04 13:48:59 +0800

    fix: add missing export for CompressedTextureUtils

--- src/mc/deps/core_graphics/CompressedTextureUtils.h
+++ src/mc/deps/core_graphics/CompressedTextureUtils.h
@@ -2,5 +2,5 @@
 #include "mc/deps/core_graphics/enums/TextureFormat.h"
 
 namespace mce {
-size_t getCompressedImageSize(mce::TextureFormat format, uint32 width, uint32 height, uint32 mipLevelCount);
+LLAPI size_t getCompressedImageSize(mce::TextureFormat format, uint32 width, uint32 height, uint32 mipLevelCount);
 }
