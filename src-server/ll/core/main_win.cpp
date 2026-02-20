720afd79716413a6b455a3e7a4d9b1fbb96d98be
Lovelylavender4 <MiracleForest@outlook.com>
2026-02-20 17:25:16 +0800

    feat: added loading of packages from the corresponding directory under the server-side mods

--- src-server/ll/core/main_win.cpp
+++ src-server/ll/core/main_win.cpp
@@ -33,9 +33,33 @@
 #include "mc/server/ServerInstance.h"
 #include "mc/server/commands/StopCommand.h"
 
+#include "mc/deps/core/file/Path.h"
+#include "mc/deps/core/resource/PackOrigin.h"
+#include "mc/deps/core/resource/PackType.h"
+#include "mc/resources/CompositePackSource.h"
+#include "mc/resources/DirectoryPackSource.h"
+#include "mc/resources/IResourcePackRepository.h"
+#include "mc/resources/Pack.h"
+#include "mc/resources/PackInstance.h"
+#include "mc/resources/PackManifest.h"
+#include "mc/resources/PackSettingsFactory.h"
+#include "mc/resources/PackSourceFactory.h"
+#include "mc/resources/RepositorySources.h"
+#include "mc/resources/ResourcePack.h"
+#include "mc/resources/ResourcePackStack.h"
+#include "mc/scripting/ServerScriptManager.h"
+#include "mc/server/ServerInstance.h"
+#include "mc/server/module/VanillaGameModuleServer.h"
+#include <mc/events/IMinecraftEventing.h>
+#include <mc/util/BaseGameVersion.h>
+#include <mc/world/events/EventResult.h>
+#include <mc/world/level/block/BlockType.h>
+
 #include "windows.h"
 
 #include "tlhelp32.h"
+
+#include "pl/Config.h"
 
 #if defined(LL_DEBUG)
 #include "ll/api/utils/StacktraceUtils.h"
@@ -261,6 +285,65 @@ LL_AUTO_TYPE_INSTANCE_HOOK(
     command::CommandRegistrar::getInstance(false).clear();
 
     origin();
+}
+
+LL_AUTO_TYPE_INSTANCE_HOOK(
+    InitPacksHook,
+    HookPriority::High,
+    RepositorySources,
+    &RepositorySources::initializePackSource,
+    void,
+    ::PackSourceFactory& packSourceFactory
+) {
+    origin(packSourceFactory);
+    auto& resourcePackSourceMain =
+        packSourceFactory
+            .createDirectoryPackSource(getSelfModIns()->getResourceDir(), PackType::Resources, PackOrigin::User, false);
+    mPackSource->mPackSources->push_back(&resourcePackSourceMain);
+    auto& behaviorPackSourceMain =
+        packSourceFactory
+            .createDirectoryPackSource(getSelfModIns()->getBehaviorDir(), PackType::Behavior, PackOrigin::User, false);
+    mPackSource->mPackSources->push_back(&behaviorPackSourceMain);
+    for (auto& mod : mod::ModManagerRegistry::getInstance().mods()) {
+        auto& resourcePackSource =
+            packSourceFactory
+                .createDirectoryPackSource(mod.getResourceDir(), PackType::Resources, PackOrigin::User, false);
+        mPackSource->mPackSources->push_back(&resourcePackSource);
+        auto& behaviorPackSource =
+            packSourceFactory
+                .createDirectoryPackSource(mod.getBehaviorDir(), PackType::Behavior, PackOrigin::User, false);
+        mPackSource->mPackSources->push_back(&behaviorPackSource);
+    }
+}
+
+LL_AUTO_TYPE_INSTANCE_HOOK(
+    InitPacksHook3,
+    HookPriority::High,
+    VanillaGameModuleServer,
+    &VanillaGameModuleServer::$initializeBehaviorStack,
+    void,
+    ::Experiments const&                                            experiments,
+    ::Bedrock::NotNullNonOwnerPtr<::IResourcePackRepository> const& repo,
+    ::ResourcePackStack&                                            stack,
+    ::BaseGameVersion const&                                        baseGameVersion,
+    bool                                                            includeEditorPacks
+) {
+    repo->forEachPack([&](const ::ResourcePack& pack) {
+        if (pack.mPack->mManifest->mPackType == PackType::Behavior) {
+            auto packPath = pack.mPack->mManifest->mLocation->mPath->value;
+            if (packPath.find(pl::pl_mods_path) != std::string::npos) {
+                auto& identity = pack.mPack->mManifest->mIdentity;
+                auto  newPack  = repo->getResourcePackForPackId(identity);
+                if (newPack) {
+                    PackSettingsFactory& factory  = repo->getPackSettingsFactory();
+                    auto&                manifest = newPack->mPack->mManifest;
+                    PackSettings*        settings = factory.getPackSettings(*manifest, {});
+                    stack.add({newPack, -1, false, settings}, repo, false);
+                }
+            }
+        }
+    });
+    origin(experiments, repo, stack, baseGameVersion, includeEditorPacks);
 }
 
 } // namespace ll
