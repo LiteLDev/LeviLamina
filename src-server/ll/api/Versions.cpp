a4344479267f2585f87d2b491a988fcdd9b9a9ac
ShrBox <shrbox@outlook.com>
2026-04-04 18:27:34 +0800

    fix: fix ll::getGameVersion()

--- src-server/ll/api/Versions.cpp
+++ src-server/ll/api/Versions.cpp
@@ -7,7 +7,7 @@ namespace ll {
 data::Version getGameVersion() {
     static auto ver = [] {
         auto info = Common::getBuildInfo();
-        auto v    = data::Version{info.mBuildId};
+        auto v    = data::Version{info.mGameVersion};
         v.build   = info.mCommitId.substr(0, std::min(info.mCommitId.size(), (size_t)7));
         return v;
     }();
