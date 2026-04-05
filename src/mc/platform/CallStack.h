2140f4bdb8ef4173941cf427c662b5b6412cbd55
ShrBox <shrbox@outlook.com>
2026-04-05 19:09:37 +0800

    fix: remove useless in CallStack.h

--- src/mc/platform/CallStack.h
+++ src/mc/platform/CallStack.h
@@ -97,10 +97,6 @@ public:
     // NOLINTBEGIN
     MCAPI explicit CallStack(::Bedrock::CallStack::FrameWithContext&& frame);
 
-#ifdef LL_PLAT_C
-    MCAPI ::Bedrock::CallStack& operator=(::Bedrock::CallStack&&);
-#endif
-
     MCAPI ~CallStack();
     // NOLINTEND
 
