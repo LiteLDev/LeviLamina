4c08d55dca2abe0ab3f0b1b1e26f72c03dd32a3d
ShrBox <shrbox@outlook.com>
2026-04-09 17:53:12 +0800

    feat: implement FileReference, Identifier, and Reference in mc/deps/shared_types/util/

--- src/mc/deps/shared_types/util/Identifier.h
+++ src/mc/deps/shared_types/util/Identifier.h
@@ -2,9 +2,16 @@
 
 #include "mc/_HeaderOutputPredefine.h"
 
+#include "mc/deps/shared_types/util/AssetType.h"
+
 namespace SharedTypes {
 
-template <int T0>
-struct Identifier {};
+template <AssetType type>
+struct Identifier {
+    std::string mValue;
+
+    bool     operator==(Identifier const& other) const { return mValue == other.mValue; }
+    explicit operator std::string const&() const { return mValue; }
+};
 
 } // namespace SharedTypes
--- src/mc/deps/shared_types/util/Reference.h
+++ src/mc/deps/shared_types/util/Reference.h
@@ -2,9 +2,16 @@
 
 #include "mc/_HeaderOutputPredefine.h"
 
+#include "mc/deps/shared_types/util/AssetType.h"
+
 namespace SharedTypes {
 
-template <int T0>
-struct Reference {};
+template <AssetType type>
+struct Reference {
+    std::string mValue;
+
+    bool     operator==(Reference const& other) const { return mValue == other.mValue; }
+    explicit operator std::string const&() const { return mValue; }
+};
 
 } // namespace SharedTypes
