45c19c9f2c772774999273440f14e36579b2b099
ShrBox <shrbox@outlook.com>
2026-04-12 10:50:11 +0800

    chore: update config

---
+++ src-client/mc/client/world/level/biome/biome_color_sampling/StemTessellationPolicy.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "mc/_HeaderOutputPredefine.h"
+
+// auto generated inclusion list
+#include "mc/client/world/level/biome/biome_color_sampling/TessellationPolicy.h"
+
+// auto generated forward declare list
+// clang-format off
+class BiomeTintCache;
+class Block;
+class BlockPos;
+class BlockSource;
+namespace mce { class Color; }
+// clang-format on
+
+namespace BiomeColorSampling {
+
+class StemTessellationPolicy : public ::BiomeColorSampling::TessellationPolicy {
+public:
+    // virtual functions
+    // NOLINTBEGIN
+    virtual ::mce::Color get(::Block const& block, ::BlockSource&, ::BlockPos const&, ::BiomeTintCache const*) const
+        /*override*/;
+
+    virtual ::mce::Color get(::Block const& block) const /*override*/;
+
+    virtual ~StemTessellationPolicy() /*override*/ = default;
+    // NOLINTEND
+
+public:
+    // virtual function thunks
+    // NOLINTBEGIN
+    MCFOLD ::mce::Color $get(::Block const& block, ::BlockSource&, ::BlockPos const&, ::BiomeTintCache const*) const;
+
+    MCFOLD ::mce::Color $get(::Block const& block) const;
+    // NOLINTEND
+
+public:
+    // vftables
+    // NOLINTBEGIN
+    MCNAPI static void** $vftable();
+    // NOLINTEND
+};
+
+} // namespace BiomeColorSampling
---
+++ src-client/mc/client/world/level/biome/biome_color_sampling/WaterTessellationPolicy.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "mc/_HeaderOutputPredefine.h"
+
+// auto generated inclusion list
+#include "mc/client/world/level/biome/biome_color_sampling/TessellationPolicy.h"
+
+// auto generated forward declare list
+// clang-format off
+class BiomeTintCache;
+class Block;
+class BlockPos;
+class BlockSource;
+namespace mce { class Color; }
+// clang-format on
+
+namespace BiomeColorSampling {
+
+class WaterTessellationPolicy : public ::BiomeColorSampling::TessellationPolicy {
+public:
+    // virtual functions
+    // NOLINTBEGIN
+    virtual ::mce::Color
+    get(::Block const&, ::BlockSource& region, ::BlockPos const& pos, ::BiomeTintCache const* biomeTintCache) const
+        /*override*/;
+
+    virtual ::mce::Color get(::Block const&) const /*override*/;
+
+    virtual ~WaterTessellationPolicy() /*override*/ = default;
+    // NOLINTEND
+
+public:
+    // virtual function thunks
+    // NOLINTBEGIN
+    MCAPI ::mce::Color
+    $get(::Block const&, ::BlockSource& region, ::BlockPos const& pos, ::BiomeTintCache const* biomeTintCache) const;
+
+    MCAPI ::mce::Color $get(::Block const&) const;
+    // NOLINTEND
+
+public:
+    // vftables
+    // NOLINTBEGIN
+    MCNAPI static void** $vftable();
+    // NOLINTEND
+};
+
+} // namespace BiomeColorSampling
