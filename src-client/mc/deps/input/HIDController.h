6891c6f4341427e225295f748e13ebfad90cd969
ShrBox <shrbox@outlook.com>
2026-06-17 19:08:04 +0800

    fix: add missing include for HIDController.h

--- src-client/mc/deps/input/HIDController.h
+++ src-client/mc/deps/input/HIDController.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "mc/_HeaderOutputPredefine.h"
+#include "mc/deps/core/islands/SignalReceiver.h"
 
 // auto generated inclusion list
 #include "mc/deps/core/utility/NonOwnerPointer.h"
