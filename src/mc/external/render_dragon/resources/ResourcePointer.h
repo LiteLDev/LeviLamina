f1cda71bfe993507fad0976da55218dfaa5aeff1
Qiuzhizhe <42761326+quizhizhe@users.noreply.github.com>
2026-07-07 09:17:27 -0700

    feat: complate `ServerResourcePointer`, `ResourcePointer`  class

--- src/mc/external/render_dragon/resources/ResourcePointer.h
+++ src/mc/external/render_dragon/resources/ResourcePointer.h
@@ -1,10 +1,16 @@
 #pragma once
 
 #include "mc/_HeaderOutputPredefine.h"
+#include "mc/external/render_dragon/resources/ResourceBlockTemplate.h"
 
 namespace mce {
 
 template <typename T0>
-class ResourcePointer {};
+class ResourcePointer {
+public:
+    ::std::shared_ptr<::mce::ResourceBlockTemplate<T0>> mResourcePointerBlock;
+
+    virtual ~ResourcePointer() = default;
+};
 
 } // namespace mce
