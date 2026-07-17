efaa03f185d42c90132e39eb4080b16bea0f6061
LordBombardir <lordanon6@gmail.com>
2026-07-17 15:05:26 +0300

    feat(ddui): introduce DduiSession, CustomFormSession, MessageBoxSession and refactor DduiManager/FormIdManager

--- src/ll/api/ddui/FormIdManager.cpp
+++ src/ll/api/ddui/FormIdManager.cpp
@@ -2,45 +2,55 @@
 #include "ll/api/ddui/CustomForm.h"
 #include "ll/api/ddui/MessageBox.h"
 #include "ll/core/ddui/DduiManager.h"
+#include "ll/core/ddui/CustomFormSession.h"
+#include "ll/core/ddui/MessageBoxSession.h"
+#include "ll/api/form/FormIdManager.h"
 #include "mc/world/actor/player/Player.h"
 
 namespace ll::ddui {
 
-uint       FormIdManager::mNextFormId = 20000;
-std::mutex FormIdManager::mMutex;
-
-// NOTE: This value should be synchronized with the BDS counter, but its location is currently unknown.
 uint FormIdManager::genFormId() {
-    std::lock_guard<std::mutex> lock(mMutex);
-    return mNextFormId++;
+    return ll::form::FormIdManager::genFormId();
 }
 
 CustomForm* FormIdManager::getCustomForm(uint id) {
     std::lock_guard<std::mutex> lock(DduiManager::mMutex);
-
-    auto it = DduiManager::mActiveCustomForms.find(id);
-    return it != DduiManager::mActiveCustomForms.end() ? it->second : nullptr;
+    auto it = DduiManager::mActiveSessions.find(id);
+    if (it != DduiManager::mActiveSessions.end() && it->second->isCustomForm()) {
+        auto session = std::static_pointer_cast<CustomFormSession>(it->second);
+        return session->mWrapper;
+    }
+    return nullptr;
 }
 
 CustomForm* FormIdManager::getCustomForm(Player& player) {
     std::lock_guard<std::mutex> lock(DduiManager::mMutex);
-
-    auto it = DduiManager::mPlayerActiveCustomForms.find(&player);
-    return it != DduiManager::mPlayerActiveCustomForms.end() ? it->second : nullptr;
+    auto it = DduiManager::mPlayerActiveSessions.find(player.getUuid().asString());
+    if (it != DduiManager::mPlayerActiveSessions.end() && it->second->isCustomForm()) {
+        auto session = std::static_pointer_cast<CustomFormSession>(it->second);
+        return session->mWrapper;
+    }
+    return nullptr;
 }
 
 MessageBox* FormIdManager::getMessageBox(uint id) {
     std::lock_guard<std::mutex> lock(DduiManager::mMutex);
-
-    auto it = DduiManager::mActiveMessageBoxes.find(id);
-    return it != DduiManager::mActiveMessageBoxes.end() ? it->second : nullptr;
+    auto it = DduiManager::mActiveSessions.find(id);
+    if (it != DduiManager::mActiveSessions.end() && !it->second->isCustomForm()) {
+        auto session = std::static_pointer_cast<MessageBoxSession>(it->second);
+        return session->mWrapper;
+    }
+    return nullptr;
 }
 
 MessageBox* FormIdManager::getMessageBox(Player& player) {
     std::lock_guard<std::mutex> lock(DduiManager::mMutex);
-
-    auto it = DduiManager::mPlayerActiveMessageBoxes.find(&player);
-    return it != DduiManager::mPlayerActiveMessageBoxes.end() ? it->second : nullptr;
+    auto it = DduiManager::mPlayerActiveSessions.find(player.getUuid().asString());
+    if (it != DduiManager::mPlayerActiveSessions.end() && !it->second->isCustomForm()) {
+        auto session = std::static_pointer_cast<MessageBoxSession>(it->second);
+        return session->mWrapper;
+    }
+    return nullptr;
 }
 
 } // namespace ll::ddui
