1ab10f0e478ca862c901cbfa6e4b68abb7c6ff30
LordBombardir <lordanon6@gmail.com>
2026-07-17 15:04:29 +0300

    refactor(ddui): make Observable thread-safe and extend ObsStringOrString to support UIRawMessage

--- src/ll/api/ddui/Observable.h
+++ src/ll/api/ddui/Observable.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "ll/api/ddui/UIRawMessage.h"
+#include "nlohmann/json.hpp"
 #include <functional>
+#include <mutex>
 #include <string>
 #include <unordered_map>
 #include <utility>
@@ -24,6 +26,7 @@ protected:
     bool                                         mClientWritable;
     std::unordered_map<SubscriptionId, Callback> mSubscribers;
     SubscriptionId                               mNextId = 0;
+    mutable std::recursive_mutex                 mMutex;
 
 public:
     explicit Observable(T initialData, ObservableOptions const& options = {})
@@ -32,28 +35,40 @@ public:
 
     virtual ~Observable() = default;
 
-    [[nodiscard]] T    getData() const { return mData; }
-    [[nodiscard]] bool isClientWritable() const { return mClientWritable; }
+    [[nodiscard]] T getData() const {
+        std::lock_guard<std::recursive_mutex> lock(mMutex);
+        return mData;
+    }
+
+    [[nodiscard]] bool isClientWritable() const {
+        std::lock_guard<std::recursive_mutex> lock(mMutex);
+        return mClientWritable;
+    }
 
     void setData(T const& data) {
-        if (mData != data) {
+        std::unordered_map<SubscriptionId, Callback> subs;
+        {
+            std::lock_guard<std::recursive_mutex> lock(mMutex);
+            if (mData == data) return;
             mData = data;
-
-            auto subs = mSubscribers;
-            for (auto const& [id, callback] : subs) {
-                if (callback) callback(mData);
-            }
+            subs = mSubscribers;
+        }
+        for (auto const& [id, callback] : subs) {
+            if (callback) callback(data);
         }
     }
 
     SubscriptionId subscribe(Callback callback) {
+        std::lock_guard<std::recursive_mutex> lock(mMutex);
         SubscriptionId id = mNextId++;
-
         mSubscribers[id] = std::move(callback);
         return id;
     }
 
-    bool unsubscribe(SubscriptionId id) { return mSubscribers.erase(id) > 0; }
+    bool unsubscribe(SubscriptionId id) {
+        std::lock_guard<std::recursive_mutex> lock(mMutex);
+        return mSubscribers.erase(id) > 0;
+    }
 };
 
 class ObservableString : public Observable<std::string> {
@@ -78,7 +93,12 @@ public:
     : Observable(data, options) {}
 };
 
-using ObsStringOrString = std::variant<std::string, std::shared_ptr<ObservableString>>;
+using ObsStringOrString = std::variant<
+    std::string,
+    std::shared_ptr<ObservableString>,
+    UIRawMessage,
+    std::shared_ptr<ObservableUIRawMessage>
+>;
 using ObsNumberOrNumber = std::variant<double, std::shared_ptr<ObservableNumber>>;
 using ObsBoolOrBool     = std::variant<bool, std::shared_ptr<ObservableBoolean>>;
 
@@ -92,13 +112,42 @@ inline T resolveOption(std::variant<T, std::shared_ptr<ObsT>> const& opt) {
     }
 }
 
+inline nlohmann::json resolveText(ObsStringOrString const& opt) {
+    return std::visit(
+        [](auto const& val) -> nlohmann::json {
+            using Type = std::decay_t<decltype(val)>;
+            if constexpr (std::is_same_v<Type, std::string>) {
+                return val;
+            } else if constexpr (std::is_same_v<Type, std::shared_ptr<ObservableString>>) {
+                return val ? val->getData() : "";
+            } else if constexpr (std::is_same_v<Type, UIRawMessage>) {
+                return val.serialize();
+            } else if constexpr (std::is_same_v<Type, std::shared_ptr<ObservableUIRawMessage>>) {
+                return val ? val->getData().serialize() : nlohmann::json::object();
+            }
+            return "";
+        },
+        opt
+    );
+}
+
 inline std::string resolveString(ObsStringOrString const& opt) {
-    if (std::holds_alternative<std::string>(opt)) {
-        return std::get<std::string>(opt);
-    } else {
-        auto obs = std::get<std::shared_ptr<ObservableString>>(opt);
-        return obs ? obs->getData() : "";
-    }
+    return std::visit(
+        [](auto const& val) -> std::string {
+            using Type = std::decay_t<decltype(val)>;
+            if constexpr (std::is_same_v<Type, std::string>) {
+                return val;
+            } else if constexpr (std::is_same_v<Type, std::shared_ptr<ObservableString>>) {
+                return val ? val->getData() : "";
+            } else if constexpr (std::is_same_v<Type, UIRawMessage>) {
+                return val.serialize().dump();
+            } else if constexpr (std::is_same_v<Type, std::shared_ptr<ObservableUIRawMessage>>) {
+                return val ? val->getData().serialize().dump() : "";
+            }
+            return "";
+        },
+        opt
+    );
 }
 
 } // namespace ll::ddui
