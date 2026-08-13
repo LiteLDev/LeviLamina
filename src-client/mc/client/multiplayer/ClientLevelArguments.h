5acfe58f0750bd0c8d167637fdbe6f6a7c72cb6b
Lovelylavender4 <MiracleForest@outlook.com>
2026-08-13 14:07:43 +0800

    chore: rename MultiPlayerLevel to ClientLevel

---
+++ src-client/mc/client/multiplayer/ClientLevelArguments.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "mc/_HeaderOutputPredefine.h"
+
+struct ClientLevelArguments {
+public:
+    // member variables
+    // NOLINTBEGIN
+    ::ll::UntypedStorage<8, 312> mUnk13f328;
+    ::ll::UntypedStorage<8, 16>  mUnkc86228;
+    ::ll::UntypedStorage<8, 216> mUnk293af1;
+    ::ll::UntypedStorage<8, 8>   mUnkfadec2;
+    ::ll::UntypedStorage<8, 8>   mUnk244b39;
+    // NOLINTEND
+
+public:
+    // prevent constructor by default
+    ClientLevelArguments& operator=(ClientLevelArguments const&);
+    ClientLevelArguments(ClientLevelArguments const&);
+    ClientLevelArguments();
+};
