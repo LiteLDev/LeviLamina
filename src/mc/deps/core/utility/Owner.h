43002907367d569ee308302c353176d64641f134
ShrBox <shrbox@outlook.com>
2026-08-17 19:43:35 +0800

    fix: fix Bedrock::Owner

--- src/mc/deps/core/utility/Owner.h
+++ src/mc/deps/core/utility/Owner.h
@@ -1,11 +1,12 @@
 #pragma once
 
 #include "mc/_HeaderOutputPredefine.h"
+#include "mc/deps/core/utility/EnableNonOwnerReferences.h"
 
 namespace Bedrock {
 
 template <typename T0>
-class Owner {
+class Owner : public Bedrock::EnableNonOwnerReferences {
 public:
     T0 mValue;
 };
