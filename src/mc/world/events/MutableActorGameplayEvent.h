13a2a9cad796845a67eb61d282eb0c48763b9307
ShrBox <shrbox@outlook.com>
2026-09-05 21:26:46 +0800

    chore: update gameplay events templates

--- src/mc/world/events/MutableActorGameplayEvent.h
+++ src/mc/world/events/MutableActorGameplayEvent.h
@@ -3,12 +3,21 @@
 #include "mc/_HeaderOutputPredefine.h"
 #include "mc/gameplayhandlers/CoordinatorResult.h"
 #include "mc/world/events/ActorAddEffectEvent.h"
+#include "mc/world/events/ActorBeforeAcquireItemEvent.h"
+#include "mc/world/events/ActorBeforeHealEvent.h"
+#include "mc/world/events/ActorBeforeHurtEvent.h"
 #include "mc/world/events/ActorDefinitionStartedEvent.h"
+#include "mc/world/events/ActorUpgradeStartedEvent.h"
 #include "mc/world/events/EventVariantImpl.h"
 
 template <typename Result>
 struct MutableActorGameplayEvent;
 
 template <>
-struct MutableActorGameplayEvent<CoordinatorResult>
-: MutableEventVariant<ActorDefinitionStartedEvent, ActorAddEffectEvent> {};
+struct MutableActorGameplayEvent<CoordinatorResult> : MutableEventVariant<
+                                                          ActorDefinitionStartedEvent,
+                                                          ActorAddEffectEvent,
+                                                          ActorBeforeAcquireItemEvent,
+                                                          ActorBeforeHealEvent,
+                                                          ActorBeforeHurtEvent,
+                                                          ActorUpgradeStartedEvent> {};
