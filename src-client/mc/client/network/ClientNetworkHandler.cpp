a2a562a17c5ea2d45dfda41d0eb220510450cd54
LordBombardir <lordanon6@gmail.com>
2026-09-11 22:35:56 +0300

    fix(client): comment out _ensureVoxelShapeRegistryExists body as TODO

--- src-client/mc/client/network/ClientNetworkHandler.cpp
+++ src-client/mc/client/network/ClientNetworkHandler.cpp
@@ -61,12 +61,12 @@ void ClientNetworkHandler::onChunkHandleCompleted(
     }
 }
 
-void ClientNetworkHandler::_ensureVoxelShapeRegistryExists(::Experiments const& experiments) {
+void ClientNetworkHandler::_ensureVoxelShapeRegistryExists(::Experiments const& /*experiments*/) {
     if (!mVoxelShapeRegistry) {
-        mVoxelShapeRegistry = std::make_unique<VoxelShapes::VoxelShapeRegistry>();
-        if (experiments.isExperimentEnabled(AllExperiments::VoxelShapes)) {
-            // TODO: https://github.com/LiteLDev/mcapi-requests/issues/242
-            // mVoxelShapeRegistry->initialize();
-        }
+        // TODO: https://github.com/LiteLDev/mcapi-requests/issues/242
+        // mVoxelShapeRegistry = std::make_unique<VoxelShapes::VoxelShapeRegistry>();
+        // if (experiments.isExperimentEnabled(AllExperiments::VoxelShapes)) {
+        //     mVoxelShapeRegistry->initialize();
+        // }
     }
 }
