5f9a28ac4fb69a98a1c779033024838e0ab5b4e9
LordBombardir <lordanon6@gmail.com>
2026-09-11 22:10:43 +0300

    feat(client): implement LevelRenderer::rebuildAllRenderChunkGeometry

---
+++ src-client/mc/client/renderer/game/LevelRenderer.cpp
@@ -0,0 +1,11 @@
+﻿#include "mc/client/renderer/game/LevelRenderer.h"
+
+#include "mc/client/renderer/chunks/RenderChunkCoordinator.h"
+
+void LevelRenderer::rebuildAllRenderChunkGeometry() {
+    for (auto& [dim, coordinator] : *mRenderChunkCoordinators) {
+        if (coordinator) {
+            coordinator->_setAllDirty(false, false);
+        }
+    }
+}
