81f7e6291e7a316bca18631a413767fc577f65a2
ShrBox <shrbox@outlook.com>
2026-09-13 22:25:33 +0800

    feat: implement BlockEventPublishingExecutor and BlockEventExecutor

--- src/mc/world/level/block/block_events/BlockEventPublishingExecutor.h
+++ src/mc/world/level/block/block_events/BlockEventPublishingExecutor.h
@@ -1,10 +1,23 @@
 #pragma once
 
 #include "mc/_HeaderOutputPredefine.h"
+#include "mc/deps/core/utility/pub_sub/Publisher.h"
+#include "mc/world/level/block/block_events/BlockEventBlockTypeExecutor.h"
 
 namespace BlockEvents::Detail {
 
-template <typename T0, typename T1>
-class BlockEventPublishingExecutor {};
+template <class T0, class T1 = void>
+class BlockEventPublishingExecutor : public BlockEvents::Detail::BlockEventBlockTypeExecutor {
+public:
+    Bedrock::PubSub::
+        Publisher<void(T0&), Bedrock::PubSub::ThreadModel::SingleThreaded, Bedrock::PubSub::ReturnPolicyType::Aggregate>
+            mPublisher;
+
+    virtual ~BlockEventPublishingExecutor() = default;
+
+    virtual ::BlockEvents::EventType getEventType() const;
+
+    virtual void dispatch(T0& event);
+};
 
 } // namespace BlockEvents::Detail
