c0b7a8a0bfad587dcf543263da7ae6e016d4d5f1
ShrBox <shrbox@outlook.com>
2026-09-16 00:31:30 +0800

    chore(deps): bump bedrock-runtime-data version

--- xmake.lua
+++ xmake.lua
@@ -51,11 +51,11 @@ end
 
 if is_server then
     if is_windows then
-        add_requires("bedrockdata v26.40.8-server.4")
+        add_requires("bedrockdata v26.40.8-server.5")
     end
 else
     if is_windows then
-        add_requires("bedrockdata v26.40.5-client.4")
+        add_requires("bedrockdata v26.40.5-client.5")
     end
 end
 
