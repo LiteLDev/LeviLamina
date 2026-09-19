e4bfe69e0a49444ef40c5271ed4bed64e6a1232e
quizhizhe <42761326+quizhizhe@users.noreply.github.com>
2026-09-19 13:29:22 +0800

    feat: complate ResourcePointer

--- src/mc/deps/core/checked_resource_service/PerFrameHandleTracker.h
+++ src/mc/deps/core/checked_resource_service/PerFrameHandleTracker.h
@@ -2,8 +2,25 @@
 
 #include "mc/_HeaderOutputPredefine.h"
 
+#include "mc/deps/core/checked_resource_service/ValidityCheckType.h"
+
 namespace mce {
 
-class PerFrameHandleTracker {};
+class PerFrameHandleTracker {
+public:
+    std::atomic<unsigned short> mCheckCount;
+    std::atomic<bool>           mIsValid;
+
+public:
+    std::tuple<bool, bool> Validate(ValidityCheckType validityCheckType) {
+        bool checkResult = false;
+
+        if (validityCheckType == (ValidityCheckType)0) {
+            checkResult = (++mCheckCount == 0);
+        }
+
+        return {checkResult, mIsValid.load()};
+    }
+};
 
 } // namespace mce
