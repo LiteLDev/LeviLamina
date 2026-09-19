3891d07ffdd5794a196c2b8175701057e18dd6ed
Qiuzhizhe <42761326+quizhizhe@users.noreply.github.com>
2026-09-18 23:51:18 -0700

    feat: complate ResourcePointer
    ClientResourcePointer
    ServerResourcePointer
    ResourceBlock

--- src/mc/external/render_dragon/resources/ResourcePointer.h
+++ src/mc/external/render_dragon/resources/ResourcePointer.h
@@ -5,12 +5,34 @@
 
 namespace mce {
 
-template <typename T0>
+template <typename T>
 class ResourcePointer {
 public:
-    ::std::shared_ptr<::mce::ResourceBlockTemplate<T0>> mResourcePointerBlock;
+    std::shared_ptr<ResourceBlockTemplate<T>> mResourcePointerBlock;
+
+    ResourcePointer() = default;
+    explicit ResourcePointer(std::shared_ptr<ResourceBlockTemplate<T>> const& block)
+    : mResourcePointerBlock(block) {} // [inferred]
 
     virtual ~ResourcePointer() = default;
+
+    bool isValid(ValidityCheckType validityCheckType) const {
+        if (!mResourcePointerBlock) {
+            return false;
+        }
+        return std::get<0>(mResourcePointerBlock->mTrackingBlock.Validate(validityCheckType));
+    }
+
+    T*       get() const { return mResourcePointerBlock ? mResourcePointerBlock->get() : nullptr; }
+    T*       operator->() const { return get(); }
+    T&       operator*() const { return *get(); }
+    explicit operator bool() const { return mResourcePointerBlock != nullptr; }
+    void     reset() { mResourcePointerBlock.reset(); }
+
+    ResourcePointer(ResourcePointer const&)            = default;
+    ResourcePointer(ResourcePointer&&)                 = default;
+    ResourcePointer& operator=(ResourcePointer const&) = default;
+    ResourcePointer& operator=(ResourcePointer&&)      = default;
 };
 
 } // namespace mce
--- src/mc/platform/threading/PrioritySharedMutex.h
+++ src/mc/platform/threading/PrioritySharedMutex.h
@@ -4,7 +4,17 @@
 
 namespace Bedrock::Threading {
 
-template <typename T0>
-class PrioritySharedMutex {};
+template <typename Strategy>
+class PrioritySharedMutex {
+public:
+    void lock() { mStrategy.lock(); }
+    bool try_lock() { return mStrategy.try_lock(); }
+    void unlock() { mStrategy.unlock(); }
+
+    void lock_shared() { mStrategy.lock_shared(); }
+    void unlock_shared() { mStrategy.unlock_shared(); }
+
+    Strategy mStrategy;
+};
 
 } // namespace Bedrock::Threading
